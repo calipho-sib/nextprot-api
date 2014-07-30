c6268193ecf76be2150f3ec5416d61ad4f2f50a4
pamichel <pamichel@infomaniak.ch>
2014-07-30 16:44:17 +0200

    test 4

--- src/main/java/org/nextprot/api/commons/bio/DescriptorMass.java
+++ src/main/java/org/nextprot/api/commons/bio/DescriptorMass.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 /**
  * return the molecular weight corresponding to a sequence
  * 
- * @author alexandre.masselot@genebio.com, copied by pam today aaa23
+ * @author alexandre.masselot@genebio.com, copied by pam today aaa234
  * 
  */
 public class DescriptorMass {
