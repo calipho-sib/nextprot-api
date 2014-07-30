ebb636e58c1919314c78bd7c13a463a0acc5cb82
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2014-07-30 16:22:55 +0200

    test

--- src/main/java/org/nextprot/api/commons/bio/DescriptorMass.java
+++ src/main/java/org/nextprot/api/commons/bio/DescriptorMass.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 /**
  * return the molecular weight corresponding to a sequence
  * 
- * @author alexandre.masselot@genebio.com, copied by pam
+ * @author alexandre.masselot@genebio.com, copied by pam today
  * 
  */
 public class DescriptorMass {
