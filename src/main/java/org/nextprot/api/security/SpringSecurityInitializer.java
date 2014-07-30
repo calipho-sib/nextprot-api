a8ecf93e1ae899412185e34964e706c46cf7a459
Daniel <ddtxra@gmail.com>
2014-07-30 08:48:04 +0200

    adding three scale security config

--- src/main/java/org/nextprot/api/security/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package org.nextprot.api.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer  extends AbstractSecurityWebApplicationInitializer{
-
-}
