98e3befee624bf9a67095bd96777495efc261c9e
pamichel <pamichel@infomaniak.ch>
2014-08-06 17:32:30 +0200

    first test diff sql / sparql generating CSV file log/diff.log

--- src/test/java/org/nextprot/api/diff/CompareMasterTest.java
+++ src/test/java/org/nextprot/api/diff/CompareMasterTest.java
@@ -3,7 +3,7 @@ package org.nextprot.api.diff;
 import org.junit.Test;
 
 
-public class CompareMasterTest extends CompareBaseTest {
+public class CompareMasterTest extends DiffBaseTest {
 	
 	@Test
 	public void testMasterCount(){
--- src/test/java/org/nextprot/api/diff/DiffBaseTest.java
+++ src/test/java/org/nextprot/api/diff/DiffBaseTest.java
@@ -2,8 +2,11 @@ package org.nextprot.api.diff;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.runner.RunWith;
 
 import org.nextprot.api.commons.spring.jdbc.DataSourceServiceLocator;
@@ -30,17 +33,44 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("dev")
 @ContextConfiguration("classpath:META-INF/spring/core-context.xml")
-abstract class CompareBaseTest {
+abstract class DiffBaseTest {
+	
+	private static final Log Logger = LogFactory.getLog(DiffBaseTest.class);
+
+	private static final Date TestDate = new Date();
 	
 	@Autowired private DataSourceServiceLocator dsLocator;
 	@Autowired private SparqlService sparqlService;
 	@Autowired private SparqlEndpoint endpoint;
+
+	private int timeSQL;
+	private int timeSPARQL;
+	private int countSQL;
+	private int countSPARQL;
+	private String status;
 	
-	protected void diffCount(String qName) {
-		assertTrue(getCountForSparql(qName) == getCountForSql(qName));
+	private void resetDefaultLogValues() {
+		timeSPARQL=-1;
+		timeSQL=-1;
+		countSPARQL=-1;
+		countSQL=-1;
+		status="ERROR";
+	}
+	
+	protected void diffCount(String qName) {		
+		try {
+			resetDefaultLogValues();		
+			getCountForSparql(qName);
+			getCountForSql(qName);
+			status = (countSQL==countSPARQL ? "OK" : "DIFF");
+			assertTrue(countSQL==countSPARQL);
+		} finally {
+			LogIt();
+		}
 	}
 	
 	private int getCountForSql(String qName) {
+		long t0 = System.currentTimeMillis();
 		String sql = getFileContentAsString("sql/" + qName + ".sql");
 		//SqlParameterSource namedParams = new MapSqlParameterSource("id", id);
 		SqlParameterSource namedParams = null;
@@ -50,12 +80,13 @@ abstract class CompareBaseTest {
 				return new Integer(resultSet.getInt("cnt"));
 			}
 		});
-		System.out.println("SQL Count=" + counts.get(0));
-		return counts.get(0);
-	
+		timeSQL = (int)(System.currentTimeMillis()-t0);
+		countSQL = counts.get(0);
+		return countSQL;
 	}
 
 	private int getCountForSparql(String qName){
+		long t0 = System.currentTimeMillis();
 		String query = getFileContentAsString("sparql/" + qName + ".sparql");
 		query= RdfUtils.RDF_PREFIXES + "\n" + query;
 		QueryExecution qExec = endpoint.queryExecution(query);			
@@ -63,13 +94,25 @@ abstract class CompareBaseTest {
 	    QuerySolution qs = rs.next();
 		Literal lit = qs.getLiteral("cnt");
 		int count = lit.getInt();
-		System.out.println("SPARQL count=" + count);
-		return count;
+		timeSPARQL = (int)(System.currentTimeMillis()-t0);
+		countSPARQL = count;
+		return countSPARQL;
 	}
 	
 	private String getFileContentAsString(String path) {
 		String resourcePath = "/org/nextprot/api/diff/";
 		return FileUtils.readResourceAsString(resourcePath + path);
+	}
+
+	private void LogIt() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("TestDate=\""+TestDate.toString() + "\";");
+		sb.append("timeSQL="+timeSQL + ";");
+		sb.append("timeSPARQL="+timeSPARQL + ";");
+		sb.append("countSQL="+countSQL + ";");
+		sb.append("countSPARQL="+countSPARQL + ";");
+		sb.append("status="+status + ";");
+		Logger.info(sb.toString());
 	}
 	
 }
