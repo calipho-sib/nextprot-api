b4651fbee73a767073e6bbc6c798d82f7beae744
olivier <olivier.evalet@isb-sib.ch>
2014-09-29 16:35:22 +0200

    add assignment method for evidence

--- src/main/resources/META-INF/sql/annotation-evidences-by-annotation-ids.sql
+++ src/main/resources/META-INF/sql/annotation-evidences-by-annotation-ids.sql
@@ -12,7 +12,8 @@ select  assoc.is_negative_evidence,
         cv_db.description as resource_desc,
         pub.md5 as publication_md5,
         cv_src.cv_name as evidence_assigned_by,
-        ev_details.publi_type as assignment_method
+-- when assigned by 9 evidence is computed else evidence is curated        
+        (case when assoc.assigned_by_id=9 then 'computed' else 'curated' end) as assignment_method
 from nextprot.annotation_resource_assoc assoc     
 left join nextprot.publications pub on (pub.resource_id = assoc.resource_id)  
 left join nextprot.resources res on (res.resource_id = assoc.resource_id)  
@@ -23,7 +24,6 @@ left join nextprot.db_xrefs dbxrefs on (dbxrefs.resource_id = res.resource_id)
 left join nextprot.cv_datasources cv_src on (assoc.assigned_by_id = cv_src.cv_id) 
 left join nextprot.cv_databases cv_db on (dbxrefs.cv_database_id = cv_db.cv_id) 
 left join nextprot.cv_quality_qualifiers q on (q.cv_id = assoc.cv_quality_qualifier_id) 
-left join nextprot.annotations annot on (annot.annotation_id=assoc.annotation_id)
-left join nextprot.sequence_identifiers master on (annot.identifier_id = master.identifier_id and master.cv_type_id = 1 and master.cv_status_id=1)
-left join nextprot.view_paper_ref_type ev_details on (master.identifier_id=ev_details.master_id and pub.resource_id=ev_details.publication_id)
 where assoc.annotation_id in (:ids) 
+
+
