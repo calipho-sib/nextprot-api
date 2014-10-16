c991e02c5bd1d39713353ab23f996a8de260dfb1
fnikitin <frederic.nikitin@gmail.com>
2014-10-16 17:40:00 +0200

    Pull annotation @ContextConfiguration up in AbstractUnitBaseTest class

--- user/src/test/java/org/nextprot/api/user/dao/test/base/UserApplicationBaseTest.java
+++ user/src/test/java/org/nextprot/api/user/dao/test/base/UserApplicationBaseTest.java
@@ -1,14 +1,9 @@
 package org.nextprot.api.user.dao.test.base;
 
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
-import com.github.springtestdbunit.dataset.DataSetLoader;
-import org.dbunit.util.fileloader.FlatXmlDataFileLoader;
 import org.nextprot.api.commons.dbunit.AbstractUnitBaseTest;
 import org.nextprot.api.commons.dbunit.DTDIgnoredFlatXMLDataSet;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
@@ -30,9 +25,8 @@ import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
     DirtiesContextTestExecutionListener.class,
     TransactionDbUnitTestExecutionListener.class })
-@ContextConfiguration("classpath:spring/commons-context.xml")
 @ActiveProfiles({"unit", "unit-schema-user"})
 @DbUnitConfiguration(dataSetLoader = DTDIgnoredFlatXMLDataSet.class)
-public abstract class UserApplicationBaseTest extends AbstractUnitBaseTest { //implements InitializingBean {
+public abstract class UserApplicationBaseTest extends AbstractUnitBaseTest {
 
 }
