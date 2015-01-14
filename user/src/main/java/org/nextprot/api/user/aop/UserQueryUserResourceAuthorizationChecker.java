2a9ec7e57dfc4ee9363dbedb138d911d02cdfc53
fnikitin <frederic.nikitin@gmail.com>
2015-01-14 12:13:03 +0100

    Remove method isPersisted() from UserResource interface and implementing classes

--- user/src/main/java/org/nextprot/api/user/aop/UserQueryUserResourceAuthorizationChecker.java
+++ user/src/main/java/org/nextprot/api/user/aop/UserQueryUserResourceAuthorizationChecker.java
@@ -3,7 +3,6 @@ package org.nextprot.api.user.aop;
 import org.nextprot.api.commons.exception.NotAuthorizedException;
 import org.nextprot.api.commons.resource.UserResource;
 import org.nextprot.api.user.dao.UserQueryDao;
-import org.nextprot.api.user.domain.UserApplication;
 import org.nextprot.api.user.domain.UserQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -23,15 +22,13 @@ public class UserQueryUserResourceAuthorizationChecker implements UserResourceAu
 
             long queryId = ((UserQuery) query).getUserQueryId();
             
-            if(query.isPersisted()){
+            if (((UserQuery) query).getUserQueryId() != 0){
 
                 UserQuery foundUserQuery = dao.getUserQueryById(queryId);
 
                 if (foundUserQuery.getOwnerId() != ownerId)
                     throw new NotAuthorizedException(query.getOwnerName() + " cannot access resource");
-
             }
-
         }
         else {
 
