d705449c1b7c593b19e1ed240d4eaf514b965b0a
Daniel <ddtxra@gmail.com>
2015-02-11 19:25:33 +0100

    adding github urls

---
+++ web/src/main/java/org/nextprot/api/web/service/GitHubService.java
@@ -0,0 +1,7 @@
+package org.nextprot.api.web.service;
+
+public interface GitHubService {
+
+	public String getPage(String page);
+
+}
---
+++ web/src/main/java/org/nextprot/api/web/service/impl/GitHubServiceImpl.java
@@ -0,0 +1,44 @@
+package org.nextprot.api.web.service.impl;
+
+import java.io.IOException;
+
+import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.nextprot.api.commons.exception.NextProtException;
+import org.nextprot.api.web.service.GitHubService;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * Service that communicates with github to retrieve the pages
+ * 
+ * @author dteixeira
+ *
+ */
+@Service
+public class GitHubServiceImpl implements GitHubService {
+
+	// will refresh every minute, because anonymous calls are limited to 60
+	// calls per hour
+
+	@Override
+	@Cacheable(value = "github-pages")
+	public String getPage(String page) {
+
+		// Not sure if this keeps a connection (if not this can be done on the postconstruct)
+		try {
+			GitHub github = GitHub.connectAnonymously();
+			GHRepository repo = github.getRepository("calipho-sib/nextprot-docs");
+			GHContent content = repo.getFileContent("/pages/" + page + ".md");
+			return content.getContent();
+
+		} catch (IOException e) {
+			
+			throw new NextProtException("Documentation not available, try to fix this soon");
+		}
+
+	}
+
+}
