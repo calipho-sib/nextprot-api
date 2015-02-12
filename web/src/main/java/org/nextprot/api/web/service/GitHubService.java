19594c94d49b324282e189b32547eef7f6bb10de
Daniel <ddtxra@gmail.com>
2015-02-12 14:37:51 +0100

    adding github controller

--- web/src/main/java/org/nextprot/api/web/service/GitHubService.java
+++ web/src/main/java/org/nextprot/api/web/service/GitHubService.java
@@ -1,7 +1,11 @@
 package org.nextprot.api.web.service;
 
+import org.nextprot.api.web.domain.GitHubTrees;
+
 public interface GitHubService {
 
 	public String getPage(String page);
+
+	public GitHubTrees getDirectoryContent();
 
 }
--- web/src/main/java/org/nextprot/api/web/service/impl/GitHubServiceImpl.java
+++ web/src/main/java/org/nextprot/api/web/service/impl/GitHubServiceImpl.java
@@ -1,11 +1,16 @@
 package org.nextprot.api.web.service.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.kohsuke.github.GHContent;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
+import org.kohsuke.github.PagedIterable;
 import org.nextprot.api.commons.exception.NextProtException;
+import org.nextprot.api.web.domain.GitHubTree;
+import org.nextprot.api.web.domain.GitHubTrees;
 import org.nextprot.api.web.service.GitHubService;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -27,17 +32,69 @@ public class GitHubServiceImpl implements GitHubService {
 	@Cacheable(value = "github-pages")
 	public String getPage(String page) {
 
-		// Not sure if this keeps a connection (if not this can be done on the postconstruct)
+		// Not sure if this keeps a connection (if not this can be done on the
+		// postconstruct)
 		try {
-			GitHub github = GitHub.connectAnonymously();
+			GitHub github = GitHub.connectAnonymously();//TODO move this with a token
 			GHRepository repo = github.getRepository("calipho-sib/nextprot-docs");
-			GHContent content = repo.getFileContent("/pages/" + page + ".md");
+			GHContent content = repo.getFileContent("pages/" + page + ".md");
 			return content.getContent();
 
 		} catch (IOException e) {
-			
-			throw new NextProtException("Documentation not available, try to fix this soon");
+			e.printStackTrace();
+			throw new NextProtException("Documentation not available, sorry for the inconvenience");
 		}
+
+	}
+
+	@Override
+	@Cacheable(value = "github-pages")
+	public GitHubTrees getDirectoryContent() {
+
+		GitHubTrees trees = new GitHubTrees();
+		trees.setSha("cf48b122d3aa50fca6049c0b804ae1cea0223fd2");
+		trees.setUrl("https://api.github.com/repos/calipho-sib/nextprot-docs/git/trees/cf48b122d3aa50fca6049c0b804ae1cea0223fd2");
+
+		List<GitHubTree> tree = new ArrayList<GitHubTree>();
+
+		try {
+
+			GitHub github = GitHub.connectAnonymously();
+			GHRepository repo = github.getRepository("calipho-sib/nextprot-docs");
+			List<GHContent> contents = repo.getDirectoryContent("");
+
+			for (GHContent c : contents) {
+				tree.add(buildTree(c));
+				if(c.isDirectory()){
+					PagedIterable<GHContent> cs = c.listDirectoryContent();
+					for(GHContent c1 : cs){
+						tree.add(buildTree(c1));
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new NextProtException("Documentation not available, sorry for the inconvenience");
+		}
+
+		trees.setTree(tree);
+		return trees;
+
+	}
+	
+	private static GitHubTree buildTree(GHContent c){
+		
+		GitHubTree t = new GitHubTree();
+		t.setPath(c.getPath());
+		t.setMode("100644");
+		t.setType(c.getType());
+		t.setSha(c.getSha());
+		t.setSize(c.getSize());
+		t.setUrl(c.getUrl());
+		
+		return t;
+
 
 	}
 
