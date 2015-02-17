947947efae13265733d03636f56b5d0768bc8571
fnikitin <frederic.nikitin@gmail.com>
2015-02-17 16:50:43 +0100

    Missing end of sql instruction delimitor

--- user/src/main/resources/db/migration/V006__alter_user_protein_lists_indexes.sql
+++ user/src/main/resources/db/migration/V006__alter_user_protein_lists_indexes.sql
@@ -1,3 +1,3 @@
-ALTER TABLE np_users.user_protein_lists DROP CONSTRAINT list_u
-ALTER TABLE np_users.user_protein_lists ADD CONSTRAINT list_u UNIQUE (owner_id, list_name)
-DROP INDEX np_users.user_protein_lists_name_udx
+ALTER TABLE np_users.user_protein_lists DROP CONSTRAINT list_u;
+ALTER TABLE np_users.user_protein_lists ADD CONSTRAINT list_u UNIQUE (owner_id, list_name);
+DROP INDEX np_users.user_protein_lists_name_udx;
