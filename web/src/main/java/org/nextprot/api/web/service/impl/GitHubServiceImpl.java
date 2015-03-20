75fe2a9f84ba6a59dd6f5809a9b749ad2b5d612a
Valentine Rech de Laval <Valentine.RechdeLaval@isb-sib.ch>
2015-03-20 16:38:45 +0100

    Change branch to name: set to develop

--- web/src/main/java/org/nextprot/api/web/service/impl/GitHubServiceImpl.java
+++ web/src/main/java/org/nextprot/api/web/service/impl/GitHubServiceImpl.java
@@ -47,7 +47,7 @@ public class GitHubServiceImpl implements GitHubService {
 		try {
 			GitHub github = getGitHubConnection();
 			GHRepository repo = github.getRepository("calipho-sib/nextprot-docs");
-			GHContent content = repo.getFileContent(folder + "/" + page + ".md", "change-urls");
+			GHContent content = repo.getFileContent(folder + "/" + page + ".md", "develop");
 			return content.getContent();
 
 		} catch (IOException e) {
@@ -64,7 +64,7 @@ public class GitHubServiceImpl implements GitHubService {
 		try {
 			GitHub github =  getGitHubConnection();
 			GHRepository repo = github.getRepository("calipho-sib/nextprot-docs");
-			return repo.getTreeRecursive("change-urls", 1);
+			return repo.getTreeRecursive("develop", 1);
 
 		} catch (IOException e) {
 			e.printStackTrace();
