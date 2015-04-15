9f8dc0f16f9772263394419d4732ae6c528712dc
Daniel <ddtxra@gmail.com>
2015-04-15 17:41:09 +0200

    adding sparql cache

--- rdf/src/main/java/org/nextprot/api/rdf/service/impl/SparqlProxyEndpointImpl.java
+++ rdf/src/main/java/org/nextprot/api/rdf/service/impl/SparqlProxyEndpointImpl.java
@@ -7,13 +7,13 @@ import org.nextprot.api.commons.exception.NextProtException;
 import org.nextprot.api.rdf.service.SparqlEndpoint;
 import org.nextprot.api.rdf.service.SparqlProxyEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -23,12 +23,14 @@ public class SparqlProxyEndpointImpl implements SparqlProxyEndpoint {
 	private SparqlEndpoint sparqlEndpoint;
 
 	@Override
+	@Cacheable("sparql-proxy")
 	public ResponseEntity<String> sparql(String body, String queryString) {
 
 		ResponseEntity<String> responseEntity = null;
 
 		String url = sparqlEndpoint.getUrl() + ((queryString != null) ? ("?" + queryString) : "");
-		// uri = new URI("http", null, "kant", 8890, request.getRequestURI(), (qs == null) ? qs : URLDecoder.decode(qs, "UTF-8"), null);
+		// uri = new URI("http", null, "kant", 8890, request.getRequestURI(),
+		// (qs == null) ? qs : URLDecoder.decode(qs, "UTF-8"), null);
 
 		RestTemplate template = new RestTemplate();
 		try {
