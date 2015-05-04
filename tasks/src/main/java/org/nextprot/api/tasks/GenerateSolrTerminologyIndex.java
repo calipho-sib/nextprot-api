a6583ceaecbcf781f99853eac04ee83a53846067
Alain-Gateau <alain.gateau@gmail.com>
2015-05-04 16:45:43 +0200

    xxx

--- tasks/src/main/java/org/nextprot/api/tasks/GenerateSolrTerminologyIndex.java
+++ tasks/src/main/java/org/nextprot/api/tasks/GenerateSolrTerminologyIndex.java
@@ -3,6 +3,7 @@ package org.nextprot.api.tasks;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.nextprot.api.core.domain.Terminology;
 import org.nextprot.api.core.service.TerminologyService;
@@ -12,6 +13,7 @@ public class GenerateSolrTerminologyIndex {
 
 	public static void main(String[] args) throws SolrServerException, IOException {
 
+		Logger logger = Logger.getLogger(GenerateSolrTerminologyIndex.class);
 		System.setProperty("spring.profiles.active", "dev");
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/commons-context.xml",	"classpath:spring/core-context.xml");
 		TerminologyService terminologyService = ctx.getBean(TerminologyService.class);
@@ -20,19 +22,23 @@ public class GenerateSolrTerminologyIndex {
 		
 		SolrIndexer<Terminology> indexer = new CvTermSolrIndexer("http://localhost:8983/solr/npcvs1");
 		
-		// Remove previous indexes, find appropriate string for ontology-specific deletion (filters:ontologyname?)
+		// Remove previous indexes, TODO: find appropriate string for ontology-specific deletion (filters:ontologyname?)
+		logger.info("removing all solr terminology records");
 		indexer.deleteByQuery( "*:*" );
 		indexer.commit();
 		
 		List<Terminology> allterms;
 		if(args.length == 0) { // No arg: index all ontologies
 			System.err.println("indexing: all ontologies");
+			logger.info("indexing all terminologies");
 			allterms = terminologyService.findAllTerminology();
 		   }
 		else { // Index ontology given as argument
 			System.err.println("indexing: " + args[0]);
+			logger.info("indexing terminology: " + args[0]);
 			allterms = terminologyService.findTerminologyByOntology(args[0]);
 		   }
+		
 		for (Terminology t : allterms) {
 			indexer.add(t);
 			termcnt++;
@@ -42,7 +48,9 @@ public class GenerateSolrTerminologyIndex {
 		if(indexer.docs.size() > 0) // There are some prepared docs not yet sent to solr server
 			indexer.solrServer.add(indexer.docs);
 		
+		logger.info("comitting");
 		indexer.commit();
+		logger.info(termcnt + " terms indexed...");
 		System.err.println(termcnt + " terms indexed...");
 	}
 	
