96a61bc4367f008b83f894bd7ca3fba1e54b6f65
Alain-Gateau <alain.gateau@gmail.com>
2015-05-13 17:13:29 +0200

    added booleans for 'curated' and 'computed

--- core/src/main/resources/sql-queries/core/publication-by-title.sql
+++ core/src/main/resources/sql-queries/core/publication-by-title.sql
@@ -1,5 +1,7 @@
          select pubs.*, pubtypes.cv_name as pub_type, rp.property_value submission_database, 
   (select p.is_largescale from nextprot.view_paper_scale p where p.publication_id=pubs.resource_id) as is_largescale         
+  (select pv.annotated from nextprot.publication_view pv where pv.resource_id=pubs.resource_id limit 1) as is_curated,   
+  (select pv.computed from nextprot.publication_view pv where pv.resource_id=pubs.resource_id limit 1) as is_computed    
            from nextprot.publications pubs
      inner join  nextprot.cv_publication_types pubtypes on ( pubs.cv_publication_type_id = pubtypes.cv_id)
 left outer join nextprot.resource_properties rp on (pubs.resource_id = rp.resource_id and rp.property_name = 'database') 
