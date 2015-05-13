de0bf83a544f99afcea6a1118b4ee66e8308258d
Alain-Gateau <alain.gateau@gmail.com>
2015-05-13 17:16:02 +0200

    solr indexer for publications

---
+++ tasks/src/main/java/org/nextprot/api/tasks/GenerateSolrPublicationIndex.java
@@ -0,0 +1,60 @@
+package org.nextprot.api.tasks;
+
+import java.util.List;
+
+import org.nextprot.api.core.domain.Publication;
+import org.nextprot.api.core.service.PublicationService;
+
+public class GenerateSolrPublicationIndex extends GenerateSolrIndex {
+
+	
+	public static void main(String[] args) {
+		GenerateSolrPublicationIndex i = new GenerateSolrPublicationIndex();
+		i.launch(args);
+	}	    
+
+	@Override
+	public void start(String[] args) {
+
+		PublicationService publicationService = getBean(PublicationService.class);
+		
+		int pubcnt = 0, artcnt = 0, submcnt = 0, othercnt = 0;
+		
+		SolrIndexer<Publication> indexer = new PublicationSolrindexer("http://localhost:8983/solr/nppublications1");
+		
+		// Remove previous indexes
+		logger.info("removing all solr publication records");
+		indexer.clearDatabase();
+		
+		List<Long> allpubids;
+		
+		System.err.println("getting all publications from API");
+		logger.info("getting all publications from API");
+		long start = System.currentTimeMillis();
+		allpubids = publicationService.findAllPublicationIds();
+		System.err.println("indexing " + allpubids.size() +  " publications...");
+		logger.info("indexing " + allpubids.size() +  " publications...");
+		for (Long id : allpubids) {
+			//System.err.println("id: " + id);
+			Publication currpub = publicationService.findPublicationById(id);
+			indexer.add(currpub);
+			pubcnt++;
+			if(currpub.getPublicationType().equals("ARTICLE")) artcnt++;
+			else if(currpub.getPublicationType().equals("SUBMISSION")) submcnt++;
+			else othercnt++;
+			if(pubcnt >= 2000) break;
+		}
+		
+		System.err.println("got all publications" + (System.currentTimeMillis()-start)/1000 + " seconds...");
+
+		indexer.addRemaing();
+		
+		logger.info("comitting");
+		indexer.commit();
+		logger.info(pubcnt + " publications indexed..." + (System.currentTimeMillis()-start)/1000 + " seconds...");
+		System.err.println(pubcnt + " publications indexed: " + artcnt + " articles, " + submcnt + " submissions, " + othercnt + " other.");
+		System.err.println("All this in " + (System.currentTimeMillis()-start)/1000 + " seconds...");
+		
+	}
+	
+}
---
+++ tasks/src/main/java/org/nextprot/api/tasks/PublicationSolrindexer.java
@@ -0,0 +1,69 @@
+package org.nextprot.api.tasks;
+
+//import java.util.List;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.SortedSet;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.nextprot.api.core.domain.CvJournal;
+import org.nextprot.api.core.domain.DbXref;
+import org.nextprot.api.core.domain.Publication;
+import org.nextprot.api.core.domain.PublicationAuthor;
+import org.nextprot.api.core.utils.TerminologyUtils;
+
+public class PublicationSolrindexer extends SolrIndexer<Publication>{
+
+
+	public PublicationSolrindexer(String url) {
+		super(url);
+	}
+
+	@Override
+	public SolrInputDocument convertToSolrDocument(Publication publi) {
+		SolrInputDocument doc = new SolrInputDocument();
+		doc.addField("id", publi.getPublicationId());
+		Set<DbXref> xrefs = publi.getDbXrefs();
+		// TODO: this 'ac' field should be renamed 'xrefs'
+		if (xrefs != null) { doc.addField("ac",TerminologyUtils.convertXrefsToSolrString(new ArrayList<DbXref>(xrefs))); }
+		String filters="";
+		filters+=((publi.getIsLargeScale())?" largescale":"");
+		filters+=((publi.getIsCurated())?" curated":"");
+		filters+=((publi.getIsComputed())?" computed":"");
+		doc.addField("filters", filters);
+		doc.addField("title", publi.getTitle());
+		doc.addField("title_s", publi.getTitle());
+		doc.addField("date", publi.getPublicationDate());
+		doc.addField("year", publi.getPublicationYear());
+		doc.addField("first_page", publi.getFirstPage());
+		doc.addField("last_page", publi.getLastPage());
+		doc.addField("volume", publi.getVolume());
+		doc.addField("abstract", publi.getAbstractText());
+		doc.addField("type", publi.getPublicationType());
+		//doc.addField("source", rs.getString("source"));
+		CvJournal journal = publi.getCvJournal();
+		if(journal != null) { // TODO: rename "pretty_journal" to "abbrev_journal"
+			String jfield = journal.getName();
+			String jabbrev = journal.getAbbrev();
+			if(!jfield.equals(jabbrev)) jfield += " " + jabbrev;
+			doc.addField("journal", jfield); 
+			doc.addField("pretty_journal", jabbrev); 
+		}
+
+		SortedSet<PublicationAuthor> authorset = publi.getAuthors();
+		if (authorset != null) {
+			int i = authorset.size();
+			StringBuilder sb = new StringBuilder();
+			for (PublicationAuthor author : authorset) {
+				doc.addField("authors",author.getLastName() + " " + author.getForeName());
+				sb.append(author.getLastName() + " ");
+				sb.append(author.getInitials().replaceAll("(.)", "$1\\.")); // add a '.' after each initial
+				if (--i != 0) sb.append(" | ");
+				}
+			doc.addField("pretty_authors",sb.toString()); // for display only
+		}
+		
+		return doc;
+	}
+}
