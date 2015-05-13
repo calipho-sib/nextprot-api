7909b0ea460127b785c063b561967c6d95a6a6e5
Alain-Gateau <alain.gateau@gmail.com>
2015-05-13 17:14:47 +0200

    new generic superclass for solr indexers

---
+++ tasks/src/main/java/org/nextprot/api/tasks/GenerateSolrIndex.java
@@ -0,0 +1,46 @@
+package org.nextprot.api.tasks;
+
+import org.apache.log4j.Logger;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ * @ Generic Solr indexer for the three cores (Annotation, Publications and Terminology
+ */
+public abstract class GenerateSolrIndex {
+
+	private CacheManager cacheManager = null;
+	private ClassPathXmlApplicationContext ctx = null;
+	protected Logger logger = Logger.getLogger(GenerateSolrIndex.class);
+
+	protected void launch(String[] args) {
+		try {
+
+			System.setProperty("spring.profiles.active", "dev, cache");
+			ctx = new ClassPathXmlApplicationContext(
+					"classpath:spring/commons-context.xml",
+					"classpath:spring/core-context.xml");
+			cacheManager = ctx.getBean(CacheManager.class);
+
+			start(args);
+
+		} finally {
+			shutdown();
+		}
+	}
+
+	public abstract void start(String[] args);
+	
+	protected void shutdown() {
+		if(cacheManager != null){
+			((EhCacheCacheManager) cacheManager).getCacheManager().shutdown();
+		}
+	}
+	
+	protected <T> T getBean(Class<T> requiredType) {
+		return ctx.getBean(requiredType);
+	}
+
+}
