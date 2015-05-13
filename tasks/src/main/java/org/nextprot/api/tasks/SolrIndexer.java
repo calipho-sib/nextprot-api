34b27b662fcb3783acc923fc2697a68ea78f7f09
Alain-Gateau <alain.gateau@gmail.com>
2015-05-13 17:31:14 +0200

    restructuration

--- tasks/src/main/java/org/nextprot/api/tasks/SolrIndexer.java
+++ tasks/src/main/java/org/nextprot/api/tasks/SolrIndexer.java
@@ -7,40 +7,76 @@ import java.util.List;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.common.SolrInputDocument;
-
+import org.nextprot.api.commons.exception.NextProtException;
 
 abstract class SolrIndexer<T> {
-	
-	protected HttpSolrServer solrServer;
+
+	private HttpSolrServer solrServer;
 	protected List<SolrInputDocument> docs;
-	
+
 	private static final int BATCH_SIZE = 200;
-	
+
 	public SolrIndexer(String url) {
-		this.solrServer =  new HttpSolrServer(url);
-		this.docs = new ArrayList<SolrInputDocument>();		
+		this.solrServer = new HttpSolrServer(url);
+		this.docs = new ArrayList<SolrInputDocument>();
 	}
-	
+
 	public abstract SolrInputDocument convertToSolrDocument(T documentTypes);
 
-	public void add(T t) throws SolrServerException, IOException {
-		docs.add(this.convertToSolrDocument(t));
-		
-		if (docs.size() % BATCH_SIZE == 0) {
-			//System.err.println("sent " +docs.size() + " docs to solr so far");
-			this.solrServer.add(docs);
-			docs.clear();
+	public void add(T t) {
+
+		try {
+
+			docs.add(this.convertToSolrDocument(t));
+
+			if (docs.size() % BATCH_SIZE == 0) {
+				// System.err.println("sent " +docs.size() + " docs to solr so far");
+				this.solrServer.add(docs);
+				docs.clear();
+
+			}
+
+		} catch (SolrServerException | IOException e) {
+			throw new NextProtException(e);
 		}
 
 	}
-	
-	public void commit() throws SolrServerException, IOException{
-		solrServer.commit();
-		solrServer.optimize();
+
+	public void addRemaing() {
+		try {
+
+			if (docs.size() > 0)
+			// There are some prepared docs not yet sent to solr server
+			{
+				solrServer.add(docs);
+				docs.clear();
+			}
+
+		} catch (SolrServerException | IOException e) {
+			throw new NextProtException(e);
+		}
+
 	}
 
-	public void deleteByQuery(String dstring) throws SolrServerException, IOException {
-		solrServer.deleteByQuery(dstring);
+	public void commit() {
+		try {
+
+			solrServer.commit();
+			solrServer.optimize();
+		} catch (SolrServerException | IOException e) {
+			throw new NextProtException(e);
+		}
+
+	}
+
+	public void clearDatabase() {
+		try {
+			solrServer.deleteByQuery("*:*");
+			solrServer.commit();
+			solrServer.optimize();
+		} catch (SolrServerException | IOException e) {
+			throw new NextProtException(e);
+		}
 	}
 
 }
