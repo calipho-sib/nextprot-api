fdf46fffb4322f16a7a21237d0119d5fcb20c0ac
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2015-06-26 18:20:15 +0200

    added expression level as a property of evidences CALIPHOMISC-343

--- core/src/main/java/org/nextprot/api/core/domain/annotation/AnnotationEvidence.java
+++ core/src/main/java/org/nextprot/api/core/domain/annotation/AnnotationEvidence.java
@@ -1,9 +1,15 @@
 package org.nextprot.api.core.domain.annotation;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import com.mchange.v2.codegen.bean.PropertyMapConstructorGeneratorExtension;
 
 public class AnnotationEvidence implements Serializable {
 
@@ -299,6 +305,32 @@ public class AnnotationEvidence implements Serializable {
 	}
 
 	/**
+	 * 
+	 * @return a set of property names related to the evidence and that are allowed to be shown
+	 */
+	public Set<String> getPropertiesNames() {
+		// do an intersection between properties we want to show and properties we have
+		Set<String> propsOk = new HashSet<String>(Arrays.asList("expressionLevel","antibodies acc"));
+		propsOk.retainAll(propertiesMap.keySet());
+		// return the intersection
+		return propsOk;
+	}
+	
+	/**
+	 * 
+	 * @param name a proprty name
+	 * @return a string representing the property value associated to name
+	 */
+	public String getPropertyValue(String name) {
+		// special cases first
+		if ("expressionLevel".equals(name)) return getExpressionLevel();
+		if ("integrationLevel".equals(name)) return getIntegrationLevel();
+		//...
+		// general case finally
+		return propertiesMap.get(name);
+	}
+	
+	/**
 	 * deploy properties,
 	 * 
 	 * select distinct property_value from
@@ -306,6 +338,7 @@ public class AnnotationEvidence implements Serializable {
 	 * 'integrationLevel' order by property_value
 	 */
 	private String extractProperty(String propertyName) {
+		
 		return propertiesMap.get(propertyName);
 
 	}
