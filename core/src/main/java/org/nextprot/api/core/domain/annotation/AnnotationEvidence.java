bc5308f8a85a9b8e15cc85ec32c0a7a8ba1c4bc9
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2015-06-26 18:21:58 +0200

    added expression level as a property of evidences CALIPHOMISC-343 (cosmetic)

--- core/src/main/java/org/nextprot/api/core/domain/annotation/AnnotationEvidence.java
+++ core/src/main/java/org/nextprot/api/core/domain/annotation/AnnotationEvidence.java
@@ -2,14 +2,11 @@ package org.nextprot.api.core.domain.annotation;
 
 import java.io.Serializable;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import com.mchange.v2.codegen.bean.PropertyMapConstructorGeneratorExtension;
 
 public class AnnotationEvidence implements Serializable {
 
