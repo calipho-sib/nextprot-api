e34e418a4ef14a3ed4eff2c1671755527600419e
Alain-Gateau <alain.gateau@gmail.com>
2015-07-01 12:03:06 +0200

    added Entryproperties setting

--- core/src/main/java/org/nextprot/api/core/service/fluent/FluentEntryService.java
+++ core/src/main/java/org/nextprot/api/core/service/fluent/FluentEntryService.java
@@ -52,7 +52,10 @@ public class FluentEntryService {
 	private InteractionService interactionService;
 	@Autowired
 	private ExperimentalContextService ecService;
-
+	
+	@Autowired
+	private EntryPropertiesService entryPropertiesService;
+	
 	public FluentEntry newFluentEntry(String entryName) {
 		return new FluentEntry(entryName);
 	}
@@ -64,6 +67,8 @@ public class FluentEntryService {
 		public FluentEntry(String entryName) {
 			this.entryName = entryName;
 			this.entry = new Entry(entryName);
+			this.entry.setProperties(entryPropertiesService.findEntryProperties(entryName));
+
 		}
 
 		public FluentEntry withOverview() {
