d2e06faf04565996fc8a403576c253861543289e
fnikitin <frederic.nikitin@gmail.com>
2015-07-09 14:41:04 +0200

    New amino acid code enumeration

---
+++ commons/src/test/java/org/nextprot/api/commons/bio/AminoAcidCodeTest.java
@@ -0,0 +1,46 @@
+package org.nextprot.api.commons.bio;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by fnikitin on 09/07/15.
+ */
+public class AminoAcidCodeTest {
+
+    @Test
+    public void testValueOfCode1AAFound() throws Exception {
+
+        Assert.assertEquals(AminoAcidCode.Alanine, AminoAcidCode.valueOfCode1AA('A'));
+    }
+
+    @Test
+    public void testGetAACode1() throws Exception {
+
+        Assert.assertEquals('A', AminoAcidCode.valueOfCode1AA('A').get1LetterCode());
+    }
+
+    @Test
+    public void testGetAACode3() throws Exception {
+
+        Assert.assertEquals("Ala", AminoAcidCode.valueOfCode1AA('A').get3LetterCode());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testValueOfCode1AANotFound() throws Exception {
+
+        AminoAcidCode.valueOfCode1AA('a');
+    }
+
+    @Test
+     public void testGetStopCode1() throws Exception {
+
+        Assert.assertEquals('*', AminoAcidCode.valueOfCode1AA('*').get1LetterCode());
+    }
+
+    @Test
+    public void testGetStopCode3() throws Exception {
+
+        Assert.assertEquals("Ter", AminoAcidCode.valueOfCode1AA('*').get3LetterCode());
+    }
+}
