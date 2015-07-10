a95abde367182f383e9cbbcab0e2729ef267ccf3
fnikitin <frederic.nikitin@gmail.com>
2015-07-10 11:55:28 +0200

    A new interface to parse and format ProteinMutations

---
+++ commons/src/main/java/org/nextprot/api/commons/bio/mutation/ProteinMutationFormat.java
@@ -0,0 +1,30 @@
+package org.nextprot.api.commons.bio.mutation;
+
+import java.text.ParseException;
+
+/**
+ * <code>ProteinMutationFormat</code> provides the interface for formatting and parsing
+ * ProteinMutation.
+ *
+ * Created by fnikitin on 10/07/15.
+ */
+public interface ProteinMutationFormat {
+
+    /**
+     * Formats a <code>ProteinMutation</code>.
+     *
+     * @param mutation the mutation to format
+     * @return a formatter <code>String</>
+     */
+    String format(ProteinMutation mutation);
+
+    /**
+     * Parses text from the beginning of the given string to produce a ProteinMutation.
+     *
+     * @param source A <code>String</code> whose beginning should be parsed.
+     * @return A <code>ProteinMutation</code> parsed from the string.
+     * @exception ParseException if the beginning of the specified string
+     *            cannot be parsed.
+     */
+    ProteinMutation parse(String source);
+}
