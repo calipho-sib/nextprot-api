02712584286aed2cccd1dd088ad0f9402632393f
fnikitin <frederic.nikitin@gmail.com>
2015-07-31 18:47:28 +0200

    BioPhyChemPropsDao changes return type, update DaoImpl

--- core/src/main/resources/sql-queries/core/biophychem-by-entry.sql
+++ core/src/main/resources/sql-queries/core/biophychem-by-entry.sql
@@ -1,4 +1,4 @@
-select distinct cv.display_name, ip.property_value
+select distinct cv.display_name, ip.property_value, ip.identifier_property_id
 from nextprot.identifier_properties ip 
 inner join nextprot.cv_property_names cv on ip.cv_property_name_id = cv.cv_id 
 inner join nextprot.sequence_identifiers si on ip.identifier_id = si.identifier_id 
