fe75a4a2b4cd841e90465564e682e2e6c77ca94c
fnikitin <frederic.nikitin@gmail.com>
2015-09-14 15:35:33 +0200

    BioComplex now extends BioList

--- core/src/main/java/org/nextprot/api/core/domain/BioComplex.java
+++ core/src/main/java/org/nextprot/api/core/domain/BioComplex.java
@@ -1,59 +1,16 @@
 package org.nextprot.api.core.domain;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 /**
- * A group of BioObjects is still a BioObject
+ * A complex of biological objects
  *
  * Created by fnikitin on 26/08/15.
  */
-public class BioComplex extends BioObject<List<BioObject<?>>> {
+public class BioComplex extends BioList {
 
-    private static final long serialVersionUID = 0L;
-
-    private final List<BioObject<?>> bioObjects;
+    private static final long serialVersionUID = 1L;
 
     protected BioComplex(BioObject<?> bioObject, BioObject<?>... others) {
 
-        super(BioType.COMPLEX, deduceResourceType(bioObject, others));
-
-        this.bioObjects = new ArrayList<>();
-
-        populateBioObjects(bioObject, others);
-    }
-
-    private void populateBioObjects(BioObject<?> bioObject, BioObject<?>... others) {
-
-        this.bioObjects.add(bioObject);
-        for (BioObject<?> bo : others) {
-
-            this.bioObjects.add(bo);
-        }
-    }
-
-    private static ResourceType deduceResourceType(BioObject<?> bioObject, BioObject<?>... others) {
-
-        Set<ResourceType> refs = new HashSet<>();
-
-        refs.add(bioObject.getResourceType());
-
-        for (BioObject<?> bo : others) {
-
-            refs.add(bo.getResourceType());
-        }
-
-        return (refs.size() == 1) ? refs.iterator().next() : ResourceType.MIXED;
-    }
-
-    public int size() {
-        return bioObjects.size();
-    }
-
-    @Override
-    public List<BioObject<?>> getContent() {
-        return bioObjects;
+        super(BioType.COMPLEX, bioObject, others);
     }
 }
--- core/src/test/java/org/nextprot/api/core/domain/BioListTest.java
+++ core/src/test/java/org/nextprot/api/core/domain/BioListTest.java
@@ -6,15 +6,25 @@ import org.junit.Test;
 /**
  * Created by fnikitin on 26/08/15.
  */
-public class BioComplexTest {
+public class BioListTest {
 
     @Test
-    public void test() {
+    public void testComplex() {
 
         BioComplex bioComplex = new BioComplex(new BioIsoform(), new BioIsoform(), new BioObjectExternal(BioObject.BioType.CHEMICAL));
 
         Assert.assertEquals(3, bioComplex.size());
         Assert.assertEquals(BioObject.BioType.COMPLEX, bioComplex.getBioType());
         Assert.assertEquals(BioObject.ResourceType.MIXED, bioComplex.getResourceType());
+    }
+
+    @Test
+    public void testGroup() {
+
+        BioGroup bioGroup = new BioGroup(new BioIsoform(), new BioIsoform(), new BioObjectExternal(BioObject.BioType.CHEMICAL));
+
+        Assert.assertEquals(3, bioGroup.size());
+        Assert.assertEquals(BioObject.BioType.GROUP, bioGroup.getBioType());
+        Assert.assertEquals(BioObject.ResourceType.MIXED, bioGroup.getResourceType());
     }
 }
