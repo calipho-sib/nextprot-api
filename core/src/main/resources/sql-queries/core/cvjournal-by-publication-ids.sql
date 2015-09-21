b4950d1699a8dbce2b93fd6c92d7d6a14a7929ef
Alain-Gateau <alain.gateau@gmail.com>
2015-09-21 16:46:35 +0200

    added NLMid

--- core/src/main/resources/sql-queries/core/cvjournal-by-publication-ids.sql
+++ core/src/main/resources/sql-queries/core/cvjournal-by-publication-ids.sql
@@ -1,3 +1,3 @@
-SELECT cv_journals.cv_id, cv_journals.journal_name, cv_journals.iso_abbrev, publications.resource_id publication_id 
+SELECT cv_journals.cv_id, cv_journals.journal_name, cv_journals.iso_abbrev, cv_journals.nlmid, publications.resource_id publication_id 
 FROM nextprot.cv_journals, nextprot.publications 
 WHERE cv_journals.cv_id = publications.cv_journal_id and publications.resource_id in (:publicationIds)
