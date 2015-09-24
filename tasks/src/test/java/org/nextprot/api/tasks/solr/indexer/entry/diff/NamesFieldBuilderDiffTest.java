6afb90d57059fbe374ce55dc69b6df7eb5b3b8ef
Alain-Gateau <alain.gateau@gmail.com>
2015-09-24 17:17:24 +0200

    progress

---
+++ tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/NamesFieldBuilderDiffTest.java
@@ -0,0 +1,58 @@
+package org.nextprot.api.tasks.solr.indexer.entry.diff;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.nextprot.api.core.domain.Entry;
+import org.nextprot.api.solr.index.EntryIndex.Fields;
+import org.nextprot.api.tasks.solr.indexer.entry.SolrDiffTest;
+import org.nextprot.api.tasks.solr.indexer.entry.impl.NamesFieldBuilder;
+
+public class NamesFieldBuilderDiffTest extends SolrDiffTest {
+
+	@Test
+	public void testNames() {
+
+		
+		 for(int i=0; i < 10; i++){ testNames(getEntry(i)); }
+		 
+
+		//Entry entry = getEntry("NX_Q96I99");
+		//testNames(entry);
+
+	}
+
+	public void testNames(Entry entry) {
+		String entryName = entry.getUniqueName();
+
+		System.out.println("Testing: " + entryName);
+		NamesFieldBuilder nfb = new NamesFieldBuilder();
+		nfb.initializeBuilder(entry);
+		
+		List<String> altnamelist = (List) getValueForFieldInCurrentSolrImplementation(entryName, Fields.ALTERNATIVE_NAMES);
+		Set<String> expectedAltnames = null;
+		Set<String> AltenameSet = null;
+		if(altnamelist != null)  {
+			expectedAltnames = new TreeSet<String>(altnamelist);
+			AltenameSet = new TreeSet<String>(nfb.getFieldValue(Fields.ALTERNATIVE_NAMES, List.class));
+			//System.err.println(expectedAltnames);
+			//System.err.println(AltenameSet);
+			Assert.assertEquals(expectedAltnames.size(), AltenameSet.size());
+		}
+
+		List<String> altgenlist = (List) getValueForFieldInCurrentSolrImplementation(entryName, Fields.ALTERNATIVE_GENE_NAMES);
+		Set<String> expectedAltGenename = null;
+		Set<String> AltGenenameSet = null;
+		if(altgenlist != null)  {
+			expectedAltGenename = new TreeSet<String>(altgenlist);
+			AltGenenameSet = new TreeSet<String>(nfb.getFieldValue(Fields.ALTERNATIVE_GENE_NAMES, List.class));
+			//System.err.println(expectedAltGenename);
+			//System.err.println(AltGenenameSet);
+			Assert.assertEquals(expectedAltGenename.size(), AltGenenameSet.size());
+		}
+	}
+
+}
