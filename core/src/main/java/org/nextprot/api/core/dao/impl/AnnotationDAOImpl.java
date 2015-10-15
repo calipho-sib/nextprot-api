d5536894dde43862507a01a4471b472023cade55
fnikitin <frederic.nikitin@gmail.com>
2015-10-15 17:23:02 +0200

    Get Integer objects and setFirst and Last positions in AnnotationIsoformRowMapper

--- core/src/main/java/org/nextprot/api/core/dao/impl/AnnotationDAOImpl.java
+++ core/src/main/java/org/nextprot/api/core/dao/impl/AnnotationDAOImpl.java
@@ -107,8 +107,8 @@ public class AnnotationDAOImpl implements AnnotationDAO {
 
 			AnnotationIsoformSpecificity annotation = new AnnotationIsoformSpecificity();
 			annotation.setAnnotationId(resultSet.getLong("annotation_id"));
-			annotation.setFirstPosition(resultSet.getInt("first_pos"));  // the SQL increments first_pos by 1
-			annotation.setLastPosition(resultSet.getInt("last_pos"));
+			annotation.setFirstPosition((Integer)resultSet.getObject("first_pos"));  // the SQL increments first_pos by 1
+			annotation.setLastPosition((Integer)resultSet.getObject("last_pos"));
 			annotation.setIsoformName(resultSet.getString("unique_name"));
 			annotation.setSpecificity(resultSet.getString("iso_specificity"));
 			return annotation;
