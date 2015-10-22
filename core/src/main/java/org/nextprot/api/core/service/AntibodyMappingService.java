a46b9eedf94c90354fd0d14e4b7ac96aa2ef0142
fnikitin <frederic.nikitin@gmail.com>
2015-10-22 17:24:26 +0200

    Removing deprecated service method in AntibodyMappingService

--- core/src/main/java/org/nextprot/api/core/service/AntibodyMappingService.java
+++ core/src/main/java/org/nextprot/api/core/service/AntibodyMappingService.java
@@ -1,14 +1,12 @@
 package org.nextprot.api.core.service;
 
-import java.util.List;
-
-import org.nextprot.api.core.domain.AntibodyMapping;
 import org.nextprot.api.core.domain.annotation.Annotation;
 import org.nextprot.api.core.service.annotation.ValidEntry;
 
+import java.util.List;
+
 public interface AntibodyMappingService {
 
-	List<AntibodyMapping> findAntibodyMappingByUniqueName(@ValidEntry String uniqueName);
 	List<Annotation> findAntibodyMappingAnnotationsByUniqueName(@ValidEntry String uniqueName);
 
 }
