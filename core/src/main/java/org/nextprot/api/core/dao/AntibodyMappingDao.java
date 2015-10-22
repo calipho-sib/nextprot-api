4de9f3c5edd99dd5e33921b878a976bba5f4b835
fnikitin <frederic.nikitin@gmail.com>
2015-10-22 17:23:42 +0200

    Removing deprecated method in Dao

--- core/src/main/java/org/nextprot/api/core/dao/AntibodyMappingDao.java
+++ core/src/main/java/org/nextprot/api/core/dao/AntibodyMappingDao.java
@@ -1,18 +1,10 @@
 package org.nextprot.api.core.dao;
 
-import java.util.List;
-
-import org.nextprot.api.core.domain.AntibodyMapping;
 import org.nextprot.api.core.domain.annotation.Annotation;
+
+import java.util.List;
 
 public interface AntibodyMappingDao {
 
-	/**
-	 * Retrieves the mapping of isoforms and antibodies 
-	 * @param id
-	 * @return
-	 */
-	List<AntibodyMapping> findAntibodiesById(Long id);
-
-	List<Annotation> findAntibodyMappingAnnotationsById(Long masterId);
+	List<Annotation> findAntibodyMappingAnnotationsById(long masterId);
 }
--- core/src/main/java/org/nextprot/api/core/dao/impl/AntibodyMappingDaoImpl.java
+++ core/src/main/java/org/nextprot/api/core/dao/impl/AntibodyMappingDaoImpl.java
@@ -4,7 +4,6 @@ import org.nextprot.api.commons.constants.AnnotationCategory;
 import org.nextprot.api.commons.spring.jdbc.DataSourceServiceLocator;
 import org.nextprot.api.commons.utils.SQLDictionary;
 import org.nextprot.api.core.dao.AntibodyMappingDao;
-import org.nextprot.api.core.domain.AntibodyMapping;
 import org.nextprot.api.core.domain.annotation.Annotation;
 import org.nextprot.api.core.domain.annotation.AnnotationEvidence;
 import org.nextprot.api.core.domain.annotation.AnnotationIsoformSpecificity;
@@ -23,50 +22,10 @@ import java.util.*;
 public class AntibodyMappingDaoImpl implements AntibodyMappingDao {
 
 	@Autowired private SQLDictionary sqlDictionary;
-
 	@Autowired private DataSourceServiceLocator dsLocator;
-	
-	@Override
-	public List<AntibodyMapping> findAntibodiesById(Long id) {
-		
-		SqlParameterSource namedParams = new MapSqlParameterSource("id", id);
-
-		// step 1 - one object per each antibody - isoform - position
-		List<AntibodyMapping> flatmaps =  new NamedParameterJdbcTemplate(dsLocator.getDataSource()).query(sqlDictionary.getSQLQuery("antibodies-by-id"), namedParams, new RowMapper<AntibodyMapping>() {
-			@Override
-			public AntibodyMapping mapRow(ResultSet resultSet, int row) throws SQLException {
-
-				AntibodyMapping antibodyMapping = new AntibodyMapping();
-				antibodyMapping.setXrefId(resultSet.getLong("db_xref_id"));
-				antibodyMapping.setAntibodyUniqueName(resultSet.getString("antibody_unique_name"));
-				antibodyMapping.setAssignedBy(resultSet.getString("antibody_src"));
-
-				AnnotationIsoformSpecificity isoformSpecificity = new AnnotationIsoformSpecificity();
-				isoformSpecificity.setIsoformName(resultSet.getString("iso_unique_name"));
-
-				isoformSpecificity.setFirstPosition(resultSet.getInt("first_pos"));
-				isoformSpecificity.setLastPosition(resultSet.getInt("last_pos"));
-
-				antibodyMapping.addIsoformSpecificity(isoformSpecificity);
-				return antibodyMapping;
-			}
-		});
-		// step 2 - one object per antibody with nested isoform specs, spec with nested map positions
-		Map<String,AntibodyMapping> mergedmap = new HashMap<>();
-		for (AntibodyMapping map : flatmaps) {
-			String ab = map.getAntibodyUniqueName();
-			if (!mergedmap.containsKey(ab)) {
-				mergedmap.put(ab, map);
-			} else {
-				AntibodyMapping mapIn = mergedmap.get(ab);
-				mapIn.addIsoformSpecificity(map.getFirstIsoformSpecificity());
-			}
-		}
-		return new ArrayList<>(mergedmap.values());
-	}
 
 	@Override
-	public List<Annotation> findAntibodyMappingAnnotationsById(Long masterId) {
+	public List<Annotation> findAntibodyMappingAnnotationsById(long masterId) {
 		
 		SqlParameterSource namedParams = new MapSqlParameterSource("id", masterId);
 
@@ -127,9 +86,5 @@ public class AntibodyMappingDaoImpl implements AntibodyMappingDao {
 		public List<Annotation> getAnnotations() {
 			return new ArrayList<>(annotationsMap.values());
 		}
-		
 	}
-	
-	
-	
 }
