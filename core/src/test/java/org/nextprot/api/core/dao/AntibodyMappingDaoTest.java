51406af34208b524f88927bbc5c9bf9c3c4c1dc2
fnikitin <frederic.nikitin@gmail.com>
2015-10-27 17:03:53 +0100

    Finalizing tests, updating dataset xml

--- core/src/test/java/org/nextprot/api/core/dao/AntibodyMappingDaoTest.java
+++ core/src/test/java/org/nextprot/api/core/dao/AntibodyMappingDaoTest.java
@@ -15,25 +15,27 @@ import java.util.List;
 @DatabaseSetup(value = "AntibodyMappingDaoTest.xml", type = DatabaseOperation.INSERT)
 @TransactionConfiguration(defaultRollback = true)
 public class AntibodyMappingDaoTest extends CoreUnitBaseTest {
-	
-	@Autowired private AntibodyMappingDao antibodyMappingDao;
-	
+
+	@Autowired
+	private AntibodyMappingDao antibodyMappingDao;
+
 	@Test
 	public void testFindAntibodiesById() {
-		List<Annotation> annotations = this.antibodyMappingDao.findAntibodyMappingAnnotationsById(636535);
+		List<Annotation> annotations = this.antibodyMappingDao.findAntibodyMappingAnnotationsById(636535L);
 		Assert.assertEquals(2, annotations.size());
 
 		Annotation annotation = annotations.get(0);
 
 		Assert.assertEquals(AnnotationCategory.ANTIBODY_MAPPING, annotation.getAPICategory());
+		Assert.assertEquals("AN_HPA036302_4049518", annotation.getUniqueName());
 		Assert.assertEquals("GOLD", annotation.getQualityQualifier());
 		Assert.assertEquals(4049518, annotation.getAnnotationId());
 		Assert.assertEquals(1, annotation.getEvidences().size());
-		// TODO: see with pam here
 		Assert.assertEquals(4049518, annotation.getEvidences().get(0).getAnnotationId());
 		Assert.assertEquals(17201575, annotation.getEvidences().get(0).getResourceId());
 		Assert.assertEquals("database", annotation.getEvidences().get(0).getResourceType());
 		Assert.assertEquals("Human protein atlas", annotation.getEvidences().get(0).getAssignedBy());
+		Assert.assertEquals("HPA", annotation.getEvidences().get(0).getResourceDb());
 		Assert.assertEquals("evidence", annotation.getEvidences().get(0).getResourceAssociationType());
 		Assert.assertEquals("ECO:0000154", annotation.getEvidences().get(0).getEvidenceCodeAC());
 		Assert.assertTrue(!annotation.getEvidences().get(0).isNegativeEvidence());
@@ -49,13 +51,14 @@ public class AntibodyMappingDaoTest extends CoreUnitBaseTest {
 		annotation = annotations.get(1);
 
 		Assert.assertEquals(AnnotationCategory.ANTIBODY_MAPPING, annotation.getAPICategory());
+		Assert.assertEquals("AN_HPA036303_9547085", annotation.getUniqueName());
 		Assert.assertEquals("GOLD", annotation.getQualityQualifier());
 		Assert.assertEquals(9547085, annotation.getAnnotationId());
 		Assert.assertEquals(1, annotation.getEvidences().size());
-		// TODO: see with pam here
 		Assert.assertEquals(9547085, annotation.getEvidences().get(0).getAnnotationId());
 		Assert.assertEquals(39235676, annotation.getEvidences().get(0).getResourceId());
 		Assert.assertEquals("database", annotation.getEvidences().get(0).getResourceType());
+		Assert.assertEquals("HPA", annotation.getEvidences().get(0).getResourceDb());
 		Assert.assertEquals("Human protein atlas", annotation.getEvidences().get(0).getAssignedBy());
 		Assert.assertEquals("evidence", annotation.getEvidences().get(0).getResourceAssociationType());
 		Assert.assertEquals("ECO:0000154", annotation.getEvidences().get(0).getEvidenceCodeAC());
