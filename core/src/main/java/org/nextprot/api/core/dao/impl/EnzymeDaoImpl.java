5f442711b3b34034266bbbc0b2b90fdff7d9d108
fnikitin <frederic.nikitin@gmail.com>
2015-11-03 16:54:26 +0100

    Set missing ontology in enzymeDaoImpl

--- core/src/main/java/org/nextprot/api/core/dao/impl/EnzymeDaoImpl.java
+++ core/src/main/java/org/nextprot/api/core/dao/impl/EnzymeDaoImpl.java
@@ -1,11 +1,5 @@
 package org.nextprot.api.core.dao.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.nextprot.api.commons.spring.jdbc.DataSourceServiceLocator;
 import org.nextprot.api.commons.utils.SQLDictionary;
 import org.nextprot.api.core.dao.EnzymeDao;
@@ -14,6 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class EnzymeDaoImpl implements EnzymeDao {
@@ -24,7 +24,8 @@ public class EnzymeDaoImpl implements EnzymeDao {
 	
 	@Override
 	public List<Terminology> findEnzymeByMaster(String uniqueName) {
-		Map<String, Object> params = new HashMap<String, Object>();
+
+		Map<String, Object> params = new HashMap<>();
 		params.put("uniqueName", uniqueName);
 		
 		return new NamedParameterJdbcTemplate(dsLocator.getDataSource()).query(sqlDictionary.getSQLQuery("enzyme-by-entry-name"), params, new EnzymeRowMapper());
@@ -34,9 +35,13 @@ public class EnzymeDaoImpl implements EnzymeDao {
 
 		@Override
 		public Terminology mapRow(ResultSet resultSet, int row) throws SQLException {
+
 			Terminology kw = new Terminology();
+
+			kw.setOntology(resultSet.getString("ontology"));
 			kw.setAccession(resultSet.getString("accession"));
 			kw.setName(resultSet.getString("name"));
+
 			return kw;
 		}
 		
