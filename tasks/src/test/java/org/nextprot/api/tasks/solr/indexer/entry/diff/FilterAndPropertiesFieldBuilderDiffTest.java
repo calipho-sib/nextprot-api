cf09dd9878a6163502b13cb037fa976050815b95
Alain-Gateau <alain.gateau@gmail.com>
2015-11-05 16:48:27 +0100

    progress

--- tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/FilterAndPropertiesFieldBuilderDiffTest.java
+++ tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/FilterAndPropertiesFieldBuilderDiffTest.java
@@ -15,17 +15,17 @@ public class FilterAndPropertiesFieldBuilderDiffTest extends SolrDiffTest {
 	public void testFilterAndProperties() {
 
 		
-		 //for(int i=0; i < 10; i++){ testFilterAndProperties(getEntry(i)); }
+		 for(int i=0; i < 10; i++){ testFilterAndProperties(getEntry(i)); }
 		 
 
-		Entry entry = getEntry("NX_Q96I99");
-		testFilterAndProperties(entry);
+		//Entry entry = getEntry("NX_Q96I99");
+		//testFilterAndProperties(entry);
 
 	}
 
 	public void testFilterAndProperties(Entry entry) {
 		String entryName = entry.getUniqueName();
-
+		System.out.println("Testing " + entryName);
 		FilterAndPropertiesFieldsBuilder ffb = new FilterAndPropertiesFieldsBuilder();
 		ffb.initializeBuilder(entry);
 		
@@ -34,8 +34,10 @@ public class FilterAndPropertiesFieldBuilderDiffTest extends SolrDiffTest {
 		expectedCount = (int) getValueForFieldInCurrentSolrImplementation(entryName, Fields.ISOFORM_NUM);
 		Assert.assertEquals(expectedCount, (int) ffb.getFieldValue(Fields.ISOFORM_NUM, Integer.class));
 		
+		if(getValueForFieldInCurrentSolrImplementation(entryName, Fields.PTM_NUM) != null) {
 		expectedCount = (int) getValueForFieldInCurrentSolrImplementation(entryName, Fields.PTM_NUM);
 		Assert.assertEquals(expectedCount, (int) ffb.getFieldValue(Fields.PTM_NUM, Integer.class));
+		}
 
 		expectedCount = (int) getValueForFieldInCurrentSolrImplementation(entryName, Fields.AA_LENGTH);
 		Assert.assertEquals(expectedCount, (int) ffb.getFieldValue(Fields.AA_LENGTH, Integer.class));
