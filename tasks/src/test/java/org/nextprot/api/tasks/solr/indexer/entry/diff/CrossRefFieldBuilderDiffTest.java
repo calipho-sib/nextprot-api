969efb5d0449c3e3aec924380a0a54d54486dac6
Daniel <ddtxra@gmail.com>
2015-11-11 15:46:15 +0100

    changing xref to crossers

--- tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/CrossRefFieldBuilderDiffTest.java
+++ tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/CrossRefFieldBuilderDiffTest.java
@@ -12,7 +12,7 @@ import org.nextprot.api.solr.index.EntryIndex.Fields;
 import org.nextprot.api.tasks.solr.indexer.entry.SolrDiffTest;
 import org.nextprot.api.tasks.solr.indexer.entry.impl.XrefFieldBuilder;
 
-public class xrefFieldBuilderDiffTest extends SolrDiffTest {
+public class CrossRefFieldBuilderDiffTest extends SolrDiffTest {
 
 	@Test
 	@Ignore
