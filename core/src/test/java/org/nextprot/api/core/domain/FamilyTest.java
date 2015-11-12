d3992dfb55de56ecf2848bc99b78409dd27cc745
fnikitin <frederic.nikitin@gmail.com>
2015-11-12 10:58:50 +0100

    Move test to NXVelocityUtilsTest

--- core/src/test/java/org/nextprot/api/core/domain/FamilyTest.java
+++ core/src/test/java/org/nextprot/api/core/domain/FamilyTest.java
@@ -3,8 +3,6 @@ package org.nextprot.api.core.domain;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 public class FamilyTest {
 
     @Test
@@ -41,23 +39,5 @@ public class FamilyTest {
         family.setName("bla");
 
         Assert.assertEquals("", family.getLevel());
-    }
-
-    @Test
-    public void testGetHierarchyFromRoot() throws Exception {
-
-        Family superfamily = new Family();
-        superfamily.setName("ba superfamily");
-
-        Family family = new Family();
-        family.setName("be family");
-
-        Family subfamily = new Family();
-        subfamily.setName("bi subfamily");
-
-        subfamily.setParent(family);
-        family.setParent(superfamily);
-
-        Assert.assertEquals(Arrays.asList(superfamily, family, subfamily), subfamily.getHierarchyFromRoot());
     }
 }
