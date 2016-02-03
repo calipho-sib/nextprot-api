1d56235c0703c193d9dc1ac1c82130d04df1a074
fnikitin <frederic.nikitin@gmail.com>
2016-02-03 18:25:59 +0100

    Update sql queries to get publisher infos

--- core/src/main/resources/sql-queries/core/publication-sorted-for-master.sql
+++ core/src/main/resources/sql-queries/core/publication-sorted-for-master.sql
@@ -1,7 +1,9 @@
 select p1.*, pubtypes.cv_name as pub_type, rp.property_value as submission_database , 
   actpub.pf_largescale as is_largescale,    
   actpub.pf_cited as is_curated,    
-  actpub.pf_uncited as is_computed    
+  actpub.pf_uncited as is_computed,
+  (select rp2.property_value from nextprot.resource_properties rp2 where rp2.resource_id=p1.resource_id and rp2.property_name='publisher') as publisher,
+  (select rp2.property_value from nextprot.resource_properties rp2 where rp2.resource_id=p1.resource_id and rp2.property_name='city') as city
 from nextprot.publications p1 
 inner join nextprot.view_master_publication_assoc actpub on (actpub.pub_id = p1.resource_id and actpub.entry_id= :identifierId and actpub.pub_type_id in ( :publicationTypes) )
 inner join nextprot.cv_publication_types as pubtypes on p1.cv_publication_type_id = pubtypes.cv_id 
