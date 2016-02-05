cf784c6541b7ce9b3597f57247de39226f1fdce7
fnikitin <frederic.nikitin@gmail.com>
2016-02-05 15:22:23 +0100

    Fixing missing editors by extracting method setAuthorsEditorsAndXrefs and sharing

--- core/src/main/java/org/nextprot/api/core/service/impl/PublicationServiceImpl.java
+++ core/src/main/java/org/nextprot/api/core/service/impl/PublicationServiceImpl.java
@@ -1,12 +1,16 @@
 package org.nextprot.api.core.service.impl;
 
 import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import com.google.common.collect.*;
 import org.nextprot.api.commons.dao.MasterIdentifierDao;
 import org.nextprot.api.core.dao.AuthorDao;
 import org.nextprot.api.core.dao.DbXrefDao;
 import org.nextprot.api.core.dao.PublicationDao;
-import org.nextprot.api.core.domain.*;
+import org.nextprot.api.core.domain.DbXref;
+import org.nextprot.api.core.domain.Publication;
+import org.nextprot.api.core.domain.PublicationAuthor;
+import org.nextprot.api.core.domain.PublicationDbXref;
 import org.nextprot.api.core.service.DbXrefService;
 import org.nextprot.api.core.service.PublicationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,20 @@ import java.util.*;
 
 @Service
 public class PublicationServiceImpl implements PublicationService {
+
+	private final static Predicate<PublicationAuthor> EDITOR_PREDICATE = new Predicate<PublicationAuthor>() {
+		@Override
+		public boolean apply(PublicationAuthor contributor) {
+			return contributor.isPerson() && contributor.isEditor();
+		}
+	};
+
+	private final static Predicate<PublicationAuthor> AUTHOR_PREDICATE = new Predicate<PublicationAuthor>() {
+		@Override
+		public boolean apply(PublicationAuthor contributor) {
+			return contributor.isPerson() && !contributor.isEditor();
+		}
+	};
 
 	@Autowired private MasterIdentifierDao masterIdentifierDao;
 	@Autowired private PublicationDao publicationDao;
@@ -36,8 +54,7 @@ public class PublicationServiceImpl implements PublicationService {
 	public List<Publication> findPublicationByTitle(String title) {
 		return publicationDao.findPublicationByTitle(title);
 	}
-	
-	
+
 	/**
 	 * TO REMOVE
 	 */
@@ -85,34 +102,11 @@ public class PublicationServiceImpl implements PublicationService {
 		for (Publication publication : publications) {
 			long publicationId = publication.getPublicationId();
 
-			SortedSet<PublicationAuthor> authorSet = new TreeSet<>(authorMap.get(publicationId));
-			SortedSet<PublicationAuthor> editors = splitEditorsFromAuthors(authorSet);
-
-			publication.setEditors(editors);
-			publication.setAuthors(authorSet);
-			publication.setDbXrefs(new HashSet<DbXref>(xrefMap.get(publicationId)));
+			setAuthorsEditorsAndXrefs(publication, authorMap.get(publicationId), xrefMap.get(publicationId));
 		}
 		
 		//returns a immutable list when the result is cacheable (this prevents modifying the cache, since the cache returns a reference) copy on read and copy on write is too much time consuming
 		return new ImmutableList.Builder<Publication>().addAll(publications).build();
-	}
-
-
-	private SortedSet<PublicationAuthor> splitEditorsFromAuthors(SortedSet<PublicationAuthor> publicationPersons) {
-
-		SortedSet<PublicationAuthor> editors = new TreeSet<>();
-
-		for (PublicationAuthor person : publicationPersons) {
-
-			if (person.isEditor()) {
-
-				editors.add(person);
-			}
-		}
-
-		publicationPersons.removeAll(editors);
-
-		return editors;
 	}
 
 	@Autowired
@@ -127,17 +121,26 @@ public class PublicationServiceImpl implements PublicationService {
 		return publication;
 	}
 
-
-	private void loadAuthorsAndXrefs(Publication p){
-		long publicationId = p.getPublicationId();
-		p.setAuthors(new TreeSet<>(this.authorDao.findAuthorsByPublicationId(publicationId)));
-		p.setDbXrefs(new HashSet<>(this.dbXrefDao.findDbXRefsByPublicationId(publicationId)));
-	}
-
 	@Override
 	public List<Long> findAllPublicationIds() {		
 		return publicationDao.findAllPublicationsIds();
 	}
 
+	private void loadAuthorsAndXrefs(Publication publication){
+		long publicationId = publication.getPublicationId();
 
+		setAuthorsEditorsAndXrefs(publication, authorDao.findAuthorsByPublicationId(publicationId), dbXrefDao.findDbXRefsByPublicationId(publicationId));
+	}
+
+	/**
+	 * Extract editors from authors, set authors, editors and xrefs to publication
+	 */
+	private void setAuthorsEditorsAndXrefs(Publication publication, Collection<PublicationAuthor> authorsAndEditors, Collection<? extends DbXref> xrefs){
+
+		SortedSet<PublicationAuthor> authorsAndEditorSet = new TreeSet<>(authorsAndEditors);
+
+		publication.setAuthors(Sets.filter(authorsAndEditorSet, AUTHOR_PREDICATE));
+		publication.setEditors(Sets.filter(authorsAndEditorSet, EDITOR_PREDICATE));
+		publication.setDbXrefs(new HashSet<>(xrefs));
+	}
 }
