7bfabf306f51a16c721fa1acd6583a1e212b8a36
fnikitin <frederic.nikitin@gmail.com>
2016-02-16 17:41:51 +0100

    Fixing overview xml for region-list and chain-list

--- web/src/test/java/org/nextprot/api/web/xml/integration/EntryOverviewXMLIntegrationTest.java
+++ web/src/test/java/org/nextprot/api/web/xml/integration/EntryOverviewXMLIntegrationTest.java
@@ -1,10 +1,10 @@
 package org.nextprot.api.web.xml.integration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
-
 import org.junit.Test;
 import org.nextprot.api.web.dbunit.base.mvc.WebIntegrationBaseTest;
 import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 public class EntryOverviewXMLIntegrationTest extends WebIntegrationBaseTest {
 	
@@ -19,9 +19,7 @@ public class EntryOverviewXMLIntegrationTest extends WebIntegrationBaseTest {
 	  actions.andExpect(xpath("entry/overview/gene-list/gene/gene-name[@type='synonym']").exists());
 	  actions.andExpect(xpath("entry/overview/gene-list/gene/gene-name[@type='synonym']").string("RNF53"));
   }
-  
-  
-	
+
   @Test
   public void shouldContainOverviewWithGeneNameIncludingORFName() throws Exception {
 
@@ -29,26 +27,21 @@ public class EntryOverviewXMLIntegrationTest extends WebIntegrationBaseTest {
 	  ResultActions actions = this.mockMvc.perform(get("/entry/NX_Q3L8U1/overview.xml"));
 	  actions.andExpect(xpath("entry/overview/gene-list/gene/gene-name[@type='ORF']").exists());
   }
-  
-  
+
   @Test
   public void shouldContainOverviewWithChainsAndAlternativeNames() throws Exception {
 
 	  //Tests issue CALIPHOMISC-322 https://issues.isb-sib.ch/browse/CALIPHOMISC-322
 	  ResultActions actions = this.mockMvc.perform(get("/entry/NX_P05067/overview.xml"));
-	  actions.andExpect(xpath("entry/overview/chain-list/chain/alternative-name-list/alternative-name/chain-name").exists());
+	  actions.andExpect(xpath("entry/overview/protein-name-list/chain-list/chain/alternative-name-list/alternative-name/chain-name").exists());
   }
-  
-  
+
   @Test
   public void shouldContainOverviewWithRegionsAndAlternativeNames() throws Exception {
 
 	  //Tests issue CALIPHOMISC-322 https://issues.isb-sib.ch/browse/CALIPHOMISC-322
 	  ResultActions actions = this.mockMvc.perform(get("/entry/NX_O60513/overview.xml"));
-	  actions.andExpect(xpath("entry/overview/region-list/region/alternative-name-list/alternative-name/region-name").exists());
+	  actions.andExpect(xpath("entry/overview/protein-name-list/region-list/region/alternative-name-list/alternative-name/region-name").exists());
   }
-
-
-
 }
 
