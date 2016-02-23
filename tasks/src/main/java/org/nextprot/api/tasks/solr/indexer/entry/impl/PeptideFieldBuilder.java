73c80a355c9ec139530b06b519aaa0f59076386f
Alain-Gateau <alain.gateau@gmail.com>
2016-02-23 17:32:13 +0100

    progress

--- tasks/src/main/java/org/nextprot/api/tasks/solr/indexer/entry/impl/PeptideFieldBuilder.java
+++ tasks/src/main/java/org/nextprot/api/tasks/solr/indexer/entry/impl/PeptideFieldBuilder.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import org.nextprot.api.core.domain.DbXref;
 import org.nextprot.api.core.domain.Entry;
+import org.nextprot.api.core.domain.annotation.Annotation;
 import org.nextprot.api.solr.index.EntryIndex.Fields;
 import org.nextprot.api.tasks.solr.indexer.entry.EntryFieldBuilder;
 import org.nextprot.api.tasks.solr.indexer.entry.FieldBuilder;
@@ -14,8 +15,13 @@ import org.nextprot.api.tasks.solr.indexer.entry.FieldBuilder;
 public class PeptideFieldBuilder extends FieldBuilder{
 	
 	@Override
-	protected void init(Entry entry){
-		
+	protected void init(Entry entry){ 
+		for (Annotation currannot : entry.getAnnotations()) {
+			String category = currannot.getCategory();
+			if (category.contains("peptide mapping")){
+				//System.err.println(currannot.);
+			}
+		}
 		for(DbXref xf : entry.getXrefs()){
 			if("PeptideAtlas".equals(xf.getDatabaseName()) || "SRMAtlas".equals(xf.getDatabaseName())){
 				addField(Fields.PEPTIDE, xf.getDatabaseName() + ":" + xf.getAccession() + ", " + xf.getAccession());
