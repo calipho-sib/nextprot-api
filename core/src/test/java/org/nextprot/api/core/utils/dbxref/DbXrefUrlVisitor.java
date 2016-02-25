a294f84e059e2c2b18082288838f710316261393
fnikitin <frederic.nikitin@gmail.com>
2016-02-25 18:13:45 +0100

    Improving DbXrefVisitor output and test all Terminology dbxrefs

--- core/src/test/java/org/nextprot/api/core/utils/dbxref/DbXrefUrlVisitor.java
+++ core/src/test/java/org/nextprot/api/core/utils/dbxref/DbXrefUrlVisitor.java
@@ -8,12 +8,15 @@ import java.net.*;
 import java.util.*;
 import java.util.logging.*;
 
+/**
+ * Check http statuses of urls and resolved urls in DbXrefs
+ */
 public class DbXrefUrlVisitor implements Closeable, Flushable {
 
     private static final Logger LOGGER = Logger.getLogger(DbXrefUrlVisitor.class.getSimpleName());
 
     private final PrintWriter pw;
-    private final Set<String> visitedLinkedURLs;
+    private final Set<String> visitedTemplateURLs;
     private final Map<String, Set<String>> dbxrefNon200HttpStatusMap;
 
     public DbXrefUrlVisitor(String outName, String logName) throws IOException {
@@ -28,60 +31,86 @@ public class DbXrefUrlVisitor implements Closeable, Flushable {
 
         LOGGER.addHandler(fileHandler);
 
-        visitedLinkedURLs = new HashSet<>();
-        dbxrefNon200HttpStatusMap = new HashMap<>();
+        visitedTemplateURLs = new HashSet<>();
+        dbxrefNon200HttpStatusMap = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String status1, String status2) {
+
+                boolean isStatus1Integer = status1.matches("\\d+");
+                boolean isStatus2Integer = status2.matches("\\d+");
+
+                // integer comes first
+                if (isStatus1Integer && isStatus2Integer) {
+                    return Integer.parseInt(status1) - (Integer.parseInt(status2));
+                }
+                else if (isStatus1Integer) {
+                    return -1;
+                }
+                else if (isStatus2Integer) {
+                    return 1;
+                }
+
+                return status1.compareTo(status2);
+            }
+        });
 
         pw.write("entry ac\tdb\txref ac\turl\thttp status\tresolved url\thttp status\n");
     }
 
     public void visit(String entryAc, List<DbXref> xrefs) throws IOException {
 
-        for (DbXref xref : xrefs) {
+        if (xrefs != null) {
+            for (DbXref xref : xrefs) {
 
-            String resolvedUrl = xref.getResolvedUrl();
+                String resolvedUrl = xref.getResolvedUrl();
 
-            String linkedURL = xref.getLinkUrl();
+                // url template
+                String dbName = xref.getDatabaseName();
+                String templateURL = dbName+"^"+xref.getLinkUrl();
 
-            if (!visitedLinkedURLs.contains(linkedURL)) {
+                if (!visitedTemplateURLs.contains(templateURL)) {
 
-                Response response = requestUrls(xref);
+                    Response response = requestUrls(xref);
 
-                int j=0;
-                int tries = 3;
-                while (response.getResolvedUrlHttpStatus().equals("TIMEOUT") && j<tries) {
+                    int j = 0;
+                    int tries = 3;
+                    while (response.getResolvedUrlHttpStatus().equals("TIMEOUT") && j < tries) {
 
-                    response = requestUrls(xref);
-                    j++;
+                        response = requestUrls(xref);
+                        j++;
+                    }
+
+                    String xrefAcc = xref.getAccession();
+                    String url = xref.getUrl();
+
+                    pw.write(entryAc);
+                    pw.write("\t");
+                    pw.write(dbName);
+                    pw.write("\t");
+                    pw.write(xrefAcc);
+                    pw.write("\t");
+                    pw.write(url);
+                    pw.write("\t");
+                    pw.write(response.getUrlHttpStatus());
+                    pw.write("\t");
+                    pw.write(resolvedUrl);
+                    pw.write("\t");
+                    pw.write(response.getResolvedUrlHttpStatus());
+                    pw.write("\n");
+
+                    if (!response.isUrlOK())
+                        addDbNameUrlStatus(response, dbName + " => " + url);
+
+                    if (!response.isResolvedUrlOK())
+                        addResolvedDbNameUrlStatus(response, dbName + " => " + resolvedUrl);
+
+                    visitedTemplateURLs.add(templateURL);
                 }
-
-                String db = xref.getDatabaseName();
-                String xrefAc = xref.getAccession();
-                String url = xref.getUrl();
-
-                pw.write(entryAc);
-                pw.write("\t");
-                pw.write(db);
-                pw.write("\t");
-                pw.write(xrefAc);
-                pw.write("\t");
-                pw.write(url);
-                pw.write("\t");
-                pw.write(response.getUrlHttpStatus());
-                pw.write("\t");
-                pw.write(resolvedUrl);
-                pw.write("\t");
-                pw.write(response.getResolvedUrlHttpStatus());
-                pw.write("\n");
-
-                if (!response.isUrlOK()) addUrlStatus(response, db+" => "+url);
-                if (!response.isResolvedUrlOK()) addResolvedUrlStatus(response, db+" => "+resolvedUrl);
-
-                visitedLinkedURLs.add(linkedURL);
             }
         }
     }
 
-    private void addUrlStatus(Response response, String url) {
+    private void addDbNameUrlStatus(Response response, String url) {
 
         if (!dbxrefNon200HttpStatusMap.containsKey(response.getUrlHttpStatus())) {
 
@@ -90,7 +119,7 @@ public class DbXrefUrlVisitor implements Closeable, Flushable {
         dbxrefNon200HttpStatusMap.get(response.getUrlHttpStatus()).add(url);
     }
 
-    private void addResolvedUrlStatus(Response response, String url) {
+    private void addResolvedDbNameUrlStatus(Response response, String url) {
 
         if (!dbxrefNon200HttpStatusMap.containsKey(response.getResolvedUrlHttpStatus())) {
 
@@ -124,50 +153,59 @@ public class DbXrefUrlVisitor implements Closeable, Flushable {
         }
 
         dbxrefNon200HttpStatusMap.clear();
-        visitedLinkedURLs.clear();
+        visitedTemplateURLs.clear();
 
         pw.close();
     }
 
     private Response requestUrls(DbXref xref) throws IOException {
 
-        String url = xref.getUrl();
-        String urlHttpStatus = getResponseCode(url);
-        String resolvedUrlHttpStatus = getResponseCode(xref.getResolvedUrl());
+        String urlHttpStatus = getResponseCode(xref, xref.getUrl());
+        String resolvedUrlHttpStatus = getResponseCode(xref, xref.getResolvedUrl());
 
         return new Response(urlHttpStatus, resolvedUrlHttpStatus);
     }
 
-    private String getResponseCode(String url) throws IOException {
+    private String getResponseCode(DbXref xref, String url) throws IOException {
 
         String response;
         HttpURLConnection con = null;
+        String xrefAccAndDbname = xref.getAccession()+"@"+xref.getDatabaseName();
 
-        URL obj = new URL(url);
+        if (url == null || url.equalsIgnoreCase("none") || url.isEmpty()) {
+
+            LOGGER.info(xrefAccAndDbname+": No possible request for "+url+"\n");
+            return "UNDEFINED URL";
+        }
 
         try {
+            URL obj = new URL(url);
+
             con = (HttpURLConnection) obj.openConnection();
 
             con.setRequestMethod("HEAD");
             con.setRequestProperty("User-Agent", "Mozilla/5.0");
             con.setConnectTimeout(5000);
-            con.connect();
 
-            LOGGER.info("Http HEAD request "+url+"\n");
+            LOGGER.info(xrefAccAndDbname+": Http HEAD request "+url+"\n");
+            con.connect();
             response = String.valueOf(con.getResponseCode());
 
         } catch (SocketTimeoutException e) {
             response = "TIMEOUT";
-            LOGGER.warning(response+"; "+e.getMessage()+"\n");
+            LOGGER.warning(xrefAccAndDbname+": "+response+"; "+e.getMessage()+"\n");
         } catch (ProtocolException e) {
             response = "PROTOCOL";
-            LOGGER.warning(response+"; "+e.getMessage()+"\n");
+            LOGGER.warning(xrefAccAndDbname+": "+response+"; "+e.getMessage()+"\n");
         } catch (MalformedURLException e) {
             response = "MALFORMEDURL";
-            LOGGER.warning(response+"; "+e.getMessage()+"\n");
+            LOGGER.warning(xrefAccAndDbname+": "+response+"; "+e.getMessage()+"\n");
         } catch (IOException e) {
             response = "IO";
-            LOGGER.warning(response+"; "+e.getMessage()+"\n");
+            LOGGER.warning(xrefAccAndDbname+": "+response+"; "+e.getMessage()+"\n");
+        } catch (IllegalArgumentException e) {
+            response = "ILLEGAL";
+            LOGGER.info(xrefAccAndDbname+": "+e.getLocalizedMessage());
         }
 
         if (con != null)
@@ -181,7 +219,7 @@ public class DbXrefUrlVisitor implements Closeable, Flushable {
         String urlHttpStatus;
         String resolvedUrlHttpStatus;
 
-        public Response(String urlHttpStatus, String resolvedUrlHttpStatus) {
+        private Response(String urlHttpStatus, String resolvedUrlHttpStatus) {
             this.urlHttpStatus = urlHttpStatus;
             this.resolvedUrlHttpStatus = resolvedUrlHttpStatus;
         }
