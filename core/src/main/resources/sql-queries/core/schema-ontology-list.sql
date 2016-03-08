cb89d872590df5c0439eb60d899265e9cc5f4163
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2016-03-08 11:01:33 +0100

    exclude uniprot family terminology from the list of ontologies

--- core/src/main/resources/sql-queries/core/schema-ontology-list.sql
+++ core/src/main/resources/sql-queries/core/schema-ontology-list.sql
@@ -1,2 +1,4 @@
 SELECT o.cv_api_name as ontology, o.description as description, o.cv_display_name as name 
-  FROM nextprot.cv_term_categories o ORDER BY o.cv_api_name
+FROM nextprot.cv_term_categories o 
+WHERE o.cv_api_name not in ('UniprotFamilyCv')
+ORDER BY o.cv_api_name
