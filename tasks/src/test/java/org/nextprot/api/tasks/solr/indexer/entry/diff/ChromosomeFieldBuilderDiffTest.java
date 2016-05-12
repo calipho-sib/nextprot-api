7393f90babadf68b81fcaeb5cb4a9972421a39f1
Alain-Gateau <alain.gateau@gmail.com>
2016-05-12 15:49:31 +0200

    progress

--- tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/ChromosomeFieldBuilderDiffTest.java
+++ tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/ChromosomeFieldBuilderDiffTest.java
@@ -12,7 +12,6 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class ChromosomeFieldBuilderDiffTest extends SolrDiffTest {
 
