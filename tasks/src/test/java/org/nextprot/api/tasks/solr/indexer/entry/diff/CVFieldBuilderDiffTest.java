83c2853369ca6aeb6e7f2cef970c32688c87fecd
Alain-Gateau <alain.gateau@gmail.com>
2016-06-10 18:03:26 +0200

    gold annotation

--- tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/CVFieldBuilderDiffTest.java
+++ tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/CVFieldBuilderDiffTest.java
@@ -19,10 +19,11 @@ public class CVFieldBuilderDiffTest extends SolrDiffTest {
 
 	@Test
 	public void testCVs() {
-		String[] test_list = {"NX_O00116"/*, "NX_O00115","NX_Q7Z6P3","NX_E5RQL4","NX_O00115","NX_Q7Z6P3",
-				"NX_Q7Z713", "NX_P22102", "NX_Q7Z713", "NX_O00116", "NX_Q7Z713", "NX_O15056"*/};
+		String[] test_list = {"NX_Q6H8Q1", "NX_O00116","NX_Q7Z6P3","NX_E5RQL4","NX_O00115","NX_Q7Z6P3",
+				"NX_Q7Z713", "NX_P22102", "NX_Q7Z713", "NX_O00116", "NX_Q7Z713", "NX_O15056"};
 
-		for(int i=0; i < test_list.length; i++){ 	testCVs(getEntry(test_list[i])); } 
+		//for(int i=0; i < test_list.length; i++){ 	testCVs(getEntry(test_list[i])); } 
+		for(int i=0; i < 1; i++){ 	testCVs(getEntry(test_list[i])); } 
 		
 		//Entry entry = getEntry("NX_P20594");
 		//Entry entry = getEntry("NX_P14060");
