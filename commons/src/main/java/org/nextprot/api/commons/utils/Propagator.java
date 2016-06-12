8e8cdc4abb1d0bd5b278d59c091971a96de92260
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2016-06-12 13:38:05 +0200

    wrote second function master -> iso pos with tests ok

--- commons/src/main/java/org/nextprot/api/commons/utils/Propagator.java
+++ commons/src/main/java/org/nextprot/api/commons/utils/Propagator.java
@@ -6,6 +6,58 @@ import java.util.Map.Entry;
 
 public class Propagator {
 
+	/*
+	 * 
+	 * nuNum      0   1   2   3   4            5   6   7   8
+	 * exons     |---- exon1 ------|          |-- exon 2 ---|
+	 * nuPos     100 101 102 103 104          201 202 203 204
+	 * codons    |--codon1--|-------codon2-------|--codon3--|
+	 * 
+	 * 
+	 */
+
+
+	public static Integer getIsoformPosition(List<Integer> codonPos, List<Entry<Integer,Integer>> positionsOfIsoformOnReferencedGene) {
+		
+		System.out.println("----------------------------------------------------------");
+		int lowNum = 0;
+		List<Integer> codonNum = new ArrayList<Integer>();
+		for (Entry<Integer,Integer> e: positionsOfIsoformOnReferencedGene) {
+			int nu1Pos = e.getKey();
+			int nu2Pos = e.getValue();
+			int highNum = lowNum + nu2Pos - nu1Pos ;
+			System.out.println("nu1Pos:"+ nu1Pos + " nu2Pos:" + nu2Pos + " lowNum:" + lowNum + " highNum:" + highNum);
+			while (true) {
+				int nuIndex = codonNum.size();
+				int nuPos = codonPos.get(nuIndex);
+				System.out.println("nuPos("+nuIndex+")=" + nuPos);
+				if (nuPos < nu1Pos || nuPos > nu2Pos) break;
+				int nuNum = lowNum + nuPos - nu1Pos;
+				System.out.println("adding codon nucelotide number:" + nuNum);
+				codonNum.add(nuNum);
+				if (codonNum.size()==3) {
+					// check that codon nucleotides nums are consecutive
+					for (int i=0;i<3;i++) System.out.println("nu"+i+ " at gene pos " + codonPos.get(i) + " is nucleotide num " + codonNum.get(i));
+					if (codonNum.get(0)+1 != codonNum.get(1) || codonNum.get(1)+1 != codonNum.get(2) ) {
+						System.out.println("nucleotides of codon are not consecutive");
+						return null;
+					}
+					// check that nucleotides num are in same frame (optional check ?)
+					int isoPos = (codonNum.get(0) + 3) / 3;
+					if ((codonNum.get(1) + 3) / 3 != isoPos || (codonNum.get(2) + 3) / 3 != isoPos) {
+						System.out.println("nucleotides not in frame");
+						return null;
+					}
+					System.out.println("returning iso pos ( nuNum:" + codonNum.get(0) + " ) = " + isoPos);
+					return new Integer(isoPos); 
+				}
+			}
+			lowNum=highNum + 1;
+		}
+		System.out.println("codon not found in the gene mapping ranges, " + codonNum.size() + " nucleotides found");
+		return null;		
+	}	
+	
 	public static List<Integer> getGeneCodonPositions(int isoformPos, List<Entry<Integer,Integer>> positionsOfIsoformOnReferencedGene) {
 		int nu1Num = isoformPos * 3 - 3;  
 		//System.out.println("nu1Num:" + nu1Num);
--- commons/src/test/java/org/nextprot/api/commons/utils/PropagatorTest.java
+++ commons/src/test/java/org/nextprot/api/commons/utils/PropagatorTest.java
@@ -14,13 +14,128 @@ import static org.junit.Assert.assertEquals;
 public class PropagatorTest {
 
     @Test
-    public void BasicTest() {
+    public void geneIsoformPosTest() {
+ 
+    	
+    	// represent two coding regions of gene / master mapped to an isoform sequence
+    	Map<Integer,Integer> map = new TreeMap<Integer,Integer>();
+    	map.put(100, 104);
+    	map.put(201, 204);
+    	List<Map.Entry<Integer,Integer>> genePosRanges = new ArrayList<Map.Entry<Integer,Integer>>(map.entrySet());
+
+    	/*
+    	 * 
+    	 * nuNum      0   1   2   3   4            5   6   7   8
+    	 * exons     |---- exon1 ------|          |-- exon 2 ---|
+    	 * nuPos     100 101 102 103 104          201 202 203 204
+    	 * codons    |--codon1--|-------codon2-------|--codon3--|
+    	 * 
+    	 * 
+    	 */
+
+    	Integer result;
+    	List<Integer> codonNuPos = new ArrayList<Integer>();
+
+    	// get isoform position of aa corresponding to codon with nucleotides at position 100,101,102
+    	codonNuPos.clear();
+    	codonNuPos.add(100);
+    	codonNuPos.add(101);
+    	codonNuPos.add(102);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(new Integer(1), result);
+
+    	// get isoform position of aa corresponding to codon with nucleotides at position 103 104 201
+    	codonNuPos.clear();
+    	codonNuPos.add(103);
+    	codonNuPos.add(104);
+    	codonNuPos.add(201);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(new Integer(2), result);
+
+    	// get isoform position of aa corresponding to codon with nucleotides at position 202 203 204
+    	codonNuPos.clear();
+    	codonNuPos.add(202);
+    	codonNuPos.add(203);
+    	codonNuPos.add(204);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(new Integer(3), result);
+
+    	// trying to find a codon position having NON consecutive nucleotides i.e. at pos 201 203 204
+    	codonNuPos.clear();
+    	codonNuPos.add(201);
+    	codonNuPos.add(203);
+    	codonNuPos.add(204);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(null, result);
+
+    	// trying to find a codon position having nucleotide positions that are not in frame (3 nucl of same codon)
+    	// i.e. at pos 101 102 103
+    	codonNuPos.clear();
+    	codonNuPos.add(101);
+    	codonNuPos.add(102);
+    	codonNuPos.add(103);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(null, result);
+
+    	// trying to find a codon position having nucleotide positions that are not in frame (3 nucl of same codon)
+    	// i.e. at pos 102 103 104
+    	codonNuPos.clear();
+    	codonNuPos.add(102);
+    	codonNuPos.add(103);
+    	codonNuPos.add(104);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(null, result);
+
+    	// trying to find a codon having nucleotide positions out of the gene mapping ranges
+    	// i.e. at pos  77,78,79
+    	codonNuPos.clear();
+    	codonNuPos.add(77);
+    	codonNuPos.add(78);
+    	codonNuPos.add(79);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(null, result);
+
+    	// trying to find a codon having nucleotide positions out of the gene mapping ranges
+    	// i.e. at pos 99,100,101
+    	codonNuPos.clear();
+    	codonNuPos.add(99);
+    	codonNuPos.add(100);
+    	codonNuPos.add(101);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(null, result);
+
+    	// trying to find a codon having nucleotide positions out of the gene mapping ranges
+    	// i.e. at pos 103,104, 015
+    	codonNuPos.clear();
+    	codonNuPos.add(103);
+    	codonNuPos.add(104);
+    	codonNuPos.add(105);
+    	result = Propagator.getIsoformPosition(codonNuPos, genePosRanges);
+    	assertEquals(null, result);
+
+    	
+    	
+    }
+	
+    @Test
+    public void geneCodonPosTest() {
+ 
     	
     	// represent two coding regions of gene / master mapped to an isoform sequence
     	Map<Integer,Integer> map = new TreeMap<Integer,Integer>();
     	map.put(100, 104);
     	map.put(201, 204);
     	List<Map.Entry<Integer,Integer>> list = new ArrayList<Map.Entry<Integer,Integer>>(map.entrySet());
+
+    	/*
+    	 * 
+    	 * nuNum      0   1   2   3   4            5   6   7   8
+    	 * exons     |---- exon1 ------|          |-- exon 2 ---|
+    	 * nuPos     100 101 102 103 104          201 202 203 204
+    	 * codons    |--codon1--|-------codon2-------|--codon3--|
+    	 * 
+    	 * 
+    	 */
 
     	List<Integer> result;
     	
