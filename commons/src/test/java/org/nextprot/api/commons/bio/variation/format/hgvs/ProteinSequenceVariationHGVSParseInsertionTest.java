5ed83583661bb18b010c91d87724693fb302bff8
fnikitin <frederic.nikitin@gmail.com>
2016-06-29 16:53:19 +0200

    Adding tests for insertion parsing

--- commons/src/test/java/org/nextprot/api/commons/bio/variation/format/hgvs/ProteinSequenceVariationHGVSParseInsertionTest.java
+++ commons/src/test/java/org/nextprot/api/commons/bio/variation/format/hgvs/ProteinSequenceVariationHGVSParseInsertionTest.java
@@ -1,7 +1,6 @@
 package org.nextprot.api.commons.bio.variation.format.hgvs;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.nextprot.api.commons.bio.AminoAcidCode;
 import org.nextprot.api.commons.bio.variation.Insertion;
@@ -18,22 +17,34 @@ public class ProteinSequenceVariationHGVSParseInsertionTest {
 
         ProteinSequenceVariation pm = format.parse("p.C136_A137insGM", ProteinSequenceVariationHGVSFormat.ParsingMode.PERMISSIVE);
 
-        Assert.assertEquals(AminoAcidCode.Cysteine, pm.getFirstChangingAminoAcid());
-        Assert.assertEquals(AminoAcidCode.Alanine, pm.getLastChangingAminoAcid());
-        Assert.assertEquals(136, pm.getFirstChangingAminoAcidPos());
-        Assert.assertEquals(137, pm.getLastChangingAminoAcidPos());
+        assertProteinSequenceVariation(pm, AminoAcidCode.Cysteine, AminoAcidCode.Alanine, 136, 137);
         Assert.assertTrue(pm.getProteinSequenceChange() instanceof Insertion);
         Assert.assertArrayEquals(AminoAcidCode.valueOfOneLetterCodeSequence("GM"), (AminoAcidCode[]) pm.getProteinSequenceChange().getValue());
         Assert.assertEquals(136, ((Insertion)pm.getProteinSequenceChange()).getInsertAfterPos());
     }
 
-    @Ignore
+    @Test(expected = ParseException.class)
+    public void testParseInsertionsVariantsInvalidColonInsteadOfUnderscore() throws ParseException {
+
+        format.parse("p.Met1875-Glu1876insMet", ProteinSequenceVariationHGVSFormat.ParsingMode.PERMISSIVE);
+    }
+
     @Test
     public void testParseInsertionsVariants() throws ParseException {
-        /*
-INSERTIONS:
-p.Met1875-Glu1876insMet
-         */
-        format.parse("p.Met1875-Glu1876insMet", ProteinSequenceVariationHGVSFormat.ParsingMode.PERMISSIVE);
+
+        ProteinSequenceVariation pm = format.parse("p.Met1875_Glu1876insMet", ProteinSequenceVariationHGVSFormat.ParsingMode.STRICT);
+
+        assertProteinSequenceVariation(pm, AminoAcidCode.Methionine, AminoAcidCode.GlutamicAcid, 1875, 1876);
+        Assert.assertTrue(pm.getProteinSequenceChange() instanceof Insertion);
+        Assert.assertArrayEquals(AminoAcidCode.valueOfOneLetterCodeSequence("M"), (AminoAcidCode[]) pm.getProteinSequenceChange().getValue());
+        Assert.assertEquals(1875, ((Insertion)pm.getProteinSequenceChange()).getInsertAfterPos());
+    }
+
+    public static void assertProteinSequenceVariation(ProteinSequenceVariation pm, AminoAcidCode expectedFirstAA,
+                                                      AminoAcidCode expectedLastAA, int expectedFirstPos, int expectedLastPos) {
+        Assert.assertEquals(expectedFirstAA, pm.getFirstChangingAminoAcid());
+        Assert.assertEquals(expectedLastAA, pm.getLastChangingAminoAcid());
+        Assert.assertEquals(expectedFirstPos, pm.getFirstChangingAminoAcidPos());
+        Assert.assertEquals(expectedLastPos, pm.getLastChangingAminoAcidPos());
     }
 }
