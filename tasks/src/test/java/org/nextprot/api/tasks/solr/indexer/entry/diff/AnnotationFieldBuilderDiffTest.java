f18457a12f7cc4f988c218b8fd3a60e4d894f5bc
Alain-Gateau <alain.gateau@gmail.com>
2016-06-30 18:13:10 +0200

    minor changes

--- tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/AnnotationFieldBuilderDiffTest.java
+++ tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/AnnotationFieldBuilderDiffTest.java
@@ -32,10 +32,10 @@ public class AnnotationFieldBuilderDiffTest extends SolrDiffTest {
 	//@Ignore
 	public void testAnnotationsAndFunctionalDescriptions() {
 
-		String[] test_list = {"NX_Q06830", "NX_O00115","NX_Q7Z6P3","NX_E5RQL4","NX_Q14721","NX_Q7Z6P3",
-				"NX_Q7Z713", "NX_P22102", "NX_Q7Z713", "NX_Q13286", "NX_Q9UNK4", "NX_Q9NWT6"};
+		String[] test_list = {"NX_Q8IWA4", "NX_O00115","NX_Q7Z6P3","NX_E5RQL4","NX_Q12809","NX_Q7Z6P3",
+				"NX_Q7Z713", "NX_P22102", "NX_Q8IYV9", "NX_O00116", "NX_Q7Z713", "NX_O15056"};
 
-		for(int i=0; i < 12; i++){
+		for(int i=0; i < test_list.length; i++){
 			Entry entry = getEntry(test_list[i]); 
 			//Entry entry = getEntry(i); // 'random' entry
 		    //Entry entry = getEntry("NX_P20592");
