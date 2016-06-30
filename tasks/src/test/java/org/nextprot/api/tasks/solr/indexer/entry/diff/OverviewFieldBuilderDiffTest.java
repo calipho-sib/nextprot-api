2d2ab6ee51ed244769559b8d032f44a6b6aa416b
Alain-Gateau <alain.gateau@gmail.com>
2016-06-30 18:14:14 +0200

    minor changes

--- tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/OverviewFieldBuilderDiffTest.java
+++ tasks/src/test/java/org/nextprot/api/tasks/solr/indexer/entry/diff/OverviewFieldBuilderDiffTest.java
@@ -12,13 +12,11 @@ public class OverviewFieldBuilderDiffTest extends SolrDiffTest {
 	@Test
 	public void testOverview() {
 
-		String[] test_list = {"NX_Q8IWA4", "NX_O00115","NX_Q7Z6P3","NX_E5RQL4","NX_O00115","NX_Q7Z6P3",
-				"NX_Q7Z713", "NX_P22102", "NX_Q7Z713", "NX_O00116", "NX_Q7Z713", "NX_O15056"};
+		String[] test_list = {"NX_Q8IWA4", "NX_O00115","NX_Q7Z6P3","NX_E5RQL4","NX_Q12809","NX_Q7Z6P3",
+				"NX_Q7Z713", "NX_P22102", "NX_Q8IYV9", "NX_O00116", "NX_Q7Z713", "NX_O15056"};
 		
-		 for(int i=0; i < 12; i++){ testOverview(getEntry(test_list[i])); } 
+		 for(int i=0; i < test_list.length; i++){ testOverview(getEntry(test_list[i])); } 
 		 //for(int i=0; i < 10; i++){ testOverview(getEntry(i)); } // 'random' entries
-		 
-
 		//Entry entry = getEntry("NX_Q96I99");
 		//testOverview(entry);
 
