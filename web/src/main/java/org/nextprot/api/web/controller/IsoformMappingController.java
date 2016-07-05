5fac618291936351de6df923bbe88462f5c3b7a2
fnikitin <frederic.nikitin@gmail.com>
2016-07-05 16:31:03 +0200

    Change signature of IsoformMappingController handlers

--- web/src/main/java/org/nextprot/api/web/controller/IsoformMappingController.java
+++ web/src/main/java/org/nextprot/api/web/controller/IsoformMappingController.java
@@ -7,14 +7,16 @@ import org.jsondoc.core.annotation.ApiMethod;
 import org.jsondoc.core.annotation.ApiPathParam;
 import org.jsondoc.core.annotation.ApiQueryParam;
 import org.jsondoc.core.pojo.ApiVerb;
-import org.nextprot.api.commons.constants.AnnotationCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -30,14 +32,14 @@ public class IsoformMappingController {
 	@ApiMethod(path = "/validate-feature/{category}", verb = ApiVerb.GET, description = "Validate isoform feature", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/validate-feature/{category}", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<MappedIsoformsFeatureResult> validateIsoformFeature(
-			@ApiPathParam(name = "category", description = "A feature category.",  allowedvalues = { "variant" })
+			@ApiPathParam(name = "category", description = "A feature category (either 'variant' or 'ptm').",  allowedvalues = { "variant" })
 			@PathVariable("category") String featureCategory, HttpServletRequest request, HttpServletResponse response,
 			@ApiQueryParam(name = "feature", description = "An isoform feature.",  allowedvalues = { "SCN11A-p.Leu1158Pro" })
 			@RequestParam(value = "feature", required = true) String feature,
 			@ApiQueryParam(name = "accession", description = "A nextprot accession: either isoform accession or entry accession when canonical.",  allowedvalues = { "NX_Q9UI33" })
 			@RequestParam(value = "accession", required = true) String nextprotAccession) {
 
-		MappedIsoformsFeatureResult result = isoformMappingService.validateFeature(feature, AnnotationCategory.getDecamelizedAnnotationTypeName(featureCategory), nextprotAccession);
+		MappedIsoformsFeatureResult result = isoformMappingService.validateFeature(feature, featureCategory, nextprotAccession);
 
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
@@ -45,14 +47,14 @@ public class IsoformMappingController {
 	@ApiMethod(path = "/propagate-feature/{category}", verb = ApiVerb.GET, description = "Validate isoform feature and compute feature propagations on other isoforms", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/propagate-feature/{category}", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<MappedIsoformsFeatureResult> propagateIsoformFeature(
-			@ApiPathParam(name = "category", description = "A feature category.",  allowedvalues = { "variant" })
+			@ApiPathParam(name = "category", description = "A feature category (either 'variant' or 'ptm').",  allowedvalues = { "variant" })
 			@PathVariable("category") String featureCategory, HttpServletRequest request, HttpServletResponse response,
 			@ApiQueryParam(name = "feature", description = "An isoform feature.",  allowedvalues = { "SCN11A-p.Leu1158Pro" })
 			@RequestParam(value = "feature", required = true) String feature,
 			@ApiQueryParam(name = "accession", description = "A nextprot accession: either isoform accession or entry accession when canonical.",  allowedvalues = { "NX_Q9UI33" })
 			@RequestParam(value = "accession", required = true) String nextprotAccession) {
 
-		MappedIsoformsFeatureResult result = isoformMappingService.propagateFeature(feature, AnnotationCategory.getDecamelizedAnnotationTypeName(featureCategory), nextprotAccession);
+		MappedIsoformsFeatureResult result = isoformMappingService.propagateFeature(feature, featureCategory, nextprotAccession);
 
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
