dd25c128c5eb50217645eb3b97f67c6761be7933
fnikitin <frederic.nikitin@gmail.com>
2016-07-07 14:13:54 +0200

    New ptm feature

--- isoform-mapper/src/main/java/com/nextprot/api/isoform/mapper/domain/GeneFeaturePair.java
+++ isoform-mapper/src/main/java/com/nextprot/api/isoform/mapper/domain/GeneFeaturePair.java
@@ -1,7 +1,13 @@
 package com.nextprot.api.isoform.mapper.domain;
 
+import java.text.ParseException;
+
 public interface GeneFeaturePair {
 
     String getGeneName();
     IsoformFeature getFeature();
+
+    interface GeneFeaturePairParser {
+        GeneFeaturePair parse(String variant) throws ParseException;
+    }
 }
