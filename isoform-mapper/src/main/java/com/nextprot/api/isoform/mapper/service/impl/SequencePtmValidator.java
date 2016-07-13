09e4fa831250cef12a988e970a7f8e574313f973
fnikitin <frederic.nikitin@gmail.com>
2016-07-13 19:12:46 +0200

    Add FeatureQuery param and remove useless interface

--- isoform-mapper/src/main/java/com/nextprot/api/isoform/mapper/service/impl/SequencePtmValidator.java
+++ isoform-mapper/src/main/java/com/nextprot/api/isoform/mapper/service/impl/SequencePtmValidator.java
@@ -1,11 +1,17 @@
 package com.nextprot.api.isoform.mapper.service.impl;
 
+import com.nextprot.api.isoform.mapper.domain.FeatureQuery;
 import com.nextprot.api.isoform.mapper.domain.GeneVariationPair;
 import com.nextprot.api.isoform.mapper.domain.impl.GenePtmPair;
+import com.nextprot.api.isoform.mapper.service.SequenceFeatureValidator;
 
 import java.text.ParseException;
 
-class SequencePtmValidator extends SequenceVariationValidator {
+class SequencePtmValidator extends SequenceFeatureValidator {
+
+    public SequencePtmValidator(FeatureQuery query) {
+        super(query);
+    }
 
     @Override
     protected GeneVariationPair newGeneVariationPair(String feature) throws ParseException {
