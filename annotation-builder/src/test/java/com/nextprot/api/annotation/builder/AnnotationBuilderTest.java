9845f1b39f4d7a11d9deb6b282406f7b6fd8272b
fnikitin <frederic.nikitin@gmail.com>
2016-07-15 13:11:19 +0200

    Typo fixed in nextprot-commons

--- annotation-builder/src/test/java/com/nextprot/api/annotation/builder/AnnotationBuilderTest.java
+++ annotation-builder/src/test/java/com/nextprot/api/annotation/builder/AnnotationBuilderTest.java
@@ -1,14 +1,14 @@
 package com.nextprot.api.annotation.builder;
 
-import static org.junit.Assert.*;
-
-import java.util.Arrays;
-
 import org.junit.Test;
 import org.nextprot.api.commons.constants.AnnotationCategory;
 import org.nextprot.api.commons.exception.NextProtException;
 import org.nextprot.api.core.domain.annotation.IsoformAnnotation;
 import org.nextprot.commons.statements.StatementBuilder;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 public class AnnotationBuilderTest {
 	
@@ -16,12 +16,12 @@ public class AnnotationBuilderTest {
 	public void shouldReturnOneSingleAnnotationIfTheInfoIsTheSameAndItIsComingFromDifferentSources() {
 		
 		StatementBuilder sb1 = StatementBuilder.createNew().
-		   addCompulsaryFields("NX_P01308", "NX_P01308-1", "go-cellular-component")
+				addCompulsoryFields("NX_P01308", "NX_P01308-1", "go-cellular-component")
    		  .addCvTerm("go-xxx", "nucleus", "go-cellular-component-cv")
    		  .addSourceInfo("CAVA-VP0920190912", "BioEditor");
 		
 		StatementBuilder sb2 = StatementBuilder.createNew().
-				addCompulsaryFields("NX_P01308", "NX_P01308-1", "go-cellular-component")
+				addCompulsoryFields("NX_P01308", "NX_P01308-1", "go-cellular-component")
 				.addCvTerm("go-xxx", "nucleus", "go-cellular-component-cv")
 				.addSourceInfo("HPA2222", "HPA");
 		
@@ -37,10 +37,10 @@ public class AnnotationBuilderTest {
 	public void shouldReturnAnExceptionIf2AnnotationsAreExpectedInsteadOfOne() {
 		
 		StatementBuilder sb1 = StatementBuilder.createNew().
-		   addCompulsaryFields("NX_P01308", "NX_P01308-1", "go-cellular-component");
+		   addCompulsoryFields("NX_P01308", "NX_P01308-1", "go-cellular-component");
    		
 		StatementBuilder sb2 = StatementBuilder.createNew().
-				addCompulsaryFields("NX_P99999", "NX_P99999-1", "go-cellular-component");
+				addCompulsoryFields("NX_P99999", "NX_P99999-1", "go-cellular-component");
 		
 		AnnotationBuilder.buildAnnotation("NX_P01308-1", Arrays.asList(sb1.build(), sb2.build()));
 		
