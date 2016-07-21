d7935d4010e7be107178c2229b54addf8315e14f
fnikitin <frederic.nikitin@gmail.com>
2016-07-21 16:55:36 +0200

    Jackson pretty print config

--- web/src/main/java/org/nextprot/api/web/controller/IsoformMappingController.java
+++ web/src/main/java/org/nextprot/api/web/controller/IsoformMappingController.java
@@ -9,14 +9,9 @@ import org.jsondoc.core.annotation.ApiQueryParam;
 import org.jsondoc.core.pojo.ApiVerb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,7 +26,8 @@ public class IsoformMappingController {
 
 	@ApiMethod(path = "/validate-feature/{category}", verb = ApiVerb.GET, description = "Validate isoform feature", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/validate-feature/{category}", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<FeatureQueryResult> validateIsoformFeature(
+	@ResponseBody
+	public FeatureQueryResult validateIsoformFeature(
 			@ApiPathParam(name = "category", description = "A feature category (either 'variant' or 'ptm').",  allowedvalues = { "variant" })
 			@PathVariable("category") String featureCategory, HttpServletRequest request, HttpServletResponse response,
 			@ApiQueryParam(name = "feature", description = "An isoform feature.",  allowedvalues = { "SCN11A-p.Leu1158Pro" })
@@ -39,14 +35,13 @@ public class IsoformMappingController {
 			@ApiQueryParam(name = "accession", description = "A nextprot entry accession.",  allowedvalues = { "NX_Q9UI33" })
 			@RequestParam(value = "accession", required = true) String nextprotAccession) {
 
-		FeatureQueryResult result = isoformMappingService.validateFeature(feature, featureCategory, nextprotAccession);
-
-		return new ResponseEntity<>(result, HttpStatus.OK);
+		return isoformMappingService.validateFeature(feature, featureCategory, nextprotAccession);
 	}
 
 	@ApiMethod(path = "/propagate-feature/{category}", verb = ApiVerb.GET, description = "Validate isoform feature and compute feature propagations on other isoforms", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/propagate-feature/{category}", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<FeatureQueryResult> propagateIsoformFeature(
+	@ResponseBody
+	public FeatureQueryResult propagateIsoformFeature(
 			@ApiPathParam(name = "category", description = "A feature category (either 'variant' or 'ptm').",  allowedvalues = { "variant" })
 			@PathVariable("category") String featureCategory, HttpServletRequest request, HttpServletResponse response,
 			@ApiQueryParam(name = "feature", description = "An isoform feature.",  allowedvalues = { "SCN11A-p.Leu1158Pro" })
@@ -54,9 +49,7 @@ public class IsoformMappingController {
 			@ApiQueryParam(name = "accession", description = "A nextprot entry accession.",  allowedvalues = { "NX_Q9UI33" })
 			@RequestParam(value = "accession", required = true) String nextprotAccession) {
 
-		FeatureQueryResult result = isoformMappingService.propagateFeature(feature, featureCategory, nextprotAccession);
-
-		return new ResponseEntity<>(result, HttpStatus.OK);
+		return isoformMappingService.propagateFeature(feature, featureCategory, nextprotAccession);
 	}
 }
 
