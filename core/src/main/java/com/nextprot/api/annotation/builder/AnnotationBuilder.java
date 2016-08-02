ed7934353a580dd18ad19e7e62b9dfe4f96c7882
Daniel <ddtxra@gmail.com>
2016-08-02 16:17:04 +0200

    setting evidence code

--- core/src/main/java/com/nextprot/api/annotation/builder/AnnotationBuilder.java
+++ core/src/main/java/com/nextprot/api/annotation/builder/AnnotationBuilder.java
@@ -108,14 +108,13 @@ abstract class AnnotationBuilder<T extends Annotation> {
 			 
 			 //TODO should this be hardcoded in the db for all ECOs or should we request a service at this stage?
 			 String statementEvidenceCode = s.getValue(StatementField.EVIDENCE_CODE);
+			 evidence.setEvidenceCodeOntology("evidence-code-ontology-cv");
 			 if(statementEvidenceCode != null){
 
 				 if("ECO:0000006".equals(statementEvidenceCode)){
 					 evidence.setEvidenceCodeName("experimental evidence");
-					 evidence.setEvidenceCodeOntology("evidence-code-ontology-cv");
 				 }else if("ECO:0000250".equals(statementEvidenceCode)){
 					 evidence.setEvidenceCodeName("sequence similarity");
-					 evidence.setEvidenceCodeOntology("evidence-code-ontology-cv");
 				 }else {
 					 throw new NextProtException("Not expecting " + statementEvidenceCode + " at this stage");
 				 }
