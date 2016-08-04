9c2f8df8511a8c55956dfddccb4fdd3130b99251
fnikitin <frederic.nikitin@gmail.com>
2016-08-04 19:30:24 +0200

    Implementation todo

--- core/src/main/java/org/nextprot/api/core/utils/annot/impl/SimilarityPredicateFactory.java
+++ core/src/main/java/org/nextprot/api/core/utils/annot/impl/SimilarityPredicateFactory.java
@@ -3,9 +3,12 @@ package org.nextprot.api.core.utils.annot.impl;
 import com.google.common.base.Preconditions;
 import org.nextprot.api.commons.constants.AnnotationCategory;
 import org.nextprot.api.core.domain.annotation.Annotation;
+import org.nextprot.api.core.domain.annotation.AnnotationIsoformSpecificity;
+import org.nextprot.api.core.utils.annot.ObjectMatcher;
 import org.nextprot.api.core.utils.annot.SimilarityPredicate;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class SimilarityPredicateFactory {
 
@@ -29,10 +32,21 @@ public class SimilarityPredicateFactory {
                         new ObjectSimilarityPredicate<>(Annotation::getCvTermAccessionCode),
                         new ObjectSimilarityPredicate<>(Annotation::getVariant,
                                 (v1, v2) -> v1.getOriginal().equals(v2.getOriginal()) && v1.getVariant().equals(v2.getVariant())),
-                        new ObjectSimilarityPredicate<>(Annotation::getTargetingIsoformsMap)
+                        new ObjectSimilarityPredicate<>(Annotation::getTargetingIsoformsMap, new VariantPositionMatcher()
+                        )
                 ));
             default:
                 return new ObjectSimilarityPredicate<>(Annotation::getAnnotationId);
+        }
+    }
+
+    private static class VariantPositionMatcher implements ObjectMatcher<Map<String,AnnotationIsoformSpecificity>> {
+
+        // TODO: make implementation as discussed on skype meeting with pam and daniel the 2016/08/04
+        @Override
+        public boolean match(Map<String, AnnotationIsoformSpecificity> m1, Map<String, AnnotationIsoformSpecificity> m2) {
+
+            return false;
         }
     }
 }
