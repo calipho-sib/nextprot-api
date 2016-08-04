911b7df86d3f2ce51ae9c244de3ca36466acdd84
fnikitin <frederic.nikitin@gmail.com>
2016-08-04 16:22:53 +0200

    Move creation logic to SimilarityPredicate factory

---
+++ core/src/main/java/org/nextprot/api/core/utils/annot/impl/SimilarityPredicateFactory.java
@@ -0,0 +1,33 @@
+package org.nextprot.api.core.utils.annot.impl;
+
+import org.nextprot.api.commons.constants.AnnotationCategory;
+import org.nextprot.api.core.domain.annotation.Annotation;
+import org.nextprot.api.core.utils.annot.SimilarityPredicate;
+
+import java.util.Arrays;
+
+public class SimilarityPredicateFactory {
+
+    /**
+     * Factory method based on AnnotationCategory.
+     * @return an instance of SimilarityPredicate given a category
+     */
+    public static SimilarityPredicate newSimilarityPredicate(AnnotationCategory category) {
+
+        switch (category) {
+            case GO_BIOLOGICAL_PROCESS:
+            case GO_CELLULAR_COMPONENT:
+            case GO_MOLECULAR_FUNCTION:
+                return new ObjectSimilarityPredicate(Annotation::getCvTermAccessionCode);
+            case VARIANT:
+            case MUTAGENESIS:
+                return new SimilarityPredicatePipeline(Arrays.asList(
+                        new ObjectSimilarityPredicate(Annotation::getVariant),
+                        new ObjectSimilarityPredicate(Annotation::getCvTermAccessionCode),
+                        new ObjectSimilarityPredicate(Annotation::getTargetingIsoformsMap)
+                ));
+            default:
+                return new ObjectSimilarityPredicate(Annotation::getVariant);
+        }
+    }
+}
