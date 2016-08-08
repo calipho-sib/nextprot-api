35eaea279c5ec352daf5087c0bfbcfe0a7040bfe
fnikitin <frederic.nikitin@gmail.com>
2016-08-08 12:41:03 +0200

    Add getCategory method in AnnotationCluster

--- core/src/main/java/org/nextprot/api/core/utils/annot/AnnotationCluster.java
+++ core/src/main/java/org/nextprot/api/core/utils/annot/AnnotationCluster.java
@@ -1,37 +1,53 @@
 package org.nextprot.api.core.utils.annot;
 
+import com.google.common.base.Preconditions;
+import org.nextprot.api.commons.constants.AnnotationCategory;
 import org.nextprot.api.core.domain.annotation.Annotation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * A group of annotation of the same category
+ */
 public class AnnotationCluster {
 
+    private final AnnotationCategory category;
     private final List<Annotation> group;
 
-    public AnnotationCluster() {
+    public AnnotationCluster(AnnotationCategory category) {
 
+        Preconditions.checkNotNull(category);
+        this.category = category;
         group = new ArrayList<>();
     }
 
+    /**
+     * Wrap an annotation into AnnotationCluster
+     */
     public static AnnotationCluster valueOf(Annotation annotation) {
 
-        AnnotationCluster annotationCluster = new AnnotationCluster();
-        annotationCluster.add(annotation);
+        AnnotationCluster annotationCluster = new AnnotationCluster(annotation.getAPICategory());
+        annotationCluster.group.add(annotation);
         return annotationCluster;
     }
 
+    /**
+     * Wrap annotations into AnnotationClusters
+     */
     public static List<AnnotationCluster> valueOfClusters(List<Annotation> annotations) {
 
         List<AnnotationCluster> annotationClusters = new ArrayList<>(annotations.size());
-        for (Annotation annotation : annotations) {
+        annotationClusters.addAll(annotations.stream().map(AnnotationCluster::valueOf).collect(Collectors.toList()));
 
-            annotationClusters.add(valueOf(annotation));
-        }
         return annotationClusters;
     }
 
-    public boolean add(Annotation annotation) {
+    public boolean add(Annotation annotation) throws InvalidAnnotationClusterCategoryException {
+
+        if (annotation.getAPICategory() != category)
+            throw new InvalidAnnotationClusterCategoryException(annotation, category);
 
         return group.add(annotation);
     }
@@ -42,5 +58,18 @@ public class AnnotationCluster {
 
     public List<Annotation> getAnnotations() {
         return group;
+    }
+
+    public AnnotationCategory getCategory() {
+
+        return category;
+    }
+
+    public static class InvalidAnnotationClusterCategoryException extends Exception {
+
+        public InvalidAnnotationClusterCategoryException(Annotation annotation, AnnotationCategory expectedCategory) {
+
+            super("could not add annotation of different category "+annotation.getAPICategory() + " (expected: "+expectedCategory+")");
+        }
     }
 }
