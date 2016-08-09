134084c610ad2980742523a255e3fa056b5b4a64
fnikitin <frederic.nikitin@gmail.com>
2016-08-09 20:11:52 +0200

    New spring application that export gene statements.

---
+++ core/src/main/java/com/nextprot/api/annotation/builder/statement/app/export/ArgumentParser.java
@@ -0,0 +1,119 @@
+package com.nextprot.api.annotation.builder.statement.app.export;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import org.apache.commons.cli.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Parse arguments and set Config object
+ *
+ * Created by fnikitin on 09/08/16.
+ */
+class ArgumentParser {
+
+    private final StatementExportApp.Config config;
+
+    ArgumentParser(String[] args) throws ParseException {
+
+        this.config = parseArgs(args);
+    }
+
+    public StatementExportApp.Config getConfig() {
+        return config;
+    }
+
+    /*
+     * Options:
+     * --profile (default: dev, cache)
+     * --filtered-categories (default: variant, mutagenesis)
+     * --genes (default: all)
+     * --output-file-format (default: tsv)
+     *
+     * Arguments:
+     * --output-file
+     */
+    private Options createOptions() {
+
+        Options options = new Options();
+
+        options.addOption(OptionBuilder.withArgName("profile").hasArg().withDescription("spring profile (default: dev, cache)").create("p"));
+        options.addOption(OptionBuilder.withArgName("categories").hasArg().withDescription("filtered categories (default: variant, mutagenesis)").create("c"));
+        options.addOption(OptionBuilder.withArgName("genes").hasArg().withDescription("genes to export (all genes are exported if not defined)").create("g"));
+
+        return options;
+    }
+
+    private StatementExportApp.Config parseArgs(String[] args) throws ParseException {
+
+        Options options = createOptions();
+
+        CommandLineParser parser = new GnuParser();
+
+        CommandLine commandLine = parser.parse(options, args);
+
+        StatementExportApp.Config mainConfig = new StatementExportApp.Config();
+
+        mainConfig.setSpringConfig(parseSpringConfig(commandLine));
+        mainConfig.setExporterConfig(parseExporterConfig(commandLine));
+        mainConfig.setGeneListToExport(parseGeneListToExport(commandLine));
+
+        String[] remainder = commandLine.getArgs();
+
+        if (remainder.length != 1)
+            throw new ParseException("missing output filename");
+
+        mainConfig.setOutputFilename(remainder[0]);
+
+        return mainConfig;
+    }
+
+    private StatementExportApp.SpringConfig parseSpringConfig(CommandLine commandLine) {
+
+        if (commandLine.hasOption("p")) {
+            System.out.print("Option profile is present.  The value is: ");
+            System.out.println(commandLine.getOptionValue("p"));
+
+            return new StatementExportApp.SpringConfig(commandLine.getOptionValue("p"));
+        }
+
+        return new StatementExportApp.SpringConfig();
+    }
+
+    private StatementExporter.Config parseExporterConfig(CommandLine commandLine) {
+
+        if (commandLine.hasOption("c")) {
+            System.out.print("Option category is present.  The value is: ");
+            System.out.println(commandLine.getOptionValue("c"));
+
+            Iterable<String> categories = Splitter.on(',')
+                    .trimResults()
+                    .omitEmptyStrings()
+                    .split(commandLine.getOptionValue("c"));
+
+            return new StatementExporter.Config(Iterables.toArray(categories, String.class));
+        }
+
+        return new StatementExporter.Config();
+    }
+
+    private List<String> parseGeneListToExport(CommandLine commandLine) {
+
+        if (commandLine.hasOption("g")) {
+            System.out.print("Option gene is present.  The value is: ");
+            System.out.println(commandLine.getOptionValue("g"));
+
+            Iterable<String> genes = Splitter.on(',')
+                    .trimResults()
+                    .omitEmptyStrings()
+                    .split(commandLine.getOptionValue("g"));
+
+            return Lists.newArrayList(genes);
+        }
+
+        return new ArrayList<>();
+    }
+}
