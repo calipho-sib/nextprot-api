71d89109371d285b73e8b982a0636d4cca718741
fnikitin <frederic.nikitin@gmail.com>
2016-08-10 13:55:13 +0200

    Ignoring test doing nothing

--- core/src/test/java/com/nextprot/api/annotation/builder/statement/service/StatementServiceTest.java
+++ core/src/test/java/com/nextprot/api/annotation/builder/statement/service/StatementServiceTest.java
@@ -1,49 +1,49 @@
 package com.nextprot.api.annotation.builder.statement.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.nextprot.api.annotation.builder.AnnotationBuilderBaseTest;
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.nextprot.api.core.domain.annotation.Annotation;
 import org.nextprot.api.core.domain.annotation.IsoformAnnotation;
 import org.nextprot.api.core.utils.annot.AnnotationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.nextprot.api.annotation.builder.AnnotationBuilderBaseTest;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StatementServiceTest extends AnnotationBuilderBaseTest {
 
 	@Autowired
 	private StatementService statementService;
 	
-
 	@Test
 	public void findAllMappedStatements() {
 		List<IsoformAnnotation> modifiedIsoformAnnotation = statementService.getIsoformAnnotations("NX_Q9BX63-1");
 		System.out.println(modifiedIsoformAnnotation.size());
 	}
 
+	// TODO: this test do nothing and f90790567dc0650b5737fa62332bb38a does not exist
+	@Ignore
 	@Test
 	public void findAllNormalAnnotations() {
 		List<Annotation> isoformAnnotations = statementService.getAnnotations("NX_Q15858");
-		Annotation a = isoformAnnotations.stream().filter(ia -> ia.getAnnotationHash().equals("f90790567dc0650b5737fa62332bb38a")).collect(Collectors.toList()).iterator().next();
-		
-		System.out.println(a.getSpecificityForIsoform("NX_Q15858-1"));
+		List<Annotation> list = isoformAnnotations.stream().filter(ia -> ia.getAnnotationHash().equals("f90790567dc0650b5737fa62332bb38a")).collect(Collectors.toList());
+
+		Assert.assertTrue(!list.isEmpty());
+
+		System.out.println(list.get(0).getSpecificityForIsoform("NX_Q15858-1"));
 	}
-	
-	
-	
-	
+
 	@Test
 	public void findModifiedAnnotationsByIsoform() {
 		List isoformAnnotations = statementService.getIsoformAnnotations("NX_Q15858-3");
 		System.out.println(isoformAnnotations.size());
 	}
-	
-	
+
 	@Test
 	public void findPhenotypeAnnotationsGoldOnly() {
 		List<IsoformAnnotation> isoformAnnotations = statementService.getIsoformAnnotations("NX_Q15858-3");
