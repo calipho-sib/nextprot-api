3377a83381371913e54115e530b3eff3863b7639
Daniel <ddtxra@gmail.com>
2016-08-15 17:34:45 +0200

    adding  evidence note and entry utils missing methods (things that were note merged very well)

--- core/src/main/java/org/nextprot/api/core/domain/EntryUtils.java
+++ core/src/main/java/org/nextprot/api/core/domain/EntryUtils.java
@@ -1,5 +1,14 @@
 package org.nextprot.api.core.domain;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 import org.nextprot.api.commons.constants.AnnotationCategory;
 import org.nextprot.api.core.domain.annotation.Annotation;
 import org.nextprot.api.core.service.fluent.EntryConfig;
@@ -8,14 +17,18 @@ import org.nextprot.api.core.utils.PublicationUtils;
 import org.nextprot.api.core.utils.XrefUtils;
 import org.nextprot.api.core.utils.annot.AnnotationUtils;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
-
 public class EntryUtils implements Serializable{
 	
 	private static final long serialVersionUID = 3009334685615648172L;
+
+	
+	public static String getEntryName(String nextprotAccession) {
+		String entryAccession = nextprotAccession;
+		if((nextprotAccession != null) && (nextprotAccession.length() > 0) && (nextprotAccession.contains("-"))){
+			entryAccession = nextprotAccession.substring(0, nextprotAccession.indexOf("-"));
+		}
+		return entryAccession;
+	}
 
 	public static Entry filterEntryBySubPart(Entry entry, EntryConfig config) {
 		
@@ -47,6 +60,97 @@ public class EntryUtils implements Serializable{
 		
 		return entry;
 	}
+
+
+	//TODO: PAM temporary method
+	public static Set<Proteoform> getProteoformSet(Entry entry, String isoformAc) {
+		
+		Set<Proteoform> result = new HashSet<Proteoform>();
+		for (Annotation annot: entry.getAnnotations()) {
+			if (annot.isProteoformAnnotation()) {
+				if (annot.getTargetingIsoformsMap().containsKey(isoformAc)) {
+					result.add(new Proteoform(isoformAc, annot.getSubjectName(), annot.getSubjectComponents()));
+				}
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Builds a dictionary (HashMap) where the key is the annotation uniqueName and the value the annotation itself.
+	 * @param entry
+	 * @return a dictionary of annotations where the key is the annotation uniqueName (= identifier in both NP1 and BED world)
+	 */
+	public static Map<String,Annotation> getUniqueNameAnnotationMap(Entry entry) {
+		
+		Map<String,Annotation> result = new HashMap<String,Annotation>();
+		for (Annotation annot: entry.getAnnotations()) {
+			result.put(annot.getUniqueName(), annot);
+		}
+		return result;
+	}
+	
+	public static Map<String,Integer> getAnnotationCategoryCountMap(Entry entry) {
+		Map<String,Integer> result = new TreeMap<String,Integer>();
+		for (Annotation annot: entry.getAnnotations()) {
+			String key = annot.getApiTypeName();
+			if (!result.containsKey(key)) result.put(key, new Integer(0));
+			int value = result.get(key).intValue()+1;
+			result.put(key, new Integer(value));
+		}
+		return result;
+	}
+	
+	private static void printMap(Map map) {
+		for (Object k: map.keySet()) {
+			System.out.println(k + " => " + map.get(k));
+		}
+	}
+	
+	/**
+	 * Builds a dictionary (HashMap) where the key is the annotation annotationHash and the value the annotation itself.
+	 * Annotations with no hash are skipped
+	 * @param entry
+	 * @return a dictionary of annotations where the key is the annotation hash (= identifier in BED world)
+	 */
+	public static Map<String,Annotation> getHashAnnotationMap(Entry entry) {
+		
+		//printMap(getAnnotationCategoryCountMap(entry));
+		
+		Map<String,Annotation> result = new HashMap<String,Annotation>();
+		for (Annotation annot: entry.getAnnotations()) {
+			if (annot.getAnnotationHash() != null && ! annot.getAnnotationHash().isEmpty()) {
+				result.put(annot.getAnnotationHash(), annot);
+			}
+		}
+		return result;
+	}
+		
+	/**
+	 * Returns a dictionary mapping proteoforms to their annotations.
+	 * The key is the proteoform, the value the list of annotations related to it.
+	 * Note that the method is "isoform "aware": only annotations having 
+	 * an AnnotationIsoformSpecificity record in getTargetingIsoformsMap 
+	 * for the isoformAc specified in the parameter are taken into account
+	 * @param entry
+	 * @param isoformAc
+	 * @return
+	 */
+	public static Map<Proteoform,List<Annotation>> getProteoformAnnotationsMap(Entry entry, String isoformAc) {
+		
+		Map<Proteoform,List<Annotation>> result = new HashMap<Proteoform,List<Annotation>>();
+		for (Annotation annot: entry.getAnnotations()) {
+			if (annot.isProteoformAnnotation()) {
+				if (annot.getTargetingIsoformsMap().containsKey(isoformAc)) {
+					Proteoform key = new Proteoform(isoformAc, annot.getSubjectName(), annot.getSubjectComponents());
+					if (!result.containsKey(key)) result.put(key, new ArrayList<Annotation>());
+					result.get(key).add(annot);
+				}
+			}
+		}
+		return result;
+	}
+	
 
 	public static List<String> getFunctionInfoWithCanonicalFirst(Entry entry) {
 		List<String> fInfoCanonical = new  ArrayList<String>();
--- core/src/main/java/org/nextprot/api/core/domain/annotation/AnnotationEvidence.java
+++ core/src/main/java/org/nextprot/api/core/domain/annotation/AnnotationEvidence.java
@@ -40,6 +40,8 @@ public class AnnotationEvidence implements Serializable {
 	private String evidenceCodeName;
 	private String evidenceCodeOntology;
 
+	private String note;
+
 	public String getEvidenceCodeAC() {
 		return evidenceCodeAC;
 	}
@@ -431,5 +433,13 @@ public class AnnotationEvidence implements Serializable {
 
 	public void setEvidenceCodeOntology(String ontology) {
 		this.evidenceCodeOntology = ontology;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
 	}
 }
