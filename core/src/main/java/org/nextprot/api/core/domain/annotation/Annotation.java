b2ec5ba6f0d345a1887c6062fa4f281454bff5c6
Daniel <ddtxra@gmail.com>
2016-08-22 22:07:25 +0200

    turning subject name deprecated

--- core/src/main/java/org/nextprot/api/core/domain/annotation/Annotation.java
+++ core/src/main/java/org/nextprot/api/core/domain/annotation/Annotation.java
@@ -14,6 +14,7 @@ import java.util.*;
 //I would love to do @JsonInclude(Include.NON_NULL)
 public class Annotation implements Serializable, IsoformSpecific {
 
+	@Deprecated
 	private String subjectName;
 	private List<String> subjectComponents;
 	
@@ -417,10 +418,12 @@ public class Annotation implements Serializable, IsoformSpecific {
 		return booleanOptional;
 	}
 	
+	@Deprecated
 	public String getSubjectName() {
 		return subjectName;
 	}
 
+	@Deprecated
 	public void setSubjectName(String subjectName) {
 		this.subjectName = subjectName;
 	}
