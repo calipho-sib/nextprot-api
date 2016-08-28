d7e31d52be2b350026f543d22df1a829f80390bc
Daniel <ddtxra@gmail.com>
2016-08-29 01:09:55 +0200

    adding integration tests for RDF

--- rdf/src/main/java/org/nextprot/api/rdf/controller/SparqlController.java
+++ rdf/src/main/java/org/nextprot/api/rdf/controller/SparqlController.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,6 +49,11 @@ public class SparqlController {
 	@ResponseBody
 	public List<String> sparqlPrefixes() {
 		return sparqlDictionary.getSparqlPrefixesList();
+	}
+
+	@RequestMapping(value ="/common-prefixes", produces = {"text/turtle"})
+	public String commonPrefixes(Model model) {
+		return "prefix"; //will get prefix.ttl.vm
 	}
 
 
---
+++ web/src/test/java/org/nextprot/api/integration/tests/rdf/PhenotypicTTLIntegrationTest.java
@@ -0,0 +1,80 @@
+package org.nextprot.api.integration.tests.rdf;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.query.MalformedQueryException;
+import org.eclipse.rdf4j.query.QueryEvaluationException;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.nextprot.api.rdf.utils.SparqlDictionary;
+import org.nextprot.api.web.dbunit.base.mvc.WebIntegrationBaseTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+
+public class PhenotypicTTLIntegrationTest extends WebIntegrationBaseTest {
+
+	@Autowired
+	private SparqlDictionary sparqlDictionary;
+	
+
+	private Repository testRepo = null;
+
+	@Before
+	public void init() throws Exception {
+		
+		String prefixes = this.mockMvc.perform(get("/common-prefixes.ttl")).andReturn().getResponse().getContentAsString();
+
+		// Prepare repository
+		String ttlContent = this.mockMvc.perform(get("/entry/NX_Q15858.ttl")).andReturn().getResponse().getContentAsString();
+		// Daniel: Missing terminologies and other things I guess...
+
+		String ttl = prefixes + ttlContent;
+		//System.out.println(ttl);
+		
+		testRepo = new SailRepository(new MemoryStore());
+		testRepo.initialize();
+
+		InputStream stream = new ByteArrayInputStream((ttl).getBytes());
+		testRepo.getConnection().add(stream, "", RDFFormat.TURTLE, new Resource[] {});
+
+	}
+
+	private TupleQueryResult doTupleQuery(String sparqlQuery) throws RepositoryException, QueryEvaluationException, MalformedQueryException {
+
+		RepositoryConnection conn = testRepo.getConnection();
+		TupleQuery query = (TupleQuery) conn.prepareQuery(QueryLanguage.SPARQL, sparqlQuery);
+		TupleQueryResult result = query.evaluate();
+
+		return result;
+	}
+
+	@Test
+	public void shouldReturnCorrectTTLAndBeAbleToPerformAnRDFQuery() throws Exception {
+
+
+		// String sparqlQuery = sparqlDictionary.getSparqlOnly("NXQ_00147");
+		// //For phenotypic
+		String sparqlQuery = "SELECT (COUNT(*) AS ?no) where { ?s ?p ?o  }";
+		TupleQueryResult result = doTupleQuery(sparqlQuery);
+		//System.err.println(result.getBindingNames().iterator().next());
+		long numberOfTriples = Long.valueOf(result.next().getBinding("no").getValue().stringValue());
+		Assert.assertTrue(numberOfTriples > 1000);
+		result.close();
+
+	}
+
+}
