d46d1402a4a8db381f5f62a389bbdd4d9c45ebf1
fnikitin <frederic.nikitin@gmail.com>
2016-08-30 18:07:56 +0200

    Change contract for AnnotationBaseMerger (2 mergeable annotations maximum)

--- core/src/main/java/org/nextprot/api/core/utils/annot/merge/impl/AnnotationBaseMerger.java
+++ core/src/main/java/org/nextprot/api/core/utils/annot/merge/impl/AnnotationBaseMerger.java
@@ -4,58 +4,48 @@ import org.nextprot.api.core.domain.annotation.Annotation;
 import org.nextprot.api.core.utils.annot.merge.AnnotationMerger;
 import org.nextprot.commons.constants.QualityQualifier;
 
-import java.util.List;
-
 public abstract class AnnotationBaseMerger implements AnnotationMerger {
 
     @Override
-    public Annotation merge(Annotation annotation1, Annotation annotation2, Annotation... others) {
+    public Annotation merge(Annotation annotation1, Annotation annotation2) {
 
-        Annotation dest = getDestAnnotation(annotation1, annotation2, others);
-        List<Annotation> sources = getSourceAnnotations(annotation1, annotation2, others);
+        Annotation dest = getDestAnnotation(annotation1, annotation2);
+        Annotation source = getSourceAnnotation(annotation1, annotation2);
 
-        updateDestEvidences(dest, sources);
-        updateDestAnnotationHash(dest, sources);
-        updateDestAnnotationName(dest, sources);
-        updateDestIsoformSpecificityName(dest, sources);
-        updateDestQualityQualifier(dest, sources);
-        updateDestBioObject(dest, sources);
+        updateDestEvidences(dest, source);
+        updateDestAnnotationHash(dest, source);
+        updateDestIsoformSpecificityName(dest, source);
+        updateDestQualityQualifier(dest, source);
+        updateDestBioObject(dest, source);
 
         return dest;
     }
 
     /** Get annotation where all sources should be merged to */
-    protected abstract Annotation getDestAnnotation(Annotation annotation1, Annotation annotation2, Annotation... others);
+    protected abstract Annotation getDestAnnotation(Annotation annotation1, Annotation annotation2);
 
-    /** Get source annotations */
-    protected abstract List<Annotation> getSourceAnnotations(Annotation annotation1, Annotation annotation2, Annotation... others);
+    /** Get source annotation */
+    protected abstract Annotation getSourceAnnotation(Annotation annotation1, Annotation annotation2);
 
     /** Update dest evidences with sources evidences */
-    protected abstract void updateDestEvidences(Annotation dest, List<Annotation> sources);
+    protected abstract void updateDestEvidences(Annotation dest, Annotation source);
 
     /** Update dest annotation hash */
-    protected abstract void updateDestAnnotationHash(Annotation dest, List<Annotation> sources);
-
-    /** Update dest annotation name */
-    protected abstract void updateDestAnnotationName(Annotation dest, List<Annotation> sources);
+    protected abstract void updateDestAnnotationHash(Annotation dest, Annotation source);
 
     /** Update dest isoform specificity name (variant name) */
-    protected abstract void updateDestIsoformSpecificityName(Annotation dest, List<Annotation> sources);
+    protected abstract void updateDestIsoformSpecificityName(Annotation dest, Annotation source);
 
     /** Reset dest qualityqualifier to gold if there is at least one gold source */
-    private void updateDestQualityQualifier(Annotation dest, List<Annotation> sources) {
+    private void updateDestQualityQualifier(Annotation dest, Annotation source) {
 
         if (dest.getQualityQualifier() == null || QualityQualifier.valueOf(dest.getQualityQualifier()) != QualityQualifier.GOLD) {
 
-            for (Annotation source : sources) {
-
-                if (source.getQualityQualifier() != null && source.getQualityQualifier().equals(QualityQualifier.GOLD.name())) {
-                    dest.setQualityQualifier(QualityQualifier.GOLD.name());
-                    break;
-                }
+            if (source.getQualityQualifier() != null && source.getQualityQualifier().equals(QualityQualifier.GOLD.name())) {
+                dest.setQualityQualifier(QualityQualifier.GOLD.name());
             }
         }
     }
 
-    protected abstract void updateDestBioObject(Annotation dest, List<Annotation> sources);
+    protected abstract void updateDestBioObject(Annotation dest, Annotation source);
 }
