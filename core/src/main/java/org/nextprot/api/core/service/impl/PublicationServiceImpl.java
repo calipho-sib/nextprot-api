13160d6639db3439db0ecb312d47aa2ba4ddbc44
fnikitin <frederic.nikitin@gmail.com>
2016-09-07 14:59:42 +0200

    Fixing author-list, xref bug

--- core/src/main/java/org/nextprot/api/core/service/impl/PublicationServiceImpl.java
+++ core/src/main/java/org/nextprot/api/core/service/impl/PublicationServiceImpl.java
@@ -64,43 +64,56 @@ public class PublicationServiceImpl implements PublicationService {
 	@Cacheable("publications")
 	public List<Publication> findPublicationsByMasterUniqueName(String uniqueName) {
 
-		List<Publication> publications =
-				publicationDao.findSortedPublicationsByMasterId(masterIdentifierDao.findIdByUniqueName(uniqueName));
-		
+		List<Publication> npPublications = publicationDao.findSortedPublicationsByMasterId(masterIdentifierDao.findIdByUniqueName(uniqueName));
+		Map<Long, List<PublicationDbXref>> npPublicationsXrefs = updateMissingPublicationFields(npPublications);
+
+		// Getting publications from nx flat database
+		List<String> nxFlatPubmedIds = this.statementDao.
+				findAllDistinctValuesforFieldWhereFieldEqualsValues(StatementField.REFERENCE_ACCESSION,
+						StatementField.ENTRY_ACCESSION, uniqueName, StatementField.REFERENCE_DATABASE, "PubMed");
+
+		List<Publication> nxflatPublications = getPublicationsFromPubmedIds(nxFlatPubmedIds, npPublicationsXrefs);
+		updateMissingPublicationFields(nxflatPublications);
+
+		//returns a immutable list when the result is cacheable (this prevents modifying the cache, since the cache returns a reference) copy on read and copy on write is too much time consuming
+		return new ImmutableList.Builder<Publication>().addAll(npPublications).addAll(nxflatPublications).build();
+	}
+
+	private Map<Long, List<PublicationDbXref>> updateMissingPublicationFields(List<Publication> publications) {
+
 		List<Long> publicationIds = publications.stream().map(Publication::getPublicationId).collect(Collectors.toList());
-		
-		Map<Long, List<PublicationDbXref>> xrefMap = dbXrefService.findDbXRefByPublicationIds(publicationIds).stream()
-				.collect(Collectors.groupingBy(PublicationDbXref::getPublicationId));
 
 		Map<Long, List<PublicationAuthor>> authorMap = authorDao.findAuthorsByPublicationIds(publicationIds).stream()
 				.collect(Collectors.groupingBy(PublicationAuthor::getPublicationId));
 
-		publications.addAll(getPublicationsFromNxflat(uniqueName, xrefMap));
+		Map<Long, List<PublicationDbXref>> xrefMap = dbXrefService.findDbXRefByPublicationIds(publicationIds).stream()
+				.collect(Collectors.groupingBy(PublicationDbXref::getPublicationId));
 
 		for (Publication publication : publications) {
+
 			setAuthorsAndEditors(publication, authorMap.get(publication.getPublicationId()));
 			setXrefs(publication, xrefMap.get(publication.getPublicationId()));
 		}
 
-		//returns a immutable list when the result is cacheable (this prevents modifying the cache, since the cache returns a reference) copy on read and copy on write is too much time consuming
-		return new ImmutableList.Builder<Publication>().addAll(publications).build();
+		return xrefMap;
 	}
 
-	private List<Publication> getPublicationsFromNxflat(String uniqueName, Map<Long, List<PublicationDbXref>> np1PublicationXrefs) {
+	/**
+	 * Get all publications not found in npPublication from pubmedids
+	 * @param nxflatPubmedIds entry name
+	 * @param npPublicationXrefs needed to avoid loading pubmed id publication multiple times
+	 * @return
+	 */
+	private List<Publication> getPublicationsFromPubmedIds(List<String> nxflatPubmedIds, Map<Long, List<PublicationDbXref>> npPublicationXrefs) {
 
 		List<Publication> nxflatPublications = new ArrayList<>();
 
-		// Getting publications from flat database
-		List<String> pubmedIds = this.statementDao.
-				findAllDistinctValuesforFieldWhereFieldEqualsValues(StatementField.REFERENCE_ACCESSION,
-						StatementField.ENTRY_ACCESSION, uniqueName, StatementField.REFERENCE_DATABASE, "PubMed");
-
-		// Searching publication where ids already found in np1 publications
-		List<Long> foundPublicationIds = np1PublicationXrefs.keySet().stream()
-				.filter(pubid -> np1PublicationXrefs.get(pubid).stream().anyMatch(xref -> pubmedIds.contains(xref.getAccession())))
+		// Filtering publications which pubmed was not already found in np publications
+		List<Long> foundPublicationIds = npPublicationXrefs.keySet().stream()
+				.filter(pubid -> npPublicationXrefs.get(pubid).stream().anyMatch(xref -> nxflatPubmedIds.contains(xref.getAccession())))
 				.collect(Collectors.toList());
 
-		pubmedIds.stream()
+		nxflatPubmedIds.stream()
 				.filter(pubmed -> pubmed != null)
 				.forEach(pubmed -> {
 					Publication pub = this.publicationDao.findPublicationByDatabaseAndAccession("PubMed", pubmed);
