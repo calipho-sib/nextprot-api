0d993df5a4d2d6e0daa84d990005c9c20c3deb79
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2016-11-03 17:48:15 +0100

    fixes case 1/2 of CALIPHOMISH-1139: Reactome evidences & annotations are now SILVER

--- core/src/test/java/org/nextprot/api/core/service/DbXrefServiceIntegrationTest.java
+++ core/src/test/java/org/nextprot/api/core/service/DbXrefServiceIntegrationTest.java
@@ -59,7 +59,7 @@ having sum(a.cnt)=1
 		Annotation annot = annotations.get(0);
 		assertTrue(annot.getCategory().equals(AnnotationCategory.PATHWAY.getDbAnnotationTypeName()));
 		assertTrue(annot.getAPICategory()== AnnotationCategory.PATHWAY);
-		assertTrue(annot.getQualityQualifier().equals("GOLD"));
+		assertTrue(annot.getQualityQualifier().equals("SILVER"));
 		Assert.assertEquals("Intraflagellar transport", annot.getDescription());
 		for (AnnotationIsoformSpecificity spec: annot.getTargetingIsoformsMap().values()) {
 			assertTrue(spec.getSpecificity().equals("UNKNOWN"));
