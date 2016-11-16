436d75e37e651205199c00d1e3388a67fcf9c78b
fnikitin <frederic.nikitin@gmail.com>
2016-11-16 18:42:04 +0100

    New handler to propagate multiple features defined in POST request

--- isoform-mapper/src/main/java/org/nextprot/api/isoform/mapper/controller/IsoformMappingController.java
+++ isoform-mapper/src/main/java/org/nextprot/api/isoform/mapper/controller/IsoformMappingController.java
@@ -1,24 +1,18 @@
 package org.nextprot.api.isoform.mapper.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.jsondoc.core.annotation.Api;
-import org.jsondoc.core.annotation.ApiMethod;
-import org.jsondoc.core.annotation.ApiPathParam;
-import org.jsondoc.core.annotation.ApiQueryParam;
+import org.jsondoc.core.annotation.*;
 import org.jsondoc.core.pojo.ApiVerb;
 import org.nextprot.api.isoform.mapper.domain.FeatureQueryResult;
+import org.nextprot.api.isoform.mapper.domain.MultipleFeatureQuery;
 import org.nextprot.api.isoform.mapper.service.IsoformMappingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Lazy
 @Controller
@@ -33,7 +27,7 @@ public class IsoformMappingController {
 	@ResponseBody
 	public FeatureQueryResult validateIsoformFeature(
 			@ApiPathParam(name = "category", description = "A feature category (either 'variant', 'mutagenesis' or 'ptm').",  allowedvalues = { "variant" })
-			@PathVariable("category") String featureCategory, HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("category") String featureCategory,
 			@ApiQueryParam(name = "feature", description = "An isoform feature ('variant' category uses HGVS format (http://varnomen.hgvs.org/recommendations/protein)).",  allowedvalues = { "SCN11A-p.Leu1158Pro" })
 			@RequestParam(value = "feature") String feature,
 			@ApiQueryParam(name = "accession", description = "An optional nextprot entry accession (deduced from feature gene name if undefined).",  allowedvalues = { })
@@ -47,13 +41,79 @@ public class IsoformMappingController {
 	@ResponseBody
 	public FeatureQueryResult propagateIsoformFeature(
 			@ApiPathParam(name = "category", description = "A feature category (either 'variant', 'mutagenesis' or 'ptm').",  allowedvalues = { "variant" })
-			@PathVariable("category") String featureCategory, HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("category") String featureCategory,
 			@ApiQueryParam(name = "feature", description = "An isoform feature ('variant' category uses HGVS format (http://varnomen.hgvs.org/recommendations/protein)).",  allowedvalues = { "SCN11A-p.Leu1158Pro" })
 			@RequestParam(value = "feature") String feature,
 			@ApiQueryParam(name = "accession", description = "An optional nextprot entry accession (deduced from feature gene name if undefined).",  allowedvalues = { })
 			@RequestParam(value = "accession", required = false) String nextprotAccession) {
 
 		return isoformMappingService.propagateFeature(feature, featureCategory, nextprotAccession);
+	}
+
+	/*
+	{
+		"featureType": "variant",
+		"featureList": [
+			"SCN11A-p.Leu1158Pro",
+			"SCN11A-p.Leu1158Pro"
+		],
+		"accession": "NX_Q9UI33" // feature list accession: optional if deducible from gene defined in feature
+	}
+
+	or
+
+	{
+		"featureType": "variant",
+		"featureMaps": [
+			{
+				"feature": "SCN11A-p.Leu1158Pro",
+				"accession": "NX_Q9UI33"
+			},
+			{
+				"feature": "SCN11A-p.Leu1158Pro",
+				"accession": "NX_Q9UI33"
+			}
+		]
+	}
+
+	or both
+
+	{
+		"featureType": "variant",
+		"featureList": [
+			"SCN11A-p.Leu1158Pro",
+			"SCN11A-p.Leu1158Pro"
+		],
+		"accession": "NX_Q9UI33",
+		"featureMaps": [
+			{
+				"feature": "SCN11A-p.Leu1158Pro",
+				"accession": "NX_Q9UI33" // optional if deducible from gene defined in feature
+			},
+			{
+				"feature": "SCN11A-p.Leu1158Pro",
+				"accession": "NX_Q9UI33"
+			}
+		]
+	}
+	 */
+
+	@ApiMethod(path = "/propagate-features", verb = ApiVerb.POST, description = "Validate isoform feature list and compute feature propagations on other isoforms", produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = { MediaType.APPLICATION_JSON_VALUE})
+	@RequestMapping(value = "/propagate-features", method = { RequestMethod.POST }, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody
+	public Map<String, FeatureQueryResult> propagateIsoformFeaturePost(@RequestBody @ApiBodyObject MultipleFeatureQuery multipleFeatureQuery) {
+
+		Map<String, FeatureQueryResult> results = new HashMap<>(multipleFeatureQuery.getFeatureList().size()+multipleFeatureQuery.getFeatureMaps().size());
+
+		multipleFeatureQuery.getFeatureList().stream()
+				.filter(feature -> !results.containsKey(feature))
+				.forEach(feature -> results.put(feature, isoformMappingService.propagateFeature(feature, multipleFeatureQuery.getFeatureType(), multipleFeatureQuery.getAccession())));
+
+		multipleFeatureQuery.getFeatureMaps().stream()
+				.filter(featureQuery -> !results.containsKey(featureQuery.get("feature")))
+				.forEach(featureQuery -> results.put(featureQuery.get("feature"), isoformMappingService.propagateFeature(featureQuery.get("feature"), multipleFeatureQuery.getFeatureType(), featureQuery.get("accession"))));
+
+		return results;
 	}
 }
 
