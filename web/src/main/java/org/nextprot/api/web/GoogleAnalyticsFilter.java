fdb5d2ae59b774fe3ae0ee33d018323f44b71d80
Daniel <ddtxra@gmail.com>
2016-11-17 17:30:03 +0100

    adding GA tracking filter

--- web/src/main/java/org/nextprot/api/web/GoogleAnalyticsFilter.java
+++ web/src/main/java/org/nextprot/api/web/GoogleAnalyticsFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.brsanthu.googleanalytics.GoogleAnalytics;
@@ -19,40 +20,49 @@ import com.google.common.base.Optional;
 
 public class GoogleAnalyticsFilter extends OncePerRequestFilter {
 
+	private GoogleAnalytics ga = null;
 	private final Log Logger = LogFactory.getLog(GoogleAnalyticsFilter.class);
-	
-	private String trackingId = "UA-85488642-1";
-	
-	private GoogleAnalytics ga = new GoogleAnalytics(trackingId);
 
-	public GoogleAnalyticsFilter() {
-		logger.debug("Google Analytics filter initialized");
+	private String gaTrackingID = null;
+	private static final String GA_TRACKING_ID_SYS_PROP = "GATrackingId";
+	
+	@Override
+	public void initFilterBean() {
+		if(System.getProperty(GA_TRACKING_ID_SYS_PROP) != null){
+			gaTrackingID = System.getProperty(GA_TRACKING_ID_SYS_PROP);
+		}
+		if(gaTrackingID != null){
+			ga = new GoogleAnalytics(gaTrackingID);
+			Logger.info("Google Analytics filter initialized with " + gaTrackingID);
+		}else {
+			Logger.info("Google Analytics not initialized because -D" + GA_TRACKING_ID_SYS_PROP + "=UA-17852148-3 system property was not found.");
+		}
 	}
 
-	private GoogleAnalyticsRequest<?> generateHit(HttpServletRequest request){
-	
+	private GoogleAnalyticsRequest<?> generateHit(HttpServletRequest request) {
+
 		PageViewHit hit = new PageViewHit(request.getRequestURL().toString(), request.getPathInfo());
 
 		String clientId = getClientId(request).toString();
-		Logger.debug("Client Id" + clientId);
+		//Logger.debug("Client Id" + clientId);
 		Logger.debug("Sending hit: " + request.getRequestURL().toString());
-		
+
 		hit.clientId(getClientId(request).toString());
-		
-		//Overriding IP if present from the client
+
+		// Overriding IP if present from the client
 		Optional<String> ip = getClientIP(request);
-		if(ip.isPresent()){
+		if (ip.isPresent()) {
 			hit.userIp(ip.get());
 		}
-		
+
 		return hit;
 
 	}
-	
+
 	private void sendToGoogleAnalytics(HttpServletRequest request) {
 		try {
 			ga.postAsync(generateHit(request));
-		} catch (Exception e){
+		} catch (Exception e) {
 			Logger.error("Failed to send to GA" + e.getMessage());
 		}
 	}
@@ -60,32 +70,27 @@ public class GoogleAnalyticsFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
 
-        if(!"OPTIONS".equals(req.getMethod())){
-    		sendToGoogleAnalytics(req);
-        }
-        
+		if ((ga != null)  && (!"OPTIONS".equals(req.getMethod()))) {
+			sendToGoogleAnalytics(req);
+		}
+
 		chain.doFilter(req, resp);
 	}
 
-	public String getTrackingId() {
-		return trackingId;
-	}
-
-	public void setTrackingId(String trackingId) {
-		this.trackingId = trackingId;
-	}
-	
 	public UUID getClientId(HttpServletRequest request) {
 
-		// Keeping just IP + agent method, because some methods may require authentication and others not. And therefore different UUID would be generated.
+		// Keeping just IP + agent method, because some methods may require
+		// authentication and others not. And therefore different UUID would be
+		// generated.
 		UUID id = getClientUniqueIdentifier(request);
-		//Logger.debug("Found UUID " + id + " based on custom headers");
+		// Logger.debug("Found UUID " + id + " based on custom headers");
 		return id;
 
-	}	
+	}
 
 	/**
 	 * Get a client unique identifier created using the headers
+	 * 
 	 * @param request
 	 * @return
 	 */
@@ -100,28 +105,29 @@ public class GoogleAnalyticsFilter extends OncePerRequestFilter {
 		sb.append(request.getRemoteHost() + "; ");
 		sb.append(request.getRemoteUser() + "; ");
 		sb.append(request.getRemoteAddr() + "; ");
-		
+
 		Logger.debug("Building UI based on string " + sb.toString());
-		
+
 		return UUID.nameUUIDFromBytes(sb.toString().getBytes());
-		
+
 	}
 
 	/**
 	 * Gets the IP of the real client
+	 * 
 	 * @param request
 	 * @return
 	 */
 	public Optional<String> getClientIP(HttpServletRequest request) {
 
-		String ip = request.getHeader("x-forwarded-for"); //May differ from implementations
-		if(ip != null){
-			return Optional.of(ip); 
-		}else {
+		String ip = request.getHeader("x-forwarded-for"); // May differ from
+															// implementations
+		if (ip != null) {
+			return Optional.of(ip);
+		} else {
 			return Optional.absent();
 		}
-		
-	}
 
+	}
 
 }
