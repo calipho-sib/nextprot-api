3375c2978650cc5b1333aa6be6e1b508c4676077
fnikitin <frederic.nikitin@gmail.com>
2016-12-05 13:33:42 +0100

    Rename controller to Blast

--- blast/src/main/java/org/nextprot/api/blast/controller/BlastPController.java
+++ blast/src/main/java/org/nextprot/api/blast/controller/BlastPController.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@Api(name = "BlastP", description = "Search protein sequence into neXtProt database.", group = "Tools")
+@Api(name = "Blast", description = "Search protein sequence into neXtProt database.", group = "Tools")
 public class BlastPController {
 
 	@Autowired
