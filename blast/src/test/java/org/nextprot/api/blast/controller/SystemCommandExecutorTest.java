54800acfd722a3b02c71b8accfd3bd0c534b6c31
fnikitin <frederic.nikitin@gmail.com>
2016-12-06 13:07:22 +0100

    Fixing tests

--- blast/src/test/java/org/nextprot/api/blast/controller/SystemCommandExecutorTest.java
+++ blast/src/test/java/org/nextprot/api/blast/controller/SystemCommandExecutorTest.java
@@ -1,7 +1,9 @@
 package org.nextprot.api.blast.controller;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +13,7 @@ public class SystemCommandExecutorTest {
     public void executeCommand() throws Exception {
 
         List<String> command = new ArrayList<>();
-        command.add("lsd");
+        command.add("ls");
         command.add("-l");
         command.add("/var/tmp");
 
@@ -21,11 +23,21 @@ public class SystemCommandExecutorTest {
         StringBuilder stdout = commandExecutor.getStandardOutputFromCommand();
         StringBuilder stderr = commandExecutor.getStandardErrorFromCommand();
 
-        System.out.println("$? "+result);
-        System.out.println("STDOUT");
-        System.out.println(stdout);
-        System.out.println("STDERR");
-        System.out.println(stderr);
+        Assert.assertEquals(0, result);
+        Assert.assertTrue(!stdout.toString().isEmpty());
+        Assert.assertTrue(stderr.toString().isEmpty());
+    }
+
+    @Test(expected = IOException.class)
+    public void commandShouldFail() throws Exception {
+
+        List<String> command = new ArrayList<>();
+        command.add("lsd");
+        command.add("-l");
+        command.add("/var/tmp");
+
+        SystemCommandExecutor commandExecutor = new SystemCommandExecutor(command);
+        commandExecutor.executeCommand();
     }
 
 }
