e9e84497a02e8f0c3d2f7eb969976621e116cd34
fnikitin <frederic.nikitin@gmail.com>
2016-12-06 13:08:57 +0100

    Extracting methods for clarity sake

--- blast/src/main/java/org/nextprot/api/blast/domain/BlastPRunner.java
+++ blast/src/main/java/org/nextprot/api/blast/domain/BlastPRunner.java
@@ -3,18 +3,14 @@ package org.nextprot.api.blast.domain;
 import org.nextprot.api.blast.controller.SystemCommandExecutor;
 import org.nextprot.api.blast.domain.gen.*;
 import org.nextprot.api.commons.exception.NextProtException;
-import org.nextprot.api.core.domain.Overview;
-import org.nextprot.api.core.service.OverviewService;
+import org.nextprot.api.core.service.MainNamesService;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static com.hp.hpl.jena.sparql.vocabulary.VocabTestQuery.query;
 
@@ -31,46 +27,57 @@ public class BlastPRunner {
         this.config = config;
     }
 
-    public BlastResult run(String header, String sequence) throws NextProtException {
+    public BlastResult run(String header, String sequence) {
 
         return run(header, sequence, null);
     }
 
-    public BlastResult run(String header, String sequence, OverviewService overviewService) throws NextProtException {
+    public BlastResult run(String header, String sequence, MainNamesService mainNamesService) {
 
         try {
-            StringBuilder fasta = new StringBuilder();
-            fasta.append(">").append(header).append("\n").append(sequence);
+            writeTempQueryFile(buildFastaContent(header, sequence));
 
-            writeTempQueryFile(fasta.toString());
+            BlastResult blastResult = executeBlast(buildCommandLine());
 
-            SystemCommandExecutor commandExecutor = new SystemCommandExecutor(buildCommandLine());
-            commandExecutor.executeCommand();
+            if (mainNamesService != null)
+                new BlastResultUpdater(mainNamesService, sequence).update(blastResult);
 
-            StringBuilder stdout = commandExecutor.getStandardOutputFromCommand();
-            StringBuilder stderr = commandExecutor.getStandardErrorFromCommand();
-
-            if (!stderr.toString().isEmpty()) {
-                throw new NextProtException("BlastP error when executing " + query + ": " + stderr.toString());
-            }
-
-            if (!config.isDebugMode()) {
-                deleteTempQueryFile();
-            }
-
-            BlastResult blastResult = BlastResult.fromJson(stdout.toString());
-
-            if (overviewService == null)
-                return blastResult;
-
-            return new BlastOutputUpdater(overviewService, sequence).update(blastResult);
+            return blastResult;
         } catch (IOException | InterruptedException e) {
 
             throw new NextProtException("BlastP exception", e);
         }
     }
 
-    List<String> buildCommandLine() {
+    private String buildFastaContent(String header, String sequence) throws IOException {
+
+        StringBuilder fasta = new StringBuilder();
+        fasta
+                .append(">").append(header).append("\n")
+                .append(sequence);
+
+        return fasta.toString();
+    }
+
+    private BlastResult executeBlast(List<String> commandLine) throws IOException, InterruptedException {
+
+        SystemCommandExecutor commandExecutor = new SystemCommandExecutor(commandLine);
+        commandExecutor.executeCommand();
+
+        String stderr = commandExecutor.getStandardErrorFromCommand().toString();
+
+        if (!stderr.isEmpty()) {
+            throw new NextProtException("BlastP error when executing " + query + ": " + stderr);
+        }
+
+        if (!config.isDebugMode()) {
+            deleteTempQueryFile();
+        }
+
+        return BlastResult.fromJson(commandExecutor.getStandardOutputFromCommand().toString());
+    }
+
+    private List<String> buildCommandLine() {
 
         List<String> command = new ArrayList<>();
 
@@ -97,76 +104,5 @@ public class BlastPRunner {
     private void deleteTempQueryFile() throws IOException {
 
         Files.deleteIfExists(tempQueryFile.toPath());
-    }
-
-    /**
-     * Edit original blast output:
-     *
-     * 1. Remove useless fields (ex: blast paper reference)
-     * 2. Add some new fields (ex: sequence query, matching isoform accession,...).
-     */
-    private class BlastOutputUpdater {
-
-        private final OverviewService overviewService;
-        private final String sequence;
-
-        private BlastOutputUpdater(OverviewService overviewService, String sequence) {
-
-            this.overviewService = overviewService;
-            this.sequence = sequence;
-        }
-
-        private BlastResult update(BlastResult originalBlastResult) {
-
-            Report report = originalBlastResult.getBlastOutput2().get(0).getReport();
-
-            // remove useless informations
-            report.setProgram(null);
-            report.setReference(null);
-            report.setSearchTarget(null);
-
-            Search search = report.getResults().getSearch();
-
-            search.setQuerySeq(sequence);
-
-            Pattern titlePattern = Pattern.compile("^.+\\s+(NX_[^|]+)\\|(NX_.+)$");
-
-            // HITs
-            for (Hit hit : search.getHits()) {
-
-                // DESCRIPTIONs
-                for (Description desc : hit.getDescription()) {
-
-                    // remove useless infos
-                    desc.setId(null);
-                    desc.setAccession(null);
-
-                    String old = desc.getTitle();
-                    Matcher matcher = titlePattern.matcher(old);
-                    if (matcher.find()) {
-
-                        String isoformAccession = matcher.group(1);
-                        String entryAccession = matcher.group(2);
-
-                        Overview overview = overviewService.findOverviewByEntry(entryAccession);
-
-                        desc.setTitle(overview.getMainProteinName()+" ("+overview.getMainGeneName()+") ["+isoformAccession+"]");
-                        desc.setEntryAccession(entryAccession);
-                        desc.setIsoAccession(isoformAccession);
-                        desc.setProteinName(overview.getMainProteinName());
-                        desc.setGeneName(overview.getMainGeneName());
-                    }
-                }
-
-                for (Hsp hsp : hit.getHsps()) {
-
-                    float identityPercent = (float)hsp.getIdentity()/search.getQueryLen()*100;
-
-                    hsp.setIdentityPercent(Float.parseFloat(new DecimalFormat("##.##").format(identityPercent)));
-                }
-            }
-
-            return originalBlastResult;
-        }
     }
 }
