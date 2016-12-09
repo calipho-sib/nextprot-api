29a9860598ddd1b904da0a3403d12ffac2554ac3
fnikitin <frederic.nikitin@gmail.com>
2016-12-09 18:05:00 +0100

    New DAO that return a map of all isoform sequences

--- blast/src/main/java/org/nextprot/api/blast/dao/impl/BlastDAOImpl.java
+++ blast/src/main/java/org/nextprot/api/blast/dao/impl/BlastDAOImpl.java
@@ -1,17 +1,41 @@
 package org.nextprot.api.blast.dao.impl;
 
 import org.nextprot.api.blast.dao.BlastDAO;
+import org.nextprot.api.commons.spring.jdbc.DataSourceServiceLocator;
+import org.nextprot.api.commons.utils.SQLDictionary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
 public class BlastDAOImpl implements BlastDAO {
 
+    @Autowired
+    private SQLDictionary sqlDictionary;
+
+    @Autowired
+    private DataSourceServiceLocator dsLocator;
+
     @Override
     public Map<String, String> getAllIsoformSequences() {
 
-        return new HashMap<>();
+        Map<String, String> map = new HashMap<>();
+
+        List<Map<String,Object>> results = new JdbcTemplate(dsLocator.getDataSource())
+                .queryForList(sqlDictionary.getSQLQuery("all-isoaccessions-with-sequence"));
+
+        for (Map<String,Object> row : results) {
+
+            String isoAccession = (String) row.get("iso_accession");
+            String bioSequence = (String) row.get("bio_sequence");
+
+            map.put(isoAccession, bioSequence);
+        }
+
+        return map;
     }
 }
