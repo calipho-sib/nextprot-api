65ac312d74b2175680ab3f3be9e3bbd380f7545b
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2017-01-31 18:40:47 +0100

    removed sys out

--- core/src/main/java/org/nextprot/api/core/utils/dbxref/GenevisibleXrefURLResolver.java
+++ core/src/main/java/org/nextprot/api/core/utils/dbxref/GenevisibleXrefURLResolver.java
@@ -5,9 +5,7 @@ import org.nextprot.api.core.domain.DbXref;
 class GenevisibleXrefURLResolver extends DbXrefURLBaseResolver {
     
     @Override protected String getTemplateURL(DbXref xref) {
-    	System.out.println("Hello pam");
         return "https://genevisible.com/tissues/HS/UniProt/%s";
-
     }
 
 
