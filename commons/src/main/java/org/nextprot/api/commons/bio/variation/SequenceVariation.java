85474c388b8ec12d9f3ced1e7c8b4498b2757ffd
fnikitin <frederic.nikitin@gmail.com>
2017-03-30 15:51:23 +0200

    Document contracts

--- commons/src/main/java/org/nextprot/api/commons/bio/variation/SequenceVariation.java
+++ commons/src/main/java/org/nextprot/api/commons/bio/variation/SequenceVariation.java
@@ -2,19 +2,35 @@ package org.nextprot.api.commons.bio.variation;
 
 import org.nextprot.api.commons.bio.AminoAcidCode;
 
+/**
+ * A {@code SequenceVariation} describes a variation of a protein sequence.
+ *
+ * It is composed of 2 parts:
+ *
+ * <ol>
+ * <li>the changing part defining and locating the amino-acids that change</li>
+ * <li>the variation itself</li>
+ * </ol>
+ */
 public interface SequenceVariation {
 
+    /** @return the first amino-acid affected by the change */
     AminoAcidCode getFirstChangingAminoAcid();
 
+    /** @return the first amino-acid position affected by the change */
     int getFirstChangingAminoAcidPos();
 
+    /** @return the last amino-acid affected by the change */
     AminoAcidCode getLastChangingAminoAcid();
 
+    /** @return the last amino-acid position affected by the change */
     int getLastChangingAminoAcidPos();
 
-    default boolean isMultipleChangingAminoAcids() {
-         return getLastChangingAminoAcidPos()-getFirstChangingAminoAcidPos() > 0;
-    }
+    /** @return the protein sequence change itself */
+    SequenceChange<?> getSequenceChange();
 
-    SequenceChange getSequenceChange();
+    /** @return true if many amino-acids are affected by the change */
+    default boolean isMultipleChangingAminoAcids() {
+        return getLastChangingAminoAcidPos()-getFirstChangingAminoAcidPos() > 0;
+    }
 }
