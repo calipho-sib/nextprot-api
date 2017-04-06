75ffceec91853ac3f9bd06a4a25e9b18103a0703
fnikitin <frederic.nikitin@gmail.com>
2017-04-06 12:26:35 +0200

    Interface renamed and find method now returns Optional

--- core/src/main/java/org/nextprot/api/core/utils/annot/merge/impl/AnnotationClusterFinder.java
+++ core/src/main/java/org/nextprot/api/core/utils/annot/merge/impl/AnnotationClusterFinder.java
@@ -3,17 +3,18 @@ package org.nextprot.api.core.utils.annot.merge.impl;
 import com.google.common.base.Preconditions;
 import org.nextprot.api.core.domain.annotation.Annotation;
 import org.nextprot.api.core.utils.annot.merge.AnnotationCluster;
-import org.nextprot.api.core.utils.annot.merge.Finder;
+import org.nextprot.api.core.utils.annot.merge.AnnotationContainerFinder;
 import org.nextprot.api.core.utils.annot.merge.SimilarityPredicate;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Find cluster containing similar annotations
  *
  * Created by fnikitin on 02/08/16.
  */
-public class AnnotationClusterFinder implements Finder<AnnotationCluster> {
+public class AnnotationClusterFinder implements AnnotationContainerFinder<AnnotationCluster> {
 
     private final SimilarityPredicate criteria;
 
@@ -25,16 +26,16 @@ public class AnnotationClusterFinder implements Finder<AnnotationCluster> {
     }
 
     @Override
-    public AnnotationCluster find(Annotation searchedAnnotation, Collection<AnnotationCluster> annotationClusters) {
+    public Optional<AnnotationCluster> find(Annotation searchedAnnotation, Collection<AnnotationCluster> annotationClusters) {
 
         for (AnnotationCluster annotationCluster : annotationClusters) {
 
             for (Annotation annotation : annotationCluster.getAnnotations()) {
                 if (criteria.isSimilar(searchedAnnotation, annotation))
-                    return annotationCluster;
+                    return Optional.of(annotationCluster);
             }
         }
 
-        return null;
+        return Optional.empty();
     }
 }
--- core/src/main/java/org/nextprot/api/core/utils/annot/merge/impl/AnnotationFinder.java
+++ core/src/main/java/org/nextprot/api/core/utils/annot/merge/impl/AnnotationFinder.java
@@ -2,17 +2,18 @@ package org.nextprot.api.core.utils.annot.merge.impl;
 
 import com.google.common.base.Preconditions;
 import org.nextprot.api.core.domain.annotation.Annotation;
-import org.nextprot.api.core.utils.annot.merge.Finder;
+import org.nextprot.api.core.utils.annot.merge.AnnotationContainerFinder;
 import org.nextprot.api.core.utils.annot.merge.SimilarityPredicate;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Find similar annotation in collection of annotations
  *
  * Created by fnikitin on 02/08/16.
  */
-public class AnnotationFinder implements Finder<Annotation> {
+public class AnnotationFinder implements AnnotationContainerFinder<Annotation> {
 
     private final SimilarityPredicate criteria;
 
@@ -26,17 +27,14 @@ public class AnnotationFinder implements Finder<Annotation> {
         this.criteria = criteria;
     }
 
-    /**
-     * @return the annotation found from a list of annotations or null if not found
-     */
     @Override
-    public Annotation find(Annotation searchedAnnotation, Collection<Annotation> annotations) {
+    public Optional<Annotation> find(Annotation searchedAnnotation, Collection<Annotation> annotations) {
 
         for (Annotation annotation : annotations) {
             if (criteria.isSimilar(searchedAnnotation, annotation))
-                return annotation;
+                return Optional.of(annotation);
         }
 
-        return null;
+        return Optional.empty();
     }
 }
