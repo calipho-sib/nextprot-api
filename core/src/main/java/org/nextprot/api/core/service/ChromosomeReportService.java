c2d2d2498e95b788be65cfaa6595ea7247c6442f
fnikitin <frederic.nikitin@gmail.com>
2017-04-21 10:17:11 +0200

    Editing javadoc

--- core/src/main/java/org/nextprot/api/core/service/ChromosomeReportService.java
+++ core/src/main/java/org/nextprot/api/core/service/ChromosomeReportService.java
@@ -5,7 +5,7 @@ import org.nextprot.api.core.domain.ChromosomeReport;
 public interface ChromosomeReportService {
 
 	/**
-	 * Report chromosome entries
+	 * Report informations about neXtProt entries and genes located on the given chromosome
 	 * @param chromosome the chromosome to get report
 	 * @return a pojo containing the report
 	 */
