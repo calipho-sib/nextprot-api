bfe2380ceeba193fea779e54989e09797c36b14d
Daniel <ddtxra@gmail.com>
2017-05-04 15:40:37 +0200

    adding SPARQL POST attribute to cache key

--- web/src/main/java/org/nextprot/api/web/controller/SparqlProxyController.java
+++ web/src/main/java/org/nextprot/api/web/controller/SparqlProxyController.java
@@ -1,23 +1,14 @@
 package org.nextprot.api.web.controller;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Properties;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import net.sf.ehcache.constructs.web.GenericResponseWrapper;
-import net.sf.ehcache.constructs.web.filter.SimplePageCachingFilter;
-import net.sf.ehcache.constructs.web.filter.SimplePageFragmentCachingFilter;
-import org.codehaus.plexus.util.StringOutputStream;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.core.env.SystemEnvironmentPropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.ServletWrappingController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Properties;
 
 @Controller
 public class SparqlProxyController extends ServletWrappingController implements InitializingBean{
--- web/src/main/java/org/nextprot/api/web/filter/SparqlProxyCachingFilter.java
+++ web/src/main/java/org/nextprot/api/web/filter/SparqlProxyCachingFilter.java
@@ -3,10 +3,13 @@ package org.nextprot.api.web.filter;
 import net.sf.ehcache.constructs.web.AlreadyCommittedException;
 import net.sf.ehcache.constructs.web.PageInfo;
 import net.sf.ehcache.constructs.web.filter.SimplePageCachingFilter;
+import org.apache.commons.io.IOUtils;
+import org.nextprot.api.web.domain.MultiReadHttpServletRequest;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by dteixeir on 28.04.17.
@@ -25,32 +28,31 @@ public class SparqlProxyCachingFilter extends SimplePageCachingFilter {
                             final HttpServletResponse response, final FilterChain chain)
             throws Exception {
 
-        if (response.isCommitted()) {
-            throw new AlreadyCommittedException(
-                    "Response already committed before doing buildPage.");
-        }
+        MultiReadHttpServletRequest multiReadRequest = new MultiReadHttpServletRequest(request);
 
+        if (response.isCommitted()) { throw new AlreadyCommittedException("Response already committed before doing buildPage.");}
         logRequestHeaders(request);
-        PageInfo pageInfo = buildPageInfo(request, response, chain);
 
+        PageInfo pageInfo = buildPageInfo(multiReadRequest, response, chain);
         if (response.isCommitted()) {
             throw new AlreadyCommittedException(
                     "Response already committed after doing buildPage"
                             + " but before writing response from PageInfo.");
         }
-        writeResponse(request, response, pageInfo);
+        writeResponse(multiReadRequest, response, pageInfo);
     }
 
     @Override
     protected String calculateKey(HttpServletRequest httpRequest) {
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append(httpRequest.getMethod()).append(httpRequest.getRequestURI()).append(httpRequest.getQueryString());
-        // TODO ADD POST DATA as part of the cache key
+        try {
+            stringBuffer.append(IOUtils.toString(httpRequest.getReader()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         String key = stringBuffer.toString();
         return key;
     }
-
-
-
 
 }
