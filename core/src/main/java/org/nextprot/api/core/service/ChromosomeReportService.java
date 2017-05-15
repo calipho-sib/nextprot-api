c2591f0bba821902f2febac07df3266d5f6fe272
fnikitin <frederic.nikitin@gmail.com>
2017-05-15 10:35:24 +0200

    New exception throwable when chromosome is not valid

--- core/src/main/java/org/nextprot/api/core/service/ChromosomeReportService.java
+++ core/src/main/java/org/nextprot/api/core/service/ChromosomeReportService.java
@@ -1,5 +1,6 @@
 package org.nextprot.api.core.service;
 
+import org.nextprot.api.commons.exception.NextProtException;
 import org.nextprot.api.core.domain.ChromosomeReport;
 
 import java.util.Arrays;
@@ -13,6 +14,7 @@ public interface ChromosomeReportService {
 	 * Report informations about neXtProt entries and genes located on the given chromosome
 	 * @param chromosome the chromosome to get report
 	 * @return a pojo containing the report
+	 * @throws ChromosomeNotFoundException should be thrown if {@code chromosome} was not found in neXtProt
 	 */
 	ChromosomeReport reportChromosome(String chromosome);
 
@@ -28,5 +30,24 @@ public interface ChromosomeReportService {
 		list.addAll(Arrays.asList("X", "Y", "MT", "unknown"));
 
 		return list;
+	}
+
+	class ChromosomeNotFoundException extends NextProtException {
+
+		private static final long serialVersionUID = 1L;
+
+		private final String chromosome;
+
+		public ChromosomeNotFoundException(String chromosome, String expectedChromosomes) {
+
+			super("Chromosome '" + chromosome + "' was not found. Give a valid name among "+expectedChromosomes);
+
+			this.chromosome = chromosome;
+		}
+
+		public String getChromosome() {
+
+			return chromosome;
+		}
 	}
 }
