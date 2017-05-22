77a69f2fc523a0a0562f9e1b1f740362ee159492
fnikitin <frederic.nikitin@gmail.com>
2017-05-22 11:58:15 +0200

    Implementing integration test for chromosome report export

---
+++ core/src/test/java/org/nextprot/api/core/service/ChromosomeEntryReportIntegrationTest.java
@@ -0,0 +1,29 @@
+package org.nextprot.api.core.service;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.nextprot.api.core.domain.ChromosomeReport;
+import org.nextprot.api.core.service.export.io.ChromosomeReportTXTReader;
+
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class ChromosomeEntryReportIntegrationTest {
+
+	@Ignore
+	@Test
+	public void chr1EntryReportsShouldMatchFTPReports() throws Exception {
+
+		ChromosomeReportTXTReader reader = new ChromosomeReportTXTReader();
+
+		URL chrReportsFTPUrl = new URL("ftp://ftp.nextprot.org/pub/current_release/chr_reports/nextprot_chromosome_1.txt");
+		ChromosomeReport cr1FTP = reader.read(new InputStreamReader(chrReportsFTPUrl.openStream()));
+
+		URL chrReportsURL = new URL("http://build-api.nextprot.org/export/reports/chromosome/1.txt");
+		ChromosomeReport cr1 = reader.read(new InputStreamReader(chrReportsURL.openStream()));
+
+		System.out.println(cr1FTP);
+	}
+}
+
