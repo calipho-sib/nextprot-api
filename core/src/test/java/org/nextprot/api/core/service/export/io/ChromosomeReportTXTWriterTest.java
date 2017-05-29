bc7d9e606a3d8970490bf46a93f88b88a4abde5e
fnikitin <frederic.nikitin@gmail.com>
2017-05-29 16:04:36 +0200

    Fixing failing test

--- core/src/test/java/org/nextprot/api/core/service/export/io/ChromosomeReportTXTWriterTest.java
+++ core/src/test/java/org/nextprot/api/core/service/export/io/ChromosomeReportTXTWriterTest.java
@@ -52,10 +52,10 @@ public class ChromosomeReportTXTWriterTest {
 
         Assert.assertEquals(30, observedLines.length);
 
-        Assert.assertTrue(observedLines[17].matches("^Gene      neXtProt     Chromosomal  Start      Stop       Protein          Prote- Anti- 3D    Dise- Iso-  Vari-  PTMs Description$"));
-        Assert.assertTrue(observedLines[18].matches("^name      AC           position     position   position   existence        omics  body        ase   forms ants\\s+$"));
-        Assert.assertTrue(observedLines[20].matches("^SRY       NX_Q05066    Yp11.2          2786855    2787699 protein level    no     yes   yes   yes       1    47     1 Sex-determining region Y protein$"));
-        Assert.assertTrue(observedLines[21].matches("^RBMY1C    NX_P0DJD4    Yq11.23               -          - protein level    no     yes   no    no        1     0     0 RNA-binding motif protein, Y chromosome, family 1 member C$"));
-        Assert.assertTrue(observedLines[22].matches("^MFSD14A   NX_Q96MC6    1p21.2        100038097  100083377 protein level    no     no    no    no        1   141     3 Hippocampus abundant transcript 1 protein$"));
+        Assert.assertEquals("Gene          neXtProt     Chromosomal  Start      Stop       Protein          Prote- Anti- 3D    Dise- Iso-  Vari-  PTMs Description", observedLines[17]);
+        Assert.assertEquals("name          AC           position     position   position   existence        omics  body        ase   forms ants        ", observedLines[18]);
+        Assert.assertEquals("SRY           NX_Q05066    Yp11.2          2786855    2787699 protein level    no     yes   yes   yes       1    47     1 Sex-determining region Y protein", observedLines[20]);
+        Assert.assertEquals("RBMY1C        NX_P0DJD4    Yq11.23               -          - protein level    no     yes   no    no        1     0     0 RNA-binding motif protein, Y chromosome, family 1 member C", observedLines[21]);
+        Assert.assertEquals("MFSD14A       NX_Q96MC6    1p21.2        100038097  100083377 protein level    no     no    no    no        1   141     3 Hippocampus abundant transcript 1 protein", observedLines[22]);
     }
 }
