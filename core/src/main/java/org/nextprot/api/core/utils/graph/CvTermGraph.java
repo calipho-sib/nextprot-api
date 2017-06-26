493048989b2dd407219e1c1c4ef2c83416af8f15
fnikitin <frederic.nikitin@gmail.com>
2017-06-26 17:50:19 +0200

    New abstract class for cvterm graph

--- core/src/main/java/org/nextprot/api/core/utils/graph/CvTermGraph.java
+++ core/src/main/java/org/nextprot/api/core/utils/graph/CvTermGraph.java
@@ -1,132 +1,66 @@
 package org.nextprot.api.core.utils.graph;
 
-import gnu.trove.list.TIntList;
-import gnu.trove.list.array.TIntArrayList;
-import gnu.trove.map.TIntObjectMap;
-import gnu.trove.map.hash.TIntObjectHashMap;
-import gnu.trove.set.TIntSet;
-import gnu.trove.set.hash.TIntHashSet;
-import grph.path.Path;
+import com.google.common.base.Preconditions;
+import org.nextprot.api.commons.constants.TerminologyCv;
+import org.nextprot.api.core.domain.CvTerm;
+import org.nextprot.api.core.service.TerminologyService;
+import toools.collection.bigstuff.longset.LongSet;
 
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * A directed graph of int nodes and edges
- *
  * Created by fnikitin on 23.06.17.
  */
-public class CvTermGraph implements DirectedGraph {
+public abstract class CvTermGraph implements DirectedGraph {
 
-    private static int nextElementAvailable = 0;
+    private final DirectedGraph graph;
+    private final Map<String, Long> cvTermIdByAccession;
+    private final Map<Long, String> cvTermAccessionById;
+    private final Map<Long, LongSet> cvTermIdAncestors;
+    private final TerminologyCv terminologyCv;
 
-    private final TIntSet nodes = new TIntHashSet();
-    private final TIntSet edges = new TIntHashSet();
-    private final TIntList tails = new TIntArrayList();
-    private final TIntList heads = new TIntArrayList();
-    private final TIntObjectMap<TIntSet> adjacencyLists = new TIntObjectHashMap<>();
+    public CvTermGraph(TerminologyCv terminologyCv, TerminologyService service) {
 
-    @Override
-    public void addNode(int node) {
+        Preconditions.checkNotNull(terminologyCv);
+        Preconditions.checkNotNull(service);
 
-        assert !nodes.contains(node) : node;
+        List<CvTerm> cvTerms = service.findCvTermsByOntology(terminologyCv.name());
 
-        nodes.add(node);
+        this.terminologyCv = terminologyCv;
+
+        cvTermIdByAccession = new HashMap<>(cvTerms.size());
+        cvTermAccessionById = new HashMap<>(cvTerms.size());
+        cvTermIdAncestors = new HashMap<>(cvTerms.size());
+
+        graph = newGraph();
+
+        cvTerms.forEach(this::addCvTermNode);
+        cvTerms.forEach(this::addCvTermEdges);
+
+        precomputeAllAncestors();
     }
 
-    @Override
-    public int addEdge(int tail, int head) {
+    protected abstract void precomputeAllAncestors();
 
-        if (edges.contains(nextElementAvailable)) {
-            return nextElementAvailable;
-        }
+    protected abstract DirectedGraph newGraph();
 
-        if (!containsNode(tail)) {
-            addNode(tail);
-        }
+    protected abstract void addCvTermNode(CvTerm cvTerm);
 
-        if (!containsNode(head)) {
-            addNode(head);
-        }
+    protected abstract void addCvTermEdges(CvTerm cvTerm);
 
-        if (!adjacencyLists.containsKey(tail)) {
-            adjacencyLists.put(tail, new TIntHashSet());
-        }
-
-        if (!adjacencyLists.get(tail).contains(head)) {
-            adjacencyLists.get(tail).add(head);
-
-            tails.add(tail);
-            heads.add(head);
-
-            edges.add(nextElementAvailable++);
-
-            return nextElementAvailable-1;
-        }
-
-        throw new IllegalStateException("already existing edge: "+tail+ " -> "+head);
+    public TerminologyCv getTerminologyCv() {
+        return terminologyCv;
     }
 
-    @Override
-    public int[] getNodes() {
+    /*String getCvTermAccessionById(long id);
 
-        return nodes.toArray();
-    }
+    long getCvTermIdByAccession(String accession) throws OntologyDAG.NotFoundNodeException;
 
-    @Override
-    public int[] getEdges() {
+    boolean hasCvTermAccession(String cvTermAccession);
 
-        return edges.toArray();
-    }
+    Map<Long, LongSet> getCvTermIdAncestors();
 
-    @Override
-    public int getTailNode(int edge) {
-
-        if (edge >= 0 && edge < tails.size()) {
-
-            return tails.get(edge);
-        }
-        return -1;
-    }
-
-    @Override
-    public int getHeadNode(int edge) {
-
-        if (edge >= 0 && edge < heads.size()) {
-
-            return heads.get(edge);
-        }
-        return -1;
-    }
-
-    @Override
-    public boolean containsNode(int node) {
-
-        return nodes.contains(node);
-    }
-
-    @Override
-    public boolean containsEdge(int edge) {
-
-        return edges.contains(edge);
-    }
-
-    @Override
-    public int[] getAncestors(int cvTermId) {
-        return new int[0];
-    }
-
-    @Override
-    public boolean isAncestorOf(int queryAncestor, int queryDescendant) {
-        return false;
-    }
-
-    @Override
-    public Collection<Path> calcAllPaths() {
-        return null;
-    }
-
-    @Override
-    public DirectedGraph calcAncestorSubgraph(int node) {
-        return null;
-    }
+    Map<String, Long> getCvTermIdByAccession();*/
 }
