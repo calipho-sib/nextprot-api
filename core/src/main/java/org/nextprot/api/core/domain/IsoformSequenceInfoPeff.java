e59bc46607a16996f8f965f798c8cd3e1af2fdf7
fnikitin <frederic.nikitin@gmail.com>
2017-07-21 12:16:15 +0200

    Adding methods

--- core/src/main/java/org/nextprot/api/core/domain/IsoformSequenceInfoPeff.java
+++ core/src/main/java/org/nextprot/api/core/domain/IsoformSequenceInfoPeff.java
@@ -1,13 +1,34 @@
 package org.nextprot.api.core.domain;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class IsoformSequenceInfoPeff implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    public enum PEFF_KEY {
+
+        DbUniqueId(),
+        PName(),
+        GName(),
+        NcbiTaxId(),
+        TaxName(),
+        Length(),
+        SV(),
+        EV(),
+        PE(),
+        ModResPsi(),
+        ModRes(),
+        VariantSimple(),
+        VariantComplex(),
+        Processed();
+
+        public String getName() {
+            return "\\" + name();
+        }
+    }
 
     private String isoformAccession;
 
@@ -38,30 +59,6 @@ public class IsoformSequenceInfoPeff implements Serializable {
     private String modResFormat;
 
     private String processedMoleculeFormat;
-
-    public static String toPeffHeader(IsoformSequenceInfoPeff pojo) {
-
-        List<String> keyValuePairsList = Arrays.asList("nxp:" + pojo.getIsoformAccession(),
-                pojo.getIsoformAccessionFormat(),       // \DbUniqueId
-                pojo.getProteinNameFormat(),            // \PName
-                pojo.getGeneNameFormat(),               // \GName
-                pojo.getNcbiTaxonomyIdentifierFormat(), // \Ncbi
-                pojo.getTaxonomyNameFormat(),           // \TaxName
-                pojo.getSequenceLengthFormat(),         // \Length
-                pojo.getSequenceVersionFormat(),        // \SV
-                pojo.getEntryVersionFormat(),           // \EV
-                pojo.getProteinEvidenceFormat(),        // \PE
-                pojo.getModResPsiFormat(),              // \ModResPsi
-                pojo.getModResFormat(),                 // \ModRes
-                pojo.getVariantSimpleFormat(),          // \VariantSimple
-                pojo.getVariantComplexFormat(),         // \VariantComplex
-                pojo.getProcessedMoleculeFormat()       // \Processed
-        );
-
-        return keyValuePairsList.stream()
-                .filter(kv -> !kv.isEmpty())
-                .collect(Collectors.joining(" "));
-    }
 
     public String getIsoformAccession() {
         return isoformAccession;
@@ -181,5 +178,88 @@ public class IsoformSequenceInfoPeff implements Serializable {
 
     public void setProcessedMoleculeFormat(String processedMoleculeFormat) {
         this.processedMoleculeFormat = processedMoleculeFormat;
+    }
+
+    public String getValue(PEFF_KEY key) {
+
+        switch(key) {
+
+            case DbUniqueId:
+                return getIsoformAccessionFormat();
+            case PName:
+                return getProteinNameFormat();
+            case GName:
+                return getGeneNameFormat();
+            case NcbiTaxId:
+                return getNcbiTaxonomyIdentifierFormat();
+            case TaxName:
+                return getTaxonomyNameFormat();
+            case Length:
+                return getSequenceLengthFormat();
+            case SV:
+                return getSequenceVersionFormat();
+            case EV:
+                return getEntryVersionFormat();
+            case PE:
+                return getProteinEvidenceFormat();
+            case ModResPsi:
+                return getModResPsiFormat();
+            case ModRes:
+                return getModResFormat();
+            case VariantSimple:
+                return getVariantSimpleFormat();
+            case VariantComplex:
+                return getVariantComplexFormat();
+            case Processed:
+                return getProcessedMoleculeFormat();
+            default:
+                throw new IllegalArgumentException("unknown key "+key.getName());
+        }
+    }
+
+    private static List<String> toKeyValueStringList(IsoformSequenceInfoPeff pojo) {
+
+        return Arrays.asList(
+                pojo.getIsoformAccessionFormat(),       // \DbUniqueId
+                pojo.getProteinNameFormat(),            // \PName
+                pojo.getGeneNameFormat(),               // \GName
+                pojo.getNcbiTaxonomyIdentifierFormat(), // \Ncbi
+                pojo.getTaxonomyNameFormat(),           // \TaxName
+                pojo.getSequenceLengthFormat(),         // \Length
+                pojo.getSequenceVersionFormat(),        // \SV
+                pojo.getEntryVersionFormat(),           // \EV
+                pojo.getProteinEvidenceFormat(),        // \PE
+                pojo.getModResPsiFormat(),              // \ModResPsi
+                pojo.getModResFormat(),                 // \ModRes
+                pojo.getVariantSimpleFormat(),          // \VariantSimple
+                pojo.getVariantComplexFormat(),         // \VariantComplex
+                pojo.getProcessedMoleculeFormat()       // \Processed
+        );
+    }
+
+    public static String toPeffHeader(IsoformSequenceInfoPeff pojo) {
+
+        List<String> keyValuePairsList = new ArrayList<>();
+
+        keyValuePairsList.add("nxp:" + pojo.getIsoformAccession());
+        keyValuePairsList.addAll(toKeyValueStringList(pojo));
+
+        return keyValuePairsList.stream()
+                .filter(kv -> !kv.isEmpty())
+                .collect(Collectors.joining(" "));
+    }
+
+    public static Map<String, String> toMap(IsoformSequenceInfoPeff pojo) {
+
+        Map<String, String> map = new HashMap<>();
+
+        toKeyValueStringList(pojo).stream()
+                .filter(kvs -> !kvs.isEmpty())
+                .forEach(kvs -> {
+                    String[] kv = kvs.split("=");
+                    map.put(kv[0], kv[1]);
+                });
+
+        return map;
     }
 }
