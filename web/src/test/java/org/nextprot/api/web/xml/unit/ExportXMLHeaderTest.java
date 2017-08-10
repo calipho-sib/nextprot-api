6f15997b18797d14be34cea2268ba07c7c359c36
fnikitin <frederic.nikitin@isb-sib.ch>
2017-08-10 14:53:15 +0200

    Fixing test

--- web/src/test/java/org/nextprot/api/web/xml/unit/ExportXMLHeaderTest.java
+++ web/src/test/java/org/nextprot/api/web/xml/unit/ExportXMLHeaderTest.java
@@ -3,7 +3,6 @@ package org.nextprot.api.web.xml.unit;
 import org.junit.Test;
 import org.nextprot.api.core.domain.release.ReleaseInfo;
 import org.nextprot.api.web.dbunit.base.mvc.WebUnitBaseTest;
-import org.nextprot.api.web.service.ExportService;
 import org.nextprot.api.web.service.impl.writer.EntryVelocityBasedStreamWriter;
 import org.nextprot.api.web.service.impl.writer.EntryXMLStreamWriter;
 import org.nextprot.api.web.utils.XMLUnitUtils;
@@ -12,9 +11,7 @@ import org.w3c.dom.NodeList;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,24 +24,17 @@ public class ExportXMLHeaderTest extends WebUnitBaseTest {
         Writer writer = new PrintWriter(out);
         EntryVelocityBasedStreamWriter exporter = new EntryXMLStreamWriter(writer, "overview");
         
-        Map<String, Object> map = new HashMap<>();
-
-        map.put("queryString", "something");
-        map.put(ExportService.ENTRIES_COUNT_PARAM, 2);
-
         ReleaseInfo rc = new ReleaseInfo();
         rc.setApiRelease("api-test-version");
         rc.setDatabaseRelease("database-test-version");
 
-        map.put("release", rc);
-        
-        exporter.write(new ArrayList<String>(), map);
+        exporter.write(Collections.emptyList(), rc);
 
         NodeList dbReleaseNodes = XMLUnitUtils.getMatchingNodes(out.toString(), "//*[local-name()='database-release']");
         assertEquals(dbReleaseNodes.item(0).getTextContent(), "database-test-version");
         NodeList apiReleaseNodes = XMLUnitUtils.getMatchingNodes(out.toString(), "//*[local-name()='api-release']");
         assertEquals(apiReleaseNodes.item(0).getTextContent(), "api-test-version");
         NodeList entriesCountNode = XMLUnitUtils.getMatchingNodes(out.toString(), "//*[local-name()='number-of-entries']");
-        assertEquals(entriesCountNode.item(0).getTextContent(), "2");
+        assertEquals(entriesCountNode.item(0).getTextContent(), "0");
     }
 }
