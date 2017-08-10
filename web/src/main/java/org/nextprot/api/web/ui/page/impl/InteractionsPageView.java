4a3e15be2e0436a96743b7ef3ab3d52c51875513
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2017-08-10 16:26:08 +0200

    Synced InteractionsPageView with specs

--- web/src/main/java/org/nextprot/api/web/ui/page/impl/InteractionsPageView.java
+++ web/src/main/java/org/nextprot/api/web/ui/page/impl/InteractionsPageView.java
@@ -7,6 +7,11 @@ import javax.annotation.Nonnull;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Please keep this class in sync with specs in https://swissprot.isb-sib.ch/wiki/display/cal/neXtProt+Interactions+view+specs
+ * @author pmichel
+ *
+ */
 public class InteractionsPageView extends PageViewBase {
 
 	InteractionsPageView() {
@@ -17,7 +22,14 @@ public class InteractionsPageView extends PageViewBase {
 	@Override
 	protected List<AnnotationCategory> getAnnotationCategoryWhiteList() {
 		return Arrays.asList(
-				AnnotationCategory.INTERACTION_INFO, // = NP1 SUBUNIT,
+				
+				AnnotationCategory.INTERACTION_INFO, 
+				AnnotationCategory.ENZYME_REGULATION,
+				AnnotationCategory.GO_MOLECULAR_FUNCTION, // further refined
+				AnnotationCategory.BINARY_INTERACTION,
+				AnnotationCategory.COFACTOR,
+				AnnotationCategory.COFACTOR_INFO,
+				AnnotationCategory.SMALL_MOLECULE_INTERACTION,
 				AnnotationCategory.MISCELLANEOUS
 		);
 	}
@@ -25,14 +37,23 @@ public class InteractionsPageView extends PageViewBase {
 	@Nonnull
 	@Override
 	protected List<AnnotationCategory> getFeatureCategoryWhiteList() {
-		return Arrays.asList(AnnotationCategory.INTERACTING_REGION);
+		return Arrays.asList(
+				AnnotationCategory.MISCELLANEOUS_REGION,
+				AnnotationCategory.CALCIUM_BINDING_REGION,
+				AnnotationCategory.DNA_BINDING_REGION,
+				AnnotationCategory.NUCLEOTIDE_PHOSPHATE_BINDING_REGION,
+				AnnotationCategory.INTERACTING_REGION,
+				AnnotationCategory.BINDING_SITE,
+				AnnotationCategory.METAL_BINDING_SITE
+			);
 	}
 
 	@Nonnull
 	@Override
 	protected List<String> getXrefDbNameWhiteList() {
 		return Arrays.asList(
-				"BindingDB","DIP","IntAct","MINT","STRING",
-				"SignaLink", "BioGrid","SIGNOR");
+				"BindingDB","DIP","IntAct","MINT",
+				"STRING", "SignaLink", "BioGrid","SIGNOR"
+			);
 	}
 }
