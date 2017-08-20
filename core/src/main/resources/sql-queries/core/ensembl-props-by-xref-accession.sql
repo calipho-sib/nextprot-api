52f47a6136bf1b2f3e2d5483ea340f9abd13412d
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2017-08-20 13:04:09 +0200

    added constraint on queries using mapping annotations of type 3 (between master and gene): mapping should NOT be BRONZE (100)

--- core/src/main/resources/sql-queries/core/ensembl-props-by-xref-accession.sql
+++ core/src/main/resources/sql-queries/core/ensembl-props-by-xref-accession.sql
@@ -3,7 +3,7 @@ select tx.resource_id as db_xref_id, m.identifier_id as entry_id,
  tx.accession as transcript_ac , px.accession as protein_ac,
  gt.annotation_id as gt_link_id, tp.assoc_id as tp_link_id
 from nextprot.sequence_identifiers m
-  inner join nextprot.mapping_annotations mg on (m.identifier_id = mg.mapped_identifier_id and mg.cv_type_id = 3)
+  inner join nextprot.mapping_annotations mg on (m.identifier_id = mg.mapped_identifier_id and mg.cv_type_id = 3 and mg.cv_quality_qualifier_id !=100)
   inner join nextprot.sequence_identifiers g on (mg.reference_identifier_id = g.identifier_id and g.cv_type_id = 3)
   inner join nextprot.db_xrefs gx on (g.db_xref_id = gx.resource_id)
   inner join nextprot.mapping_annotations gt on (g.identifier_id = gt.reference_identifier_id and gt.cv_type_id = 2)
