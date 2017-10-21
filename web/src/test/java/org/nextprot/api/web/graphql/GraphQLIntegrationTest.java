fffb198a8180ff2314b2b9588ccaefa77e3ae2d0
Daniel Teixeira <ddtxra@gmail.com>
2017-10-21 22:29:23 +0200

    adding test

--- web/src/test/java/org/nextprot/api/web/graphql/GraphQLIntegrationTest.java
+++ web/src/test/java/org/nextprot/api/web/graphql/GraphQLIntegrationTest.java
@@ -12,9 +12,9 @@ public class GraphQLIntegrationTest extends WebIntegrationBaseTest {
   @Test
   public void shouldRunAGraphqlQuery() throws Exception {
 
-      String query = "{entry (accession:\"P06213\") { isoforms { sequence }";
+      String query = "{entry (accession:\"P06213\") { isoforms { sequence } } }";
 
-      ResultActions result = this.mockMvc.perform(post("/graphql").accept(MediaType.APPLICATION_JSON).content(query));
+      ResultActions result = this.mockMvc.perform(post("/graphql").param("query", query).accept(MediaType.APPLICATION_JSON));
       result.andExpect(status().isOk());
       System.err.println(result.andReturn().getResponse().getContentAsString());
 
