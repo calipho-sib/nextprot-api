1ac03ffdff5a070bd79545f89ce596f68cb9cf49
fnikitin <frederic.nikitin@isb-sib.ch>
2017-11-02 14:28:42 +0100

    Make PublicationStatistics an internal class of GlobalPublicationStatistics

--- core/src/main/java/org/nextprot/api/core/domain/publication/GlobalPublicationStatistics.java
+++ core/src/main/java/org/nextprot/api/core/domain/publication/GlobalPublicationStatistics.java
@@ -3,6 +3,8 @@ package org.nextprot.api.core.domain.publication;
 import org.jsondoc.core.annotation.ApiObject;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @ApiObject(name = "global publication statistics", description = "Global publication statistics")
 public class GlobalPublicationStatistics implements Serializable {
@@ -13,6 +15,15 @@ public class GlobalPublicationStatistics implements Serializable {
 	private int numberOfComputationallyMappedPublications;
 	private int numberOfLargeScalePublications;
 	private int numberOfCuratedPublications;
+    private Map<Long, PublicationStatistics> publicationStatisticsById = new HashMap<>();
+
+    public PublicationStatistics getPublicationStatistics(long pubId) {
+        return publicationStatisticsById.getOrDefault(pubId, new PublicationStatistics());
+    }
+
+    public void putPublicationStatisticsById(long pubId, PublicationStatistics stats) {
+        this.publicationStatisticsById.put(pubId, stats);
+    }
 
 	public int getNumberOfCitedPublications() {
 		return numberOfCitedPublications;
@@ -45,4 +56,55 @@ public class GlobalPublicationStatistics implements Serializable {
 	public void incrementNumberOfCuratedPublications() {
 		numberOfCuratedPublications++;
 	}
+
+    public static class PublicationStatistics implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        private long publicationId;
+        private boolean isCited;
+        private boolean isLargeScale;
+        private boolean isCurated;
+        private boolean isComputed;
+
+        public long getPublicationId() {
+            return publicationId;
+        }
+
+        public void setPublicationId(long publicationId) {
+            this.publicationId = publicationId;
+        }
+
+        public boolean isCited() {
+            return isCited;
+        }
+
+        public void setCited(boolean cited) {
+            isCited = cited;
+        }
+
+        public boolean isLargeScale() {
+            return isLargeScale;
+        }
+
+        public void setLargeScale(boolean largeScale) {
+            isLargeScale = largeScale;
+        }
+
+        public boolean isCurated() {
+            return isCurated;
+        }
+
+        public void setCurated(boolean curated) {
+            isCurated = curated;
+        }
+
+        public boolean isComputed() {
+            return isComputed;
+        }
+
+        public void setComputed(boolean computed) {
+            isComputed = computed;
+        }
+    }
 }
