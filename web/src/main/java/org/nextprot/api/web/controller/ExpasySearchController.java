fa418e524a2adb3e3301a7a35ce30680e69bd607
Daniel Teixeira <ddtxra@gmail.com>
2017-11-08 16:04:09 +0100

    adding expasy search

---
+++ web/src/main/java/org/nextprot/api/web/controller/ExpasySearchController.java
@@ -0,0 +1,70 @@
+package org.nextprot.api.web.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nextprot.api.commons.exception.NextProtException;
+import org.nextprot.api.solr.Query;
+import org.nextprot.api.solr.QueryRequest;
+import org.nextprot.api.solr.SearchResult;
+import org.nextprot.api.solr.SolrService;
+import org.nextprot.api.web.service.QueryBuilderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Controller used to retrieve search results for www.expasy.org
+ */
+@Controller
+public class ExpasySearchController {
+
+	private final Log LOGGER = LogFactory.getLog(ExpasySearchController.class);
+
+	@Autowired private SolrService queryService;
+	@Autowired private QueryBuilderService queryBuilderService;
+
+	
+	@RequestMapping(value = "/expasy-search", method = { RequestMethod.POST, RequestMethod.GET })
+	public String expasySearch(@RequestParam String query, @RequestParam (required = false) String type, Model model, HttpServletResponse response) {
+
+			try {
+
+				QueryRequest qr = new QueryRequest();
+				qr.setQuality("gold-and-silver");
+				qr.setQuery(query);
+				Query bq = queryBuilderService.buildQueryForSearch(qr, "entry");
+				SearchResult result = queryService.executeQuery(bq);
+				model.addAttribute("count", result.getFound());
+				model.addAttribute("url", "https://www.nextprot.org/proteins/search?quality=gold-and-silver&query=" + query);
+				model.addAttribute("description", "Entries matching the query " + query + " in neXtProt");
+
+			}catch (NextProtException e){
+
+				LOGGER.error(e.getLocalizedMessage());
+				e.printStackTrace();
+				response.setStatus(500);
+				model.addAttribute("count", -1);
+				model.addAttribute("url", "error message " + e.getMessage());
+
+			}
+			catch (Exception e){
+
+				LOGGER.error(e.getLocalizedMessage());
+				e.printStackTrace();
+				response.setStatus(500);
+				model.addAttribute("count", -1);
+
+			}
+
+
+			return "expasy-search";
+
+	}
+
+
+}
---
+++ web/src/test/java/org/nextprot/api/web/xml/integration/ExpasySearchIntegrationTest.java
@@ -0,0 +1,33 @@
+package org.nextprot.api.web.xml.integration;
+import org.junit.Test;
+import org.mockito.internal.matchers.Contains;
+import org.mockito.internal.matchers.GreaterThan;
+import org.mockito.internal.matchers.LessThan;
+import org.mockito.internal.matchers.StartsWith;
+import org.nextprot.api.web.dbunit.base.mvc.WebIntegrationBaseTest;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+
+/**
+ * The specifications of the expasy search interface can be found in here:
+ * http://wiki.isb-sib.ch/web-team/Sib-resource_query-interface
+ *
+ */
+public class ExpasySearchIntegrationTest extends WebIntegrationBaseTest {
+
+
+  @Test
+  public void shouldReployToExpasySearchWithXML() throws Exception {
+	  this.mockMvc.perform(get("/expasy-search.xml?query=insulin&type=whatever"))
+        .andExpect(status().isOk())
+	  	.andExpect(xpath("/ExpasyResult/count").nodeCount(1))
+        .andExpect(xpath("/ExpasyResult/url").string("https://www.nextprot.org/proteins/search?quality=gold-and-silver&query=insulin"));
+  }
+  
+
+}
+
