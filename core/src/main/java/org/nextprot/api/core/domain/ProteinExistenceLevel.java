50c81a042cd097f807b6c6ccdea83a633d728ba4
fnikitin <frederic.nikitin@isb-sib.ch>
2018-01-10 14:52:35 +0100

    Custom serialization + static lookup method from multiple keys

--- core/src/main/java/org/nextprot/api/core/domain/ProteinExistenceLevel.java
+++ core/src/main/java/org/nextprot/api/core/domain/ProteinExistenceLevel.java
@@ -1,5 +1,17 @@
 package org.nextprot.api.core.domain;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.nextprot.api.commons.utils.EnumConstantDictionary;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@JsonSerialize(using = ProteinExistenceLevel.ProteinExistenceSerializer.class)
 public enum ProteinExistenceLevel {
 
     PROTEIN_LEVEL() {
@@ -35,16 +47,51 @@ public enum ProteinExistenceLevel {
 
     public abstract String getDescription();
 
+    private static EnumConstantDictionary<ProteinExistenceLevel> dictionaryOfConstants = new EnumConstantDictionary<ProteinExistenceLevel>(ProteinExistenceLevel.class, values()) {
+
+        @Override
+        protected void updateDictionaryOfConstants(Map<String, ProteinExistenceLevel> dictionary) {
+
+            for (ProteinExistenceLevel pe : ProteinExistenceLevel.values()) {
+                computeAlternativeKeys(pe).forEach(k -> dictionary.put(k, pe));
+            }
+        }
+
+        private List<String> computeAlternativeKeys(ProteinExistenceLevel pe) {
+
+            List<String> keys = new ArrayList<>(3);
+
+            keys.add(pe.getDescription());
+            keys.add(pe.getDescription().replace(" ", "_"));
+
+            if (pe.ordinal() > 1) {
+                keys.add(pe.getName().toLowerCase());
+            }
+            else {
+                String[] words = pe.getDescription().split(" ");
+                keys.add(words[words.length-2]+ " " +words[words.length-1]);
+            }
+
+            return keys;
+        }
+    };
+
     private final String name;
+    private final String descriptionName;
     private final int level;
 
     ProteinExistenceLevel() {
         this.name = this.name().toLowerCase().replace("_", " ");
+        this.descriptionName = this.getDescription().replace(" ", "_");
         this.level = this.ordinal()+1;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getDescriptionName() {
+        return descriptionName;
     }
 
     public int getLevel() {
@@ -59,21 +106,26 @@ public enum ProteinExistenceLevel {
         return ProteinExistenceLevel.values()[level-1];
     }
 
-    public static ProteinExistenceLevel valueOfString(String value) {
+    public static ProteinExistenceLevel valueOfKey(String value) {
 
-        switch (value) {
-            case "protein level":
-                return PROTEIN_LEVEL;
-            case "transcript level":
-                return TRANSCRIPT_LEVEL;
-            case "homology":
-                return HOMOLOGY;
-            case "predicted":
-                return PREDICTED;
-            case "uncertain":
-                return UNCERTAIN;
-            default:
-                throw new IllegalArgumentException("Invalid value "+value);
+        return dictionaryOfConstants.valueOfKey(value);
+    }
+
+    public static class ProteinExistenceSerializer extends JsonSerializer<ProteinExistenceLevel> {
+
+        @Override
+        public void serialize(ProteinExistenceLevel pe, JsonGenerator generator, SerializerProvider serializerProvider) throws IOException {
+
+            generator.writeStartObject();
+            generator.writeFieldName("name");
+            generator.writeString(pe.getName());
+            generator.writeFieldName("description");
+            generator.writeString(pe.getDescription());
+            generator.writeFieldName("descriptionName");
+            generator.writeString(pe.getDescriptionName());
+            generator.writeFieldName("level");
+            generator.writeNumber(pe.getLevel());
+            generator.writeEndObject();
         }
     }
 }
