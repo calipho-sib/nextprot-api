3afcaeab51c9961b844cf340387588cb381f11a9
fnikitin <frederic.nikitin@isb-sib.ch>
2018-01-23 12:39:52 +0100

    New integration test for protein existence

--- core/src/test/java/org/nextprot/api/core/dao/EntryPropertiesIntegrationDaoTest.java
+++ core/src/test/java/org/nextprot/api/core/dao/EntryPropertiesIntegrationDaoTest.java
@@ -2,23 +2,20 @@ package org.nextprot.api.core.dao;
 
 import org.junit.Test;
 import org.nextprot.api.core.domain.EntryProperties;
-import org.nextprot.api.core.domain.ProteinExistence;
 import org.nextprot.api.core.test.base.CoreUnitBaseTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @ActiveProfiles("dev")
 public class EntryPropertiesIntegrationDaoTest extends CoreUnitBaseTest {
 
 	@Autowired private EntryPropertiesDao entryPropertieDao;
-	
+
 	@Test
 	public void testFindEntryProperties() {
 		EntryProperties props = this.entryPropertieDao.findEntryProperties("NX_P51659");
-		assertEquals("Evidence at protein level", props.getProteinExistence(ProteinExistence.Source.PROTEIN_EXISTENCE_UNIPROT).getDescription());
 		assertTrue(props.getPtmCount() > 20);
 		assertTrue(props.getVarCount() > 100);
 		assertTrue(props.getIsoformCount() > 2);
