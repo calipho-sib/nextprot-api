1100dc6648883bb9f2c541b84137e58e8bf2ef1f
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2018-01-30 10:35:15 +0100

    fixed stupid error in activated profiles of base test class

--- web/src/test/java/org/nextprot/api/web/dbunit/base/mvc/WebIntegrationBaseTest.java
+++ web/src/test/java/org/nextprot/api/web/dbunit/base/mvc/WebIntegrationBaseTest.java
@@ -2,6 +2,6 @@ package org.nextprot.api.web.dbunit.base.mvc;
 
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("dev,cache")
+@ActiveProfiles({"dev","cache"})
 public abstract class WebIntegrationBaseTest extends WebUnitBaseTest {
 }
