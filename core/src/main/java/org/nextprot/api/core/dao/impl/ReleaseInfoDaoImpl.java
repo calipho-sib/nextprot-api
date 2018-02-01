215dad4e4973cc14c33a92ed05c47d7eba845a77
fnikitin <frederic.nikitin@isb-sib.ch>
2018-02-01 11:35:24 +0100

    ReleaseInfoDaoImpl now get protein existences count from MasterIdentifierService

--- core/src/main/java/org/nextprot/api/core/dao/impl/ReleaseInfoDaoImpl.java
+++ core/src/main/java/org/nextprot/api/core/dao/impl/ReleaseInfoDaoImpl.java
@@ -2,11 +2,12 @@ package org.nextprot.api.core.dao.impl;
 
 import org.nextprot.api.commons.spring.jdbc.DataSourceServiceLocator;
 import org.nextprot.api.commons.utils.SQLDictionary;
-import org.nextprot.api.commons.utils.StringUtils;
 import org.nextprot.api.core.dao.ReleaseInfoDao;
+import org.nextprot.api.core.domain.ProteinExistence;
 import org.nextprot.api.core.domain.release.ReleaseContentsDataSource;
 import org.nextprot.api.core.domain.release.ReleaseDataSources;
 import org.nextprot.api.core.domain.release.ReleaseStatsTag;
+import org.nextprot.api.core.service.MasterIdentifierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -15,23 +16,19 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class ReleaseInfoDaoImpl implements ReleaseInfoDao {
-
-	private static final Comparator<ReleaseContentsDataSource> DATA_SOURCE_COMPARATOR = new Comparator<ReleaseContentsDataSource>() {
-		@Override
-		public int compare(ReleaseContentsDataSource ds1, ReleaseContentsDataSource ds2) {
-
-			return ds1.getSource().compareToIgnoreCase(ds2.getSource());
-		}
-	};
 
 	@Autowired
 	private DataSourceServiceLocator dsLocator;
 	@Autowired
 	private SQLDictionary sqlDictionary;
+	@Autowired
+	private MasterIdentifierService masterIdentifierService;
 
 	@Override
 	public List<ReleaseContentsDataSource> findReleaseInfoDataSources() {
@@ -42,7 +39,7 @@ public class ReleaseInfoDaoImpl implements ReleaseInfoDao {
 		List<ReleaseContentsDataSource> ds = new NamedParameterJdbcTemplate(dsLocator.getDataSource()).query(sqlDictionary.getSQLQuery("release-contents"), params, new ReleaseInfoRowMapper(null));
 		ds.addAll(new NamedParameterJdbcTemplate(dsLocator.getDataSource()).query(sqlDictionary.getSQLQuery("release-contents-peptide-atlas"), params, new ReleaseInfoRowMapper(ReleaseDataSources.PeptideAtlas)));
 
-		Collections.sort(ds, DATA_SOURCE_COMPARATOR);
+		ds.sort((ds1, ds2) -> ds1.getSource().compareToIgnoreCase(ds2.getSource()));
 
 		return ds;
 	}
@@ -54,11 +51,20 @@ public class ReleaseInfoDaoImpl implements ReleaseInfoDao {
 
 	@Override
 	public List<ReleaseStatsTag> findTagStatistics() {
-		return new JdbcTemplate(dsLocator.getDataSource()).query(sqlDictionary.getSQLQuery("release-stats"), new ReleaseStatsTagRowMapper());
+
+		Map<String, Integer> proteinExistencesCount = new HashMap<>();
+
+		proteinExistencesCount.put("PROTEIN_LEVEL_MASTER", masterIdentifierService.findEntryAccessionsByProteinExistence(ProteinExistence.PROTEIN_LEVEL).size());
+		proteinExistencesCount.put("TRANSCRIPT_LEVEL_MASTER", masterIdentifierService.findEntryAccessionsByProteinExistence(ProteinExistence.TRANSCRIPT_LEVEL).size());
+		proteinExistencesCount.put("HOMOLOGY_MASTER", masterIdentifierService.findEntryAccessionsByProteinExistence(ProteinExistence.HOMOLOGY).size());
+		proteinExistencesCount.put("PREDICTED_MASTER", masterIdentifierService.findEntryAccessionsByProteinExistence(ProteinExistence.PREDICTED).size());
+		proteinExistencesCount.put("UNCERTAIN_MASTER", masterIdentifierService.findEntryAccessionsByProteinExistence(ProteinExistence.UNCERTAIN).size());
+
+		return new JdbcTemplate(dsLocator.getDataSource()).query(sqlDictionary.getSQLQuery("release-stats"), new ReleaseStatsTagRowMapper(proteinExistencesCount));
 	}
 
 	private static class ReleaseInfoRowMapper implements ParameterizedRowMapper<ReleaseContentsDataSource> {
-		
+
 		private ReleaseDataSources datasource;
 		
 		public ReleaseInfoRowMapper(ReleaseDataSources datasource){
@@ -67,6 +73,7 @@ public class ReleaseInfoDaoImpl implements ReleaseInfoDao {
 
 		@Override
 		public ReleaseContentsDataSource mapRow(ResultSet resultSet, int row) throws SQLException {
+
 			ReleaseContentsDataSource releaseInfoDs = new ReleaseContentsDataSource();
 			ReleaseDataSources ds = null;
 			if(datasource == null){  ds = ReleaseDataSources.cvValueOf(resultSet.getString("cv_name"));
@@ -78,21 +85,34 @@ public class ReleaseInfoDaoImpl implements ReleaseInfoDao {
 			releaseInfoDs.setUrl(ds.getUrl());
 			return releaseInfoDs;
 		}
-
 	}
 
 	private static class ReleaseStatsTagRowMapper implements ParameterizedRowMapper<ReleaseStatsTag> {
-		
+
+		private final Map<String, Integer> proteinExistencesCount;
+
+		public ReleaseStatsTagRowMapper(Map<String, Integer> proteinExistencesCount){
+			this.proteinExistencesCount = proteinExistencesCount;
+		}
+
 		@Override
 		public ReleaseStatsTag mapRow(ResultSet resultSet, int row) throws SQLException {
+
 			ReleaseStatsTag tagStat = new ReleaseStatsTag();
-			tagStat.setCount(resultSet.getInt("count"));
+
 			tagStat.setDescription(resultSet.getString("description"));
 			tagStat.setTag(resultSet.getString("tag"));
 			tagStat.setCategroy(resultSet.getString("category"));
-			tagStat.setSortOrder(resultSet.getInt("sort_order"));			
+			tagStat.setSortOrder(resultSet.getInt("sort_order"));
+
+			if ("Protein existence".equals(tagStat.getCategroy())) {
+				tagStat.setCount(proteinExistencesCount.get(tagStat.getTag()));
+			}
+			else {
+				tagStat.setCount(resultSet.getInt("count"));
+			}
+
 			return tagStat;
 		}
-
 	}
 }
