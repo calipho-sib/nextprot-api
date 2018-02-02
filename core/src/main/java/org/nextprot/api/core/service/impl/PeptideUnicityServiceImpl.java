67648f95aafebe58daabc8094cacfc65c7f29d63
fnikitin <frederic.nikitin@isb-sib.ch>
2018-02-02 12:20:31 +0100

    Make getPeptideNameUnicityMap() synchonized again

--- core/src/main/java/org/nextprot/api/core/service/impl/PeptideUnicityServiceImpl.java
+++ core/src/main/java/org/nextprot/api/core/service/impl/PeptideUnicityServiceImpl.java
@@ -63,7 +63,7 @@ class PeptideUnicityServiceImpl implements PeptideUnicityService {
 
 	@Override
 	@Cacheable("peptide-name-unicity-map")
-	public Map<String,PeptideUnicity> getPeptideNameUnicityMap() {
+	public synchronized Map<String,PeptideUnicity> getPeptideNameUnicityMap() {
 
 		Map<String,PeptideUnicity> result = new HashMap<>();
 		System.out.println("" + new Date() + "PeptideUnicityService building cache...");
