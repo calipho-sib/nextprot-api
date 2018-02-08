39128670f0a7d105452c6bf0da5428f7251dc538
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2018-02-08 11:39:24 +0100

    added cache profile to this test

--- web/src/test/java/org/nextprot/api/web/service/impl/SolrServiceTest.java
+++ web/src/test/java/org/nextprot/api/web/service/impl/SolrServiceTest.java
@@ -19,7 +19,7 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@ActiveProfiles("dev")
+@ActiveProfiles({"dev","cache"})
 public class SolrServiceTest extends WebUnitBaseTest {
 
     @Autowired private SolrService service;
