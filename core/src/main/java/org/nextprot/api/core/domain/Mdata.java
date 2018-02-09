e8ad8c4561cd73ce8cc57cf8f2a5b0961fbeb0dd
Daniel Teixeira <ddtxra@gmail.com>
2018-02-09 16:03:51 +0100

    adding mdata to object

--- core/src/main/java/org/nextprot/api/core/domain/Mdata.java
+++ core/src/main/java/org/nextprot/api/core/domain/Mdata.java
@@ -1,6 +1,17 @@
 package org.nextprot.api.core.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.json.JSONObject;
+import org.json.XML;
+import org.nextprot.api.commons.exception.NextProtException;
+
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 
 public class Mdata implements Serializable{
 
@@ -10,8 +21,9 @@ public class Mdata implements Serializable{
 	private long id; 			// equivalent to publication id
 	private String accession;   // MDATA name      
 	private String title; 
-	private String rawXml; 
-	
+	private String rawXml;
+	private MDataContext mdataContext;
+
 	public long getId() {
 		return id;
 	}
@@ -30,12 +42,35 @@ public class Mdata implements Serializable{
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
+	@JsonIgnore
 	public String getRawXml() {
 		return rawXml;
 	}
+
 	public void setRawXml(String rawXml) {
 		this.rawXml = rawXml;
+		if(!this.rawXml.isEmpty()){
+			this.mdataContext = convertXmlToMDataContext(this.rawXml);
+		}
 	}
+
+	static MDataContext convertXmlToMDataContext(String xml){
+		try {
+			JSONObject jObject = XML.toJSONObject(xml);
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+			mapper.enable(SerializationFeature.INDENT_OUTPUT);
+			return mapper.readValue(jObject.toString(), MDataContext.class);
+		} catch (IOException e) {
+			throw new NextProtException("Failed to convert XML mdata to object ", e);
+		}
+	}
+
+	public MDataContext getMdataContext() {
+		return mdataContext;
+	}
+
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("id       : " + this.id + "\n");
@@ -43,5 +78,140 @@ public class Mdata implements Serializable{
 		sb.append("title    : " + this.title + "'\n");
 		return sb.toString();
 	}
-	
+
+
+	// UTILITY CLASSES TO REPRESENT MDATA ////////////////////////////////////
+	static class DBXref {
+		@JsonProperty
+		private String db, dbkey;
+
+		public String getDb() {
+			return db;
+		}
+
+		public String getDbkey() {
+			return dbkey;
+		}
+
+	}
+
+	static class MDataPublication {
+		@JsonProperty
+		private String type, key;
+
+		@JsonProperty
+		private DBXref db_xref;
+
+		public String getType() {
+			return type;
+		}
+
+		public String getKey() {
+			return key;
+		}
+
+		public DBXref getDb_xref() {
+			return db_xref;
+		}
+
+	}
+
+	static class MDataPublications {
+		@JsonProperty
+		private List<MDataPublication> publication;
+
+		public List<MDataPublication> getValues() {
+			return publication;
+		}
+	}
+
+	static class MDataMetaDataProperties {
+		@JsonProperty
+		private String isVariable, label, content;
+
+		public String getIsVariable() {
+			return isVariable;
+		}
+
+		public String getLabel() {
+			return label;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+	}
+
+
+	static class MDataMetaData {
+		@JsonProperty
+		private MDataMetaDataProperties DM;
+		@JsonProperty
+		private MDataMetaDataProperties DC;
+		@JsonProperty
+		private MDataMetaDataProperties IP;
+		@JsonProperty
+		private MDataMetaDataProperties CP;
+		@JsonProperty
+		private MDataMetaDataProperties DP;
+		@JsonProperty
+		private MDataMetaDataProperties SP;
+		@JsonProperty
+		private MDataMetaDataProperties DA;
+		@JsonProperty
+		private MDataMetaDataProperties CL;
+
+		public MDataMetaDataProperties getCellLine() {
+			return CL;
+		}
+
+		public MDataMetaDataProperties getDetectionMethod() {
+			return DM;
+		}
+
+		public MDataMetaDataProperties getDataConfidenceDocumentation() {
+			return DC;
+		}
+
+		public MDataMetaDataProperties getInstrumentPlatform() {
+			return IP;
+		}
+
+		public MDataMetaDataProperties getCP() {
+			return CP;
+		}
+
+		public MDataMetaDataProperties getDataProcessingByNeXtProt() {
+			return DP;
+		}
+
+		public MDataMetaDataProperties getSamplePreparation() {
+			return SP;
+		}
+
+		public MDataMetaDataProperties getDataAnalysisProcedure() {
+			return DA;
+		}
+
+	}
+
+	static class MDataContext {
+
+		@JsonProperty
+		private MDataMetaData metadata;
+		@JsonProperty
+		private MDataPublications publications;
+
+		public MDataMetaData getMetadata() {
+			return metadata;
+		}
+
+		public MDataPublications getPublications() {
+			return publications;
+		}
+
+	}
+
+
 }
---
+++ core/src/test/java/org/nextprot/api/core/domain/MDataUnitTest.java
@@ -0,0 +1,37 @@
+package org.nextprot.api.core.domain;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MDataUnitTest {
+
+    @Test
+    public void testWithMultiplePublications() {
+        String xml = "<metadata><DM label=\"Detection method\" isVariable=\"N\" >Mass spectrometry LC-MS/MS.</DM>\n<DC label=\"Data confidence documentation\" isVariable=\"N\" >GOLD: All peptides.</DC>\n</metadata><publications>\n<publication key=\"1\" type=\"ARTICLE\"><db_xref db=\"PubMed\" dbkey=\"22068332\"></db_xref></publication>\n<publication key=\"2\" type=\"ARTICLE\"><db_xref db=\"PubMed\" dbkey=\"22278370\"></db_xref></publication>\n<publication key=\"3\" type=\"ARTICLE\"><db_xref db=\"PubMed\" dbkey=\"23242552\"></db_xref></publication>\n<publication key=\"4\" type=\"ARTICLE\"><db_xref db=\"PubMed\" dbkey=\"23933261\"></db_xref></publication>\n<publication key=\"5\" type=\"ARTICLE\"><db_xref db=\"PubMed\" dbkey=\"24870543\"></db_xref></publication>\n<publication key=\"6\" type=\"ARTICLE\"><db_xref db=\"PubMed\" dbkey=\"25977788\"></db_xref></publication>\n<publication key=\"7\" type=\"ARTICLE\"><db_xref db=\"PubMed\" dbkey=\"28112733\"></db_xref></publication>\n</publications>";
+        Mdata.MDataContext mc = Mdata.convertXmlToMDataContext(xml);
+        assertEquals(mc.getPublications().getValues().size(), 7);
+    }
+
+    @Test
+    public void testMdata() {
+        String xml = "<metadata><DM label=\"Detection method\" isVariable=\"N\" >Mass spectrometry Nano LC-MS/MS.</DM>\n" +
+                "<CL label=\"Cell line\" isVariable=\"N\" >Jurkat E6.1 [CVCL_0367].</CL>\n" +
+                "<SP label=\"Sample preparation\" isVariable=\"N\" >Lysate denaturation and treatment with either proteasome inhibitor MG132 or deubiquitinase inhibitor PR-619. Lysate in-solution digestion with trypsin. Peptides desaltification and off-line basic RP fractionation. Enrichment of Lys ubiquitinated sites by immunoprecipitation with anti-Lys-epsilon-GlyGly antibody followed by desaltification.</SP>\n" +
+                "<IP label=\"Instrument/platform\" isVariable=\"N\" >Nanoscale C18 HPLC coupled to Q Exactive mass spectrometer equipped with a nanoelectrospray source (Thermo Scientific).</IP>\n" +
+                "<DA label=\"Data analysis procedure\" isVariable=\"N\" >Protein database: UniProtKB/SwissProt (release number not available). Software: MaxQuant version 1.3.0.5. Maximum missed cleavages: 2. Mass tolerance for parent ion: 6 ppm. Mass tolerance for fragment ion: Not available. Fixed modification: Cys carbamidomethylation. Variable modifications: Met oxidation; Gly-Gly addition to Lys; N-terminal protein acetylation. Peptide FDR &lt; 0.01. Protein FDR &lt; 0.01. Minimum peptide length: 6. Ion mode: Positive. Spectra acquisition: 12 most intense precursor ions. Lys-epsilon-GlyGly sites localized to a peptide C-terminal Lys residue were removed. Spectra recalibration: Not available</DA>\n" +
+                "<DP label=\"Data processing by neXtProt\" >Exclusion of peptides of less than 7 amino acids. Alignment of peptides on the human proteome for exact matches. Peptides matching more than one entry are labeled 'found in other entries'. Exclusion of modifications found on non-proteotypic peptides. GLOBAL. Ubiquitination: a small fraction of ubiquitinated sites is likely to originate from modification by ISG15 or NEDD8 and is filtered according to neXtProt sequence annotation.</DP>\n" +
+                "<DC label=\"Data confidence documentation\" isVariable=\"N\" >Peptide identification. GOLD: Andromeda score &gt;= 100. LOCAL. Ubiquitination. GOLD: Andromeda score &gt;= 100; PTM localization probability &gt;= 0.99. SILVER: Andromeda score &gt;= 100; PTM localization probability &gt;= 0.95. N-terminal protein acetylation: GOLD.</DC>\n" +
+                "</metadata><publications>\n" +
+                "<publication key=\"1\" type=\"ARTICLE\"><db_xref db=\"PubMed\" dbkey=\"23266961\"></db_xref></publication>\n" +
+                "</publications>";
+
+        Mdata.MDataContext mc = Mdata.convertXmlToMDataContext(xml);
+        assertEquals(mc.getMetadata().getCellLine().getContent(), "Jurkat E6.1 [CVCL_0367].");
+        assertEquals(mc.getMetadata().getCellLine().getIsVariable(), "N");
+        assertEquals(mc.getMetadata().getDataAnalysisProcedure().getLabel(), "Data analysis procedure");
+        assertEquals(mc.getPublications().getValues().get(0).getDb_xref().getDbkey(), "23266961");
+
+    }
+
+}
