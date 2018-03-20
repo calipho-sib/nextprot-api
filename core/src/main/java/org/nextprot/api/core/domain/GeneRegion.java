0e6e5ae774eea9e97bac6ec447ea37af26f2dae9
fnikitin <frederic.nikitin@isb-sib.ch>
2018-03-20 18:07:32 +0100

    New gene region pojo

---
+++ core/src/main/java/org/nextprot/api/core/domain/GeneRegion.java
@@ -0,0 +1,45 @@
+package org.nextprot.api.core.domain;
+
+import java.io.Serializable;
+
+public class GeneRegion implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String geneName;
+	private int firstPosition;
+	private int lastPosition;
+
+	public GeneRegion() {}
+
+	public GeneRegion(String geneName, int firstPosition, int lastPosition) {
+
+		this.geneName = geneName;
+		this.firstPosition = firstPosition;
+		this.lastPosition = lastPosition;
+	}
+
+	public int getLastPosition() {
+		return lastPosition;
+	}
+
+	public void setLastPosition(int lastPosition) {
+		this.lastPosition = lastPosition;
+	}
+
+	public int getFirstPosition() {
+		return firstPosition;
+	}
+
+	public void setFirstPosition(int firstPosition) {
+		this.firstPosition = firstPosition;
+	}
+
+	public String getGeneName() {
+		return geneName;
+	}
+
+	public void setGeneName(String geneName) {
+		this.geneName = geneName;
+	}
+}
