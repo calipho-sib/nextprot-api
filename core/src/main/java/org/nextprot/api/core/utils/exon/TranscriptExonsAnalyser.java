5ff680da6c43f89e6a999f22ee4c6c1184cac2fb
fnikitin <frederic.nikitin@isb-sib.ch>
2018-03-27 10:56:58 +0200

    Changing field name to exonsAnalysis

--- core/src/main/java/org/nextprot/api/core/utils/exon/TranscriptExonsAnalyser.java
+++ core/src/main/java/org/nextprot/api/core/utils/exon/TranscriptExonsAnalyser.java
@@ -1,15 +1,9 @@
 package org.nextprot.api.core.utils.exon;
 
-import com.google.common.base.Preconditions;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.nextprot.api.core.domain.AminoAcid;
 import org.nextprot.api.core.domain.Exon;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Compute phases and categorize exons of isoform transcripts
@@ -18,10 +12,7 @@ import java.util.List;
  */
 public class TranscriptExonsAnalyser {
 
-    private final static Log LOGGER = LogFactory.getLog(TranscriptExonsAnalyser.class);
-    private static final ExonsAnalysis DEFAULT_HANDLER = new ExonsAnalysisImpl();
-
-    private final ExonsAnalysis handler;
+    private final ExonsAnalysis exonsAnalysis;
 
     private ExonCategorizer categorizer;
 
@@ -31,16 +22,11 @@ public class TranscriptExonsAnalyser {
     private int currentIsoformPos;
     private int currentPhase;
 
-    public TranscriptExonsAnalyser() {
+    public TranscriptExonsAnalyser(ExonsAnalysis exonsAnalysis) {
 
-        this(DEFAULT_HANDLER);
-    }
+        Objects.requireNonNull(exonsAnalysis);
 
-    public TranscriptExonsAnalyser(ExonsAnalysis handler) {
-
-        Preconditions.checkNotNull(handler);
-
-        this.handler = handler;
+        this.exonsAnalysis = exonsAnalysis;
     }
 
     private void init(int startPositionIsoformOnGene, int endPositionIsoformOnGene) {
@@ -69,10 +55,10 @@ public class TranscriptExonsAnalyser {
 
         init(startPositionIsoformOnGene, endPositionIsoformOnGene);
 
-        handler.started();
+        exonsAnalysis.started();
         for (Exon exon : exonsSorted) {
 
-            handler.startedExon(exon);
+            exonsAnalysis.startedExon(exon);
             ExonCategory exonCategory = categorizer.categorize(exon);
 
             if (exonCategory.isCoding()) {
@@ -84,11 +70,11 @@ public class TranscriptExonsAnalyser {
                 }
             }
             else {
-                handler.analysedNonCodingExon(exon, exonCategory);
+                exonsAnalysis.analysedNonCodingExon(exon, exonCategory);
             }
-            handler.terminated(exon);
+            exonsAnalysis.terminated(exon);
         }
-        handler.terminated();
+        exonsAnalysis.terminated();
 
         return true;
     }
@@ -126,17 +112,17 @@ public class TranscriptExonsAnalyser {
         AminoAcid last = newAminoAcid(isoformSequence, currentIsoformPos, currentPhase);
 
         if (first.getPosition() > isoformSequence.length()) {
-            handler.analysedCodingExonFailed(exon, new ExonOutOfBoundError(first, last,
+            exonsAnalysis.analysedCodingExonFailed(exon, new ExonOutOfBoundError(first, last,
                     ExonOutOfBoundError.AminoAcidOutOfBound.FIRST, isoformSequence.length()));
             return false;
         }
         else if (last.getPosition() > isoformSequence.length()) {
-            handler.analysedCodingExonFailed(exon, new ExonOutOfBoundError(first, last,
+            exonsAnalysis.analysedCodingExonFailed(exon, new ExonOutOfBoundError(first, last,
                     ExonOutOfBoundError.AminoAcidOutOfBound.LAST, isoformSequence.length()));
             return false;
         }
 
-        handler.analysedCodingExon(exon, first, last, cat);
+        exonsAnalysis.analysedCodingExon(exon, first, last, cat);
 
         return true;
     }
@@ -146,37 +132,5 @@ public class TranscriptExonsAnalyser {
         if (aaPosition >= isoformSequence.length()) return new AminoAcid(aaPosition + 1, phase, '?');
 
         return new AminoAcid(aaPosition + 1, phase, isoformSequence.charAt(aaPosition));
-    }
-
-    /**
-     * Update exon on the fly in this default implementation
-     */
-    private static class ExonsAnalysisImpl implements ExonsAnalysis {
-
-        @Override
-        public void started() {}
-
-        @Override
-        public void startedExon(Exon exon) {}
-
-        @Override
-        public void terminated(Exon exon) {}
-
-        @Override
-        public void terminated() {}
-
-        @Override
-        public void analysedCodingExonFailed(Exon exon, ExonOutOfBoundError exonOutOfBoundError) {
-
-            StringBuilder sb = new StringBuilder("SequenceIndexOutOfBoundsException: index (");
-
-            sb.append(exonOutOfBoundError.getOutOfBoundAminoAcid().getPosition()-1);
-
-            sb.append(") must be less than size (").append(exonOutOfBoundError.getIsoformLength()).append(")");
-
-            //TODO there is a bug in mapping transcript <-> isoform (should be fixed in the database)
-            //Should be solved with: https://issues.isb-sib.ch/browse/NEXTPROT-1005
-            LOGGER.warn(sb.toString());
-        }
     }
 }
