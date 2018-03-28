140038b528b7dfdeed70123ceafb8595bc939a94
fnikitin <frederic.nikitin@isb-sib.ch>
2018-03-28 13:52:18 +0200

    Add missing cache profile

--- core/src/test/java/org/nextprot/api/core/service/impl/peff/PEFFVariantIntegrationTest.java
+++ core/src/test/java/org/nextprot/api/core/service/impl/peff/PEFFVariantIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
 
-@ActiveProfiles({ "dev" })
+@ActiveProfiles({ "dev","cache" })
 public class PEFFVariantIntegrationTest extends CoreUnitBaseTest {
 
     @Autowired
