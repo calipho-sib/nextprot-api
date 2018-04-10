ec369fe33073d88741154bbbee7b7a8198d6bf00
fnikitin <frederic.nikitin@isb-sib.ch>
2018-04-10 11:10:03 +0200

    Adding javadoc

--- core/src/main/java/org/nextprot/api/core/service/GenomicMappingService.java
+++ core/src/main/java/org/nextprot/api/core/service/GenomicMappingService.java
@@ -9,8 +9,14 @@ import java.util.Map;
  * Extracts gene / chromosomal information about an entry
  * 
  * @author dteixeira
+ * @author fnikitin
  */
 public interface GenomicMappingService {
 
+	/**
+	 * Find genomic mappings for an entry accession
+	 * @param entryName the neXtProt entry accession
+	 * @return the genomic mappings by gene accession
+	 */
 	Map<String, GenomicMapping> findGenomicMappingsByEntryName(@ValidEntry String entryName);
 }
