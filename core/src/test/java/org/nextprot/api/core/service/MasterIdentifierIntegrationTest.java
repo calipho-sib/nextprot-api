86c2c473ffc3bd1f0ebd0dd9fe88f99d9491a94c
fnikitin <frederic.nikitin@isb-sib.ch>
2018-06-19 18:16:49 +0200

    Stressing MainNamesService

--- core/src/test/java/org/nextprot/api/core/service/MasterIdentifierIntegrationTest.java
+++ core/src/test/java/org/nextprot/api/core/service/MasterIdentifierIntegrationTest.java
@@ -3,6 +3,7 @@ package org.nextprot.api.core.service;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.nextprot.api.commons.utils.MultithreadedStressTester;
 import org.nextprot.api.core.test.base.CoreUnitBaseTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
@@ -15,6 +16,9 @@ public class MasterIdentifierIntegrationTest extends CoreUnitBaseTest {
 
 	@Autowired
 	private MasterIdentifierService service;
+
+	@Autowired
+    private MainNamesService mainNamesService;
 
 	@Ignore
 	@Test
@@ -44,4 +48,22 @@ public class MasterIdentifierIntegrationTest extends CoreUnitBaseTest {
 		Assert.assertEquals(accession.size(), 1);
 	}
 
+	@Test
+    public void testMainNamesMapIsValid() throws InterruptedException {
+
+        MultithreadedStressTester stressTester = new MultithreadedStressTester(10, 1000000);
+
+        stressTester.stress(() -> assertMainNamesMapValid());
+        stressTester.shutdown();
+
+        assertMainNamesMapValid();
+    }
+
+    private void assertMainNamesMapValid() {
+
+        Assert.assertEquals(62471, mainNamesService.findIsoformOrEntryMainName().size());
+        Assert.assertTrue(mainNamesService.findIsoformOrEntryMainName("NX_P52701").isPresent());
+        Assert.assertTrue(mainNamesService.findIsoformOrEntryMainName("NX_P01308").isPresent());
+        Assert.assertEquals("Insulin", mainNamesService.findIsoformOrEntryMainName("NX_P01308").get().getName());
+    }
 }
