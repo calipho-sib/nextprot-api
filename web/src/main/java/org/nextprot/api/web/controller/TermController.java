7afa96af9b3f5c11af2169e8f2b374cc20218ff5
fnikitin <frederic.nikitin@isb-sib.ch>
2018-06-22 14:26:13 +0200

    Add a new service /terminology-graph/{ontology}/is-hierarchical

--- web/src/main/java/org/nextprot/api/web/controller/TermController.java
+++ web/src/main/java/org/nextprot/api/web/controller/TermController.java
@@ -88,6 +88,17 @@ public class TermController {
 		return Collections.singletonMap("terminology-graph", cvTermGraphService.findCvTermGraph(TerminologyCv.getTerminologyOf(terminology)).toView());
 	}
 
+    @ApiMethod(path = "/terminology-graph/{terminology}/is-hierarchical", verb = ApiVerb.GET, description = "Gets the graph of terminology", produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/terminology-graph/{terminology}/is-hierarchical", method = { RequestMethod.GET }, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Boolean> isTerminologyGraphHierarchical(
+            @ApiPathParam(name = "terminology", description = "The name of the terminology. To get a list of possible terminologies, look at terminology-names method",  allowedvalues = { "nextprot-anatomy-cv"})
+            @PathVariable("terminology") String terminology) {
+
+        int height = (int) cvTermGraphService.findCvTermGraph(TerminologyCv.getTerminologyOf(terminology)).toView().getProperties().get(CvTermGraph.View.HEIGHT);
+
+        return Collections.singletonMap("is-hierarchical", height > 1);
+    }
+
 	@ApiMethod(path = "/term/{term}/ancestor-graph", verb = ApiVerb.GET, description = "Get the ancestor graph of the given term", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/term/{term}/ancestor-graph", method = { RequestMethod.GET }, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, CvTermGraph.View> getAncestorGraph(
