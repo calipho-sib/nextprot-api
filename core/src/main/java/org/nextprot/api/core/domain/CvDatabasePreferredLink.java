cd76f0e23d08b6cd3a0cb8fc6397df9d75a05f8b
fnikitin <frederic.nikitin@isb-sib.ch>
2018-07-06 16:06:29 +0200

    Add preferred link for uniprot domain db

--- core/src/main/java/org/nextprot/api/core/domain/CvDatabasePreferredLink.java
+++ core/src/main/java/org/nextprot/api/core/domain/CvDatabasePreferredLink.java
@@ -42,6 +42,7 @@ public enum CvDatabasePreferredLink {
 	SIGNOR("http://signor.uniroma2.it/relation_result.php?id=%u&organism=human"),
 	UCSC("https://genome.ucsc.edu/cgi-bin/hgLinkIn?resource=uniprot&id=%u"),
 	UNIPROT_DISEASES("http://www.uniprot.org/diseases/%s"),
+	UNIPROT_DOMAIN("https://www.uniprot.org/uniprot/?query=annotation%3A%28type%3Apositional+%s%29"),
 	UNIPROT_KEYWORDS("http://www.uniprot.org/keywords/%s"),
 	UNIPROT_LOCATIONS("http://www.uniprot.org/locations/%s"),
 	;
