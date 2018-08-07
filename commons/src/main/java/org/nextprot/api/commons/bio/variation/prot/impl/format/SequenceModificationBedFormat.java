070d8bce88ed9e0d37d99b7e0913ea4ffdb45b37
fnikitin <frederic.nikitin@isb-sib.ch>
2018-08-07 16:33:37 +0200

    Remove useless map

--- commons/src/main/java/org/nextprot/api/commons/bio/variation/prot/impl/format/SequenceModificationBedFormat.java
+++ commons/src/main/java/org/nextprot/api/commons/bio/variation/prot/impl/format/SequenceModificationBedFormat.java
@@ -1,27 +1,25 @@
 package org.nextprot.api.commons.bio.variation.prot.impl.format;
 
+import com.google.common.collect.Lists;
 import org.nextprot.api.commons.bio.AminoAcidCode;
-import org.nextprot.api.commons.bio.variation.prot.impl.seqchange.format.SingleModificationBEDFormat;
+import org.nextprot.api.commons.bio.variation.prot.SequenceVariation;
+import org.nextprot.api.commons.bio.variation.prot.SequenceVariationFormat;
+import org.nextprot.api.commons.bio.variation.prot.impl.seqchange.format.SingleGenericModificationBEDFormat;
 import org.nextprot.api.commons.bio.variation.prot.impl.varseq.format.AminoAcidModificationBEDFormatter;
 import org.nextprot.api.commons.bio.variation.prot.seqchange.SequenceChange;
 import org.nextprot.api.commons.bio.variation.prot.seqchange.SequenceChangeFormat;
-import org.nextprot.api.commons.bio.variation.prot.SequenceVariation;
-import org.nextprot.api.commons.bio.variation.prot.SequenceVariationFormat;
 
 import java.util.Collection;
-import java.util.EnumMap;
-import java.util.Map;
 
 public class SequenceModificationBedFormat extends SequenceVariationFormat {
 
     private final AminoAcidModificationBEDFormatter aminoAcidModificationFormatter;
-    private final Map<SequenceChange.Type, SequenceChangeFormat> changeFormats;
+    private final SequenceChangeFormat changeFormat;
 
     public SequenceModificationBedFormat() {
 
         aminoAcidModificationFormatter = new AminoAcidModificationBEDFormatter();
-        changeFormats = new EnumMap<>(SequenceChange.Type.class);
-        changeFormats.put(SequenceChange.Type.PTM, new SingleModificationBEDFormat());
+        changeFormat = new SingleGenericModificationBEDFormat();
     }
 
     @Override
@@ -33,13 +31,13 @@ public class SequenceModificationBedFormat extends SequenceVariationFormat {
     @Override
     protected SequenceChangeFormat getSequenceChangeFormat(SequenceChange.Type changeType) {
 
-        return changeFormats.get(changeType);
+        return changeFormat;
     }
 
     @Override
     protected Collection<SequenceChange.Type> getAvailableChangeTypes() {
 
-        return changeFormats.keySet();
+        return Lists.newArrayList(SequenceChange.Type.PTM);
     }
 
     @Override
@@ -47,8 +45,7 @@ public class SequenceModificationBedFormat extends SequenceVariationFormat {
 
         StringBuilder sb = new StringBuilder();
 
-        changeFormats.get(variation.getSequenceChange().getType()).format(sb, variation.getSequenceChange(), type);
-
+        changeFormat.format(sb, variation.getSequenceChange(), type);
         aminoAcidModificationFormatter.format(variation, type, sb);
 
         return sb.toString();
