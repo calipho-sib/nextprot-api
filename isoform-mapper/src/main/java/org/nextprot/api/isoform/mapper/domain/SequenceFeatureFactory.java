54993cd7643bef67b8137567ed4c6827a1e9ae44
fnikitin <frederic.nikitin@isb-sib.ch>
2018-08-22 13:08:21 +0200

    New factory method

--- isoform-mapper/src/main/java/org/nextprot/api/isoform/mapper/domain/SequenceFeatureFactory.java
+++ isoform-mapper/src/main/java/org/nextprot/api/isoform/mapper/domain/SequenceFeatureFactory.java
@@ -1,6 +1,7 @@
 package org.nextprot.api.isoform.mapper.domain;
 
 import org.nextprot.api.commons.constants.AnnotationCategory;
+import org.nextprot.api.commons.exception.NextProtException;
 import org.nextprot.api.core.service.BeanService;
 import org.nextprot.api.isoform.mapper.domain.impl.SequenceModification;
 import org.nextprot.api.isoform.mapper.domain.impl.SequenceVariant;
@@ -12,11 +13,35 @@ import org.nextprot.api.isoform.mapper.domain.impl.exception.InvalidFeatureQuery
  */
 public interface SequenceFeatureFactory {
 
-    static SequenceFeature newSequenceFeature(SingleFeatureQuery query, BeanService beanService) throws FeatureQueryException {
+    static SequenceFeature newSequenceFeature(String featureName, String featureType, BeanService beanService) throws Exception {
+
+        AnnotationCategory annotationCategory = AnnotationCategory.getDecamelizedAnnotationTypeName(featureType);
+
+        switch (annotationCategory) {
+            case MUTAGENESIS:
+                return SequenceVariant.mutagenesis(featureName, beanService);
+            case VARIANT:
+                return SequenceVariant.variant(featureName, beanService);
+            case GENERIC_PTM:
+                return new SequenceModification(featureName, beanService);
+            default:
+                throw new NextProtException("invalid feature type " + featureType + ", feature name=" + featureName);
+        }
+    }
+
+    /*static SequenceFeature newSequenceFeature(SingleFeatureQuery query, BeanService beanService) throws FeatureQueryException {
 
         // throw exception if invalid query
         query.checkFeatureQuery();
 
+        try {
+            return newSequenceFeature(query.getFeature(), query.getFeatureType(), beanService);
+        } catch (Exception e) {
+            throw new InvalidFeatureQueryTypeException(query);
+        }
+    }*/
+
+    static SequenceFeature newSequenceFeature(SingleFeatureQuery query, BeanService beanService) throws FeatureQueryException {
         AnnotationCategory annotationCategory = AnnotationCategory.getDecamelizedAnnotationTypeName(query.getFeatureType());
 
         try {
@@ -30,8 +55,7 @@ public interface SequenceFeatureFactory {
                 default:
                     throw new InvalidFeatureQueryTypeException(query);
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             throw new InvalidFeatureQueryFormatException(query, e);
         }
     }
