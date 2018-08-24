feef1a9e38ac2ee552261a9a2e3a8ed2ca44ed66
fnikitin <frederic.nikitin@isb-sib.ch>
2018-08-24 12:25:08 +0200

    Changing message name

--- isoform-mapper/src/main/java/org/nextprot/api/isoform/mapper/domain/impl/exception/InvalidFeatureQueryFormatException.java
+++ isoform-mapper/src/main/java/org/nextprot/api/isoform/mapper/domain/impl/exception/InvalidFeatureQueryFormatException.java
@@ -5,13 +5,13 @@ import org.nextprot.api.isoform.mapper.domain.SingleFeatureQuery;
 
 public class InvalidFeatureQueryFormatException extends FeatureQueryException {
 
-    public static final String PARSE_ERROR_MESSAGE = "errorMessage";
+    public static final String ERROR_MESSAGE = "errorMessage";
 
     public InvalidFeatureQueryFormatException(SingleFeatureQuery query, Exception exception) {
 
         super(query);
 
-        getReason().addCause(PARSE_ERROR_MESSAGE, exception.getMessage());
+        getReason().addCause(ERROR_MESSAGE, exception.getMessage());
         getReason().setMessage("invalid feature format: " + query.getFeature());
     }
 }
