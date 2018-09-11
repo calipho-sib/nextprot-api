00cfdbacbb112477556f9629dcbc3e7834f0c156
fnikitin <frederic.nikitin@isb-sib.ch>
2018-09-11 11:21:38 +0200

    Do not throw NextProtException if missing isoform mappings

--- core/src/main/java/org/nextprot/api/core/service/annotation/merge/impl/FeaturePositionMatcher.java
+++ core/src/main/java/org/nextprot/api/core/service/annotation/merge/impl/FeaturePositionMatcher.java
@@ -1,14 +1,16 @@
 package org.nextprot.api.core.service.annotation.merge.impl;
 
-import org.nextprot.api.commons.exception.NextProtException;
 import org.nextprot.api.core.domain.annotation.AnnotationIsoformSpecificity;
 import org.nextprot.api.core.service.annotation.merge.ObjectMatcher;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class FeaturePositionMatcher implements ObjectMatcher<Map<String, AnnotationIsoformSpecificity>> {
+
+    private static final Logger LOGGER = Logger.getLogger(FeaturePositionMatcher.class.getName());
 
     @Override
     public boolean match(Map<String, AnnotationIsoformSpecificity> m1, Map<String, AnnotationIsoformSpecificity> m2) {
@@ -23,29 +25,37 @@ public class FeaturePositionMatcher implements ObjectMatcher<Map<String, Annotat
                 Set<String> otherIsoNames = isoNames.stream().filter(isoName -> !isoName.equals(name)).collect(Collectors.toSet());
 
                 // other isoforms should have same variant at same locations
-                otherIsoformPositionsShouldBeValid(m1, m2, otherIsoNames);
-
-                return true;
+                return otherIsoformPositionsShouldBeValid(m1, m2, otherIsoNames);
             }
         }
 
         return false;
     }
 
-    private void otherIsoformPositionsShouldBeValid(Map<String, AnnotationIsoformSpecificity> m1, Map<String, AnnotationIsoformSpecificity> m2,
+    private boolean otherIsoformPositionsShouldBeValid(Map<String, AnnotationIsoformSpecificity> m1, Map<String, AnnotationIsoformSpecificity> m2,
                                                     Set<String> others) {
         for (String isoName : others) {
 
             if (!m1.containsKey(isoName) || !m2.containsKey(isoName)) {
 
-                throw new NextProtException("missing isoform positions for "+isoName+": map1: "+m1.containsKey(isoName)+", map2: "+m2.containsKey(isoName));
+                String message = "missing isoform positions for "+isoName+": in first map? "+m1.containsKey(isoName)+", in second map? "+m2.containsKey(isoName);
+                LOGGER.warning(message);
+                //throw new NextProtException(message);
+
+                return false;
             }
             else if (!m1.get(isoName).hasSameIsoformPositions(m2.get(isoName))) {
 
-                throw new NextProtException("conflicting propagation for "+isoName+": map1: "+
-                        m1.get(isoName).getFirstPosition()+ "-" + m1.get(isoName).getLastPosition() +" vs map2: " +
-                        m2.get(isoName).getFirstPosition()+ "-" + m2.get(isoName).getLastPosition());
+                String message = "conflicting propagation for "+isoName+": first map: "+
+                        m1.get(isoName).getFirstPosition()+ "-" + m1.get(isoName).getLastPosition() +" vs second map: " +
+                        m2.get(isoName).getFirstPosition()+ "-" + m2.get(isoName).getLastPosition();
+                LOGGER.warning(message);
+                //throw new NextProtException(message);
+
+                return false;
             }
         }
+
+        return true;
     }
 }
