d8061a5ee7e9b463aad215af0dce65eff98ed876
fnikitin <frederic.nikitin@isb-sib.ch>
2018-09-11 12:39:17 +0200

    Fixing missing isoform annotation start and end positions

--- core/src/main/java/org/nextprot/api/core/service/impl/peff/PEFFVariantSimple.java
+++ core/src/main/java/org/nextprot/api/core/service/impl/peff/PEFFVariantSimple.java
@@ -6,6 +6,7 @@ import org.nextprot.api.core.domain.annotation.Annotation;
 import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * A variation located on an isoform
@@ -13,6 +14,8 @@ import java.util.List;
  * Created by fnikitin on 05/05/15.
  */
 public class PEFFVariantSimple extends AnnotationBasedPEFFInformation {
+
+    private static final Logger LOGGER = Logger.getLogger(PEFFVariantSimple.class.getName());
 
     public PEFFVariantSimple(String isoformAccession, List<Annotation> isoformAnnotations) {
 
@@ -28,6 +31,17 @@ public class PEFFVariantSimple extends AnnotationBasedPEFFInformation {
 
     @Override
     protected boolean selectAnnotation(Annotation annotation) {
+
+        if (annotation.getStartPositionForIsoform(isoformAccession) == null) {
+
+            LOGGER.warning("missing start position, annotation="+annotation.getUniqueName()+", isoform="+isoformAccession);
+            return false;
+        }
+        else if (annotation.getEndPositionForIsoform(isoformAccession) == null) {
+
+            LOGGER.warning("missing end position, annotation="+annotation.getUniqueName()+", isoform="+isoformAccession);
+            return false;
+        }
 
         return super.selectAnnotation(annotation) &&
                 annotation.getStartPositionForIsoform(isoformAccession).intValue() == annotation.getEndPositionForIsoform(isoformAccession).intValue() &&
