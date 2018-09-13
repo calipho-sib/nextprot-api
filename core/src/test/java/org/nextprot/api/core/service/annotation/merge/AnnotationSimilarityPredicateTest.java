2fdf925ddae7d5e2b785ce9c53a027da0a948f9e
fnikitin <frederic.nikitin@isb-sib.ch>
2018-09-13 15:21:52 +0200

    New test for variant predicate

--- core/src/test/java/org/nextprot/api/core/service/annotation/merge/AnnotationSimilarityPredicateTest.java
+++ core/src/test/java/org/nextprot/api/core/service/annotation/merge/AnnotationSimilarityPredicateTest.java
@@ -6,6 +6,13 @@ import org.mockito.Mockito;
 import org.nextprot.api.commons.constants.AnnotationCategory;
 import org.nextprot.api.core.domain.BioObject;
 import org.nextprot.api.core.domain.annotation.Annotation;
+import org.nextprot.api.core.domain.annotation.AnnotationIsoformSpecificity;
+import org.nextprot.api.core.domain.annotation.AnnotationVariant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.nextprot.api.core.domain.annotation.AnnotationIsoformSpecificityTest.mockAnnotationIsoformSpecificity;
 
 public class AnnotationSimilarityPredicateTest {
 
@@ -41,6 +48,24 @@ public class AnnotationSimilarityPredicateTest {
         Assert.assertTrue(predicate.isSimilar(mockAnnotation("NX_P43246", "nextprot"), mockAnnotation("NX_P43246", "neXtProt")));
     }
 
+    @Test
+    public void annotationVariantShouldBeSimilar() {
+
+        AnnotationSimilarityPredicate predicate = AnnotationSimilarityPredicate.newSimilarityPredicate(AnnotationCategory.VARIANT);
+
+        Assert.assertTrue(predicate.isSimilar(mockVariant("NX_P43246", "nextprot", "A", "L", "NX_Q15858", 29),
+                mockVariant("NX_P43246", "nextprot", "A", "L", "NX_Q15858", 29)));
+    }
+
+    @Test
+    public void annotationVariantShouldNotBeSimilar() {
+
+        AnnotationSimilarityPredicate predicate = AnnotationSimilarityPredicate.newSimilarityPredicate(AnnotationCategory.VARIANT);
+
+        Assert.assertFalse(predicate.isSimilar(mockVariant("NX_P43246", "nextprot", "A", "L", "NX_Q15858", 29),
+                mockVariant("NX_P43246", "nextprot", "A", "L", "NX_Q15858", 39)));
+    }
+
     private static Annotation mockAnnotation(String accession, String database) {
 
         Annotation annot = Mockito.mock(Annotation.class);
@@ -49,6 +74,27 @@ public class AnnotationSimilarityPredicateTest {
         Mockito.when(bo.getAccession()).thenReturn(accession);
         Mockito.when(bo.getDatabase()).thenReturn(database);
 
+        Mockito.when(annot.getBioObject()).thenReturn(bo);
+
+        return annot;
+    }
+
+    private static Annotation mockVariant(String accession, String database, String variant, String original, String isoName, int from) {
+
+        Annotation annot = Mockito.mock(Annotation.class);
+        BioObject bo = Mockito.mock(BioObject.class);
+        AnnotationVariant av = Mockito.mock(AnnotationVariant.class);
+        Map<String, AnnotationIsoformSpecificity> specificityMap = new HashMap<>();
+        specificityMap.put(isoName, mockAnnotationIsoformSpecificity(isoName, from, from));
+
+        Mockito.when(bo.getAccession()).thenReturn(accession);
+        Mockito.when(bo.getDatabase()).thenReturn(database);
+        Mockito.when(av.getVariant()).thenReturn(variant);
+        Mockito.when(av.getOriginal()).thenReturn(original);
+        Mockito.when(annot.getTargetingIsoformsMap()).thenReturn(specificityMap);
+
+        Mockito.when(annot.getAPICategory()).thenReturn(AnnotationCategory.VARIANT);
+        Mockito.when(annot.getVariant()).thenReturn(av);
         Mockito.when(annot.getBioObject()).thenReturn(bo);
 
         return annot;
