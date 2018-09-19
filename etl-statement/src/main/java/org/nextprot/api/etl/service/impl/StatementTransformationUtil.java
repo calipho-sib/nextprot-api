5eb73716b4096b9e0ca4e18baf1d725df0ccea8c
fnikitin <frederic.nikitin@isb-sib.ch>
2018-09-19 12:21:43 +0200

    Refactor method mapVariationStatementToEntry()

--- etl-statement/src/main/java/org/nextprot/api/etl/service/impl/StatementTransformationUtil.java
+++ etl-statement/src/main/java/org/nextprot/api/etl/service/impl/StatementTransformationUtil.java
@@ -39,6 +39,7 @@ public class StatementTransformationUtil {
         if (!isoformAccessions.isEmpty()) {
             AnnotationCategory category = AnnotationCategory.getDecamelizedAnnotationTypeName(statement.getValue(StatementField.ANNOTATION_CATEGORY));
 
+            // POSITIONAL ANNOTATIONS
             if (category == AnnotationCategory.MODIFIED_RESIDUE || category == AnnotationCategory.GLYCOSYLATION_SITE) {
 
                 String featureName = statement.getValue(ANNOTATION_NAME);
@@ -71,6 +72,7 @@ public class StatementTransformationUtil {
                     //throw new NextProtException(errorMessage);
                 }
             }
+            // NON-POSITIONAL ANNOTATIONS
             else {
                 for (String isoAccession : isoformAccessions) {
 
@@ -191,16 +193,9 @@ public class StatementTransformationUtil {
 	 * @param result
 	 * @return
 	 */
-	static Statement mapVariationStatementToEntry(Statement variationStatement, FeatureQuerySuccess result) {
+	static IsoformPositions calcIsoformPositions(Statement variationStatement, FeatureQuerySuccess result) {
 
-		String beginPositionOfCanonicalOrIsoSpec = null;
-		String endPositionOfCanonicalOrIsoSpec = null;
-
-		String masterBeginPosition = null;
-		String masterEndPosition = null;
-
-		String isoCanonical = null;
-
+        IsoformPositions isoformPositions = new IsoformPositions();
 		TargetIsoformSet targetIsoforms = new TargetIsoformSet();
 
 		for (IsoformFeatureResult isoformFeatureResult : result.getData().values()) {
@@ -222,56 +217,123 @@ public class StatementTransformationUtil {
 
 				// Will be set in case that we don't want to propagate to
 				// canonical
-				if (beginPositionOfCanonicalOrIsoSpec == null) {
-					beginPositionOfCanonicalOrIsoSpec = String.valueOf(isoformFeatureResult.getBeginIsoformPosition());
+				if (isoformPositions.getBeginPositionOfCanonicalOrIsoSpec() == null) {
+                    isoformPositions.setBeginPositionOfCanonicalOrIsoSpec(isoformFeatureResult.getBeginIsoformPosition());
 				}
-				if (endPositionOfCanonicalOrIsoSpec == null) {
-					endPositionOfCanonicalOrIsoSpec = String.valueOf(isoformFeatureResult.getEndIsoformPosition());
+				if (isoformPositions.getEndPositionOfCanonicalOrIsoSpec() == null) {
+                    isoformPositions.setEndPositionOfCanonicalOrIsoSpec(isoformFeatureResult.getEndIsoformPosition());
 				}
 
 				// If possible use canonical
 				if (isoformFeatureResult.isCanonical()) {
-					if (isoCanonical != null) {
+					if (isoformPositions.getCanonicalIsoform() != null) {
 						throw new NextProtException("Canonical position set already");
 					}
-					isoCanonical = isoformFeatureResult.getIsoformAccession();
-					beginPositionOfCanonicalOrIsoSpec = String.valueOf(isoformFeatureResult.getBeginIsoformPosition());
-					endPositionOfCanonicalOrIsoSpec = String.valueOf(isoformFeatureResult.getEndIsoformPosition());
+					isoformPositions.setCanonicalIsoform(isoformFeatureResult.getIsoformAccession());
+                    isoformPositions.setBeginPositionOfCanonicalOrIsoSpec(isoformFeatureResult.getBeginIsoformPosition());
+                    isoformPositions.setEndPositionOfCanonicalOrIsoSpec(isoformFeatureResult.getEndIsoformPosition());
 				}
 
-				if (masterBeginPosition == null) {
-					masterBeginPosition = String.valueOf(isoformFeatureResult.getBeginMasterPosition());
+				if (isoformPositions.getMasterBeginPosition() == null) {
+                    isoformPositions.setMasterBeginPosition(isoformFeatureResult.getBeginMasterPosition());
 				}
 
-				if (masterEndPosition == null) {
-					masterEndPosition = String.valueOf(isoformFeatureResult.getEndMasterPosition());
+				if (isoformPositions.getMasterEndPosition() == null) {
+                    isoformPositions.setMasterEndPosition(isoformFeatureResult.getEndMasterPosition());
 				}
 
-				if (masterBeginPosition != null) {
-					if (!masterBeginPosition.equals(String.valueOf(isoformFeatureResult.getBeginMasterPosition()))) {
-						throw new NextProtException("Begin master position " + masterBeginPosition + " does not match " + String.valueOf(isoformFeatureResult.getBeginMasterPosition()
-								+ " for different isoforms (" + result.getData().values().size() + ") for statement " + variationStatement.getStatementId()));
+				if (isoformPositions.getMasterBeginPosition() != null) {
+					if (!isoformPositions.getMasterBeginPosition().equals(isoformFeatureResult.getBeginMasterPosition())) {
+						throw new NextProtException("Begin master position " + isoformPositions.getMasterBeginPosition()
+                                + " does not match " + isoformFeatureResult.getBeginMasterPosition()
+								+ " for different isoforms (" + result.getData().values().size() + ") for statement " + variationStatement.getStatementId());
 					}
 				}
 
-				if (masterEndPosition != null) {
-					if (!masterEndPosition.equals(String.valueOf(isoformFeatureResult.getEndMasterPosition()))) {
+				if (isoformPositions.getMasterEndPosition() != null) {
+					if (!isoformPositions.getMasterEndPosition().equals(isoformFeatureResult.getEndMasterPosition())) {
 						throw new NextProtException("End master position does not match for different isoforms" + variationStatement.getStatementId());
 					}
 				}
-
 			}
-
 		}
 
-		Statement rs = StatementBuilder.createNew().addMap(variationStatement).addField(StatementField.RAW_STATEMENT_ID, variationStatement.getStatementId()) // Keep
-																																								// statement
-				.addField(StatementField.LOCATION_BEGIN, beginPositionOfCanonicalOrIsoSpec).addField(StatementField.LOCATION_END, endPositionOfCanonicalOrIsoSpec)
-				.addField(StatementField.LOCATION_BEGIN_MASTER, masterBeginPosition).addField(StatementField.LOCATION_END_MASTER, masterEndPosition)
-				.addField(StatementField.ISOFORM_CANONICAL, isoCanonical).addField(StatementField.TARGET_ISOFORMS, targetIsoforms.serializeToJsonString())
-				.buildWithAnnotationHash();
+		isoformPositions.setTargetIsoformSet(targetIsoforms);
 
-		return rs;
+		return isoformPositions;
 	}
+
+    private static Statement mapVariationStatementToEntry(Statement variationStatement, FeatureQuerySuccess result) {
+
+        IsoformPositions isoformPositions = calcIsoformPositions(variationStatement, result);
+
+        return StatementBuilder.createNew()
+                .addMap(variationStatement).addField(StatementField.RAW_STATEMENT_ID, variationStatement.getStatementId()) // Keep statement
+                .addField(StatementField.LOCATION_BEGIN, String.valueOf(isoformPositions.getBeginPositionOfCanonicalOrIsoSpec()))
+                .addField(StatementField.LOCATION_END, String.valueOf(isoformPositions.getEndPositionOfCanonicalOrIsoSpec()))
+                .addField(StatementField.LOCATION_BEGIN_MASTER, String.valueOf(isoformPositions.getMasterBeginPosition()))
+                .addField(StatementField.LOCATION_END_MASTER, String.valueOf(isoformPositions.getMasterEndPosition()))
+                .addField(StatementField.ISOFORM_CANONICAL, isoformPositions.getCanonicalIsoform())
+                .addField(StatementField.TARGET_ISOFORMS, isoformPositions.getTargetIsoformSet().serializeToJsonString())
+                .buildWithAnnotationHash();
+    }
+
+    private static class IsoformPositions {
+
+        private Integer beginPositionOfCanonicalOrIsoSpec = null;
+        private Integer endPositionOfCanonicalOrIsoSpec = null;
+        private Integer masterBeginPosition = null;
+        private Integer masterEndPosition = null;
+        private String canonicalIsoform = null;
+        private TargetIsoformSet targetIsoformSet;
+
+        public Integer getBeginPositionOfCanonicalOrIsoSpec() {
+            return beginPositionOfCanonicalOrIsoSpec;
+        }
+
+        public void setBeginPositionOfCanonicalOrIsoSpec(Integer beginPositionOfCanonicalOrIsoSpec) {
+            this.beginPositionOfCanonicalOrIsoSpec = beginPositionOfCanonicalOrIsoSpec;
+        }
+
+        public Integer getEndPositionOfCanonicalOrIsoSpec() {
+            return endPositionOfCanonicalOrIsoSpec;
+        }
+
+        public void setEndPositionOfCanonicalOrIsoSpec(Integer endPositionOfCanonicalOrIsoSpec) {
+            this.endPositionOfCanonicalOrIsoSpec = endPositionOfCanonicalOrIsoSpec;
+        }
+
+        public Integer getMasterBeginPosition() {
+            return masterBeginPosition;
+        }
+
+        public void setMasterBeginPosition(Integer masterBeginPosition) {
+            this.masterBeginPosition = masterBeginPosition;
+        }
+
+        public Integer getMasterEndPosition() {
+            return masterEndPosition;
+        }
+
+        public void setMasterEndPosition(Integer masterEndPosition) {
+            this.masterEndPosition = masterEndPosition;
+        }
+
+        public String getCanonicalIsoform() {
+            return canonicalIsoform;
+        }
+
+        public void setCanonicalIsoform(String canonicalIsoform) {
+            this.canonicalIsoform = canonicalIsoform;
+        }
+
+        public TargetIsoformSet getTargetIsoformSet() {
+            return targetIsoformSet;
+        }
+
+        public void setTargetIsoformSet(TargetIsoformSet targetIsoformSet) {
+            this.targetIsoformSet = targetIsoformSet;
+        }
+    }
 
 }
