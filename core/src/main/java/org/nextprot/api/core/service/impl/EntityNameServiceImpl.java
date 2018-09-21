4404ad1a15eda7c2339734a17c0f8a0a11dd1a1b
fnikitin <frederic.nikitin@isb-sib.ch>
2018-09-21 16:30:22 +0200

    Add new contract findNamesByEntityNameClass() in EntityNameService

--- core/src/main/java/org/nextprot/api/core/service/impl/EntityNameServiceImpl.java
+++ core/src/main/java/org/nextprot/api/core/service/impl/EntityNameServiceImpl.java
@@ -1,13 +1,24 @@
 package org.nextprot.api.core.service.impl;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import org.nextprot.api.commons.utils.StreamUtils;
+import org.nextprot.api.core.dao.EntityNameDao;
 import org.nextprot.api.core.domain.EntityName;
+import org.nextprot.api.core.domain.Overview;
 import org.nextprot.api.core.service.EntityNameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 
 @Service
 class EntityNameServiceImpl implements EntityNameService {
+
+    @Autowired
+    private EntityNameDao entryNameDao;
 
 	@Override
 	public boolean hasNameIgnoreCase(EntityName entityName, String name) {
@@ -39,4 +50,88 @@ class EntityNameServiceImpl implements EntityNameService {
 
 		return false;
 	}
+
+    @Override
+    public Map<Overview.EntityNameClass, List<EntityName>> findNamesByEntityNameClass(String uniqueName) {
+
+        List<EntityName> entityNames = entryNameDao.findNames(uniqueName);
+        entityNames.addAll(entryNameDao.findAlternativeChainNames(uniqueName));
+
+        Map<Overview.EntityNameClass, List<EntityName>> map = new EnumMap<>(Overview.EntityNameClass.class);
+
+        Map<String, EntityName> entityMap = Maps.uniqueIndex(entityNames, EntityName::getId);
+
+        Map<String, EntityName> mutableEntityMap = Maps.newHashMap(entityMap);
+        String parentId;
+
+        for (EntityName entityName : entityMap.values()) {
+
+            parentId = entityName.getParentId();
+            if (parentId != null && mutableEntityMap.containsKey(parentId)) {
+
+                if (entityName.isMain()) {
+                    mutableEntityMap.get(parentId).addOtherRecommendedEntityName(entityName);
+                }
+                else {
+                    mutableEntityMap.get(parentId).addSynonym(entityName);
+                }
+            } else {
+                mutableEntityMap.put(entityName.getId(), entityName);
+            }
+        }
+
+        List<EntityName> mutableEntityNames = new ArrayList<>(mutableEntityMap.values());
+
+        for (EntityName entityName : mutableEntityMap.values())
+            if (entityName.getParentId() != null)
+                mutableEntityNames.remove(entityName);
+
+        Multimap<Overview.EntityNameClass, EntityName> entryNameMap = Multimaps.index(mutableEntityNames, EntityName::getClazz);
+
+        for (Overview.EntityNameClass en : entryNameMap.keySet()) {
+
+            switch (en) {
+                case PROTEIN_NAMES: {
+                    map.put(en, getSortedList(entryNameMap, en));
+                    break;
+                }
+                case GENE_NAMES: {
+                    map.put(en, getSortedList(entryNameMap, en, Comparator.comparing(EntityName::getName)));
+                    break;
+                }
+                case CLEAVED_REGION_NAMES: {
+                    map.put(en, getSortedList(entryNameMap, en));
+                    break;
+                }
+                case ADDITIONAL_NAMES: {
+                    map.put(en, getSortedList(entryNameMap, en));
+                    break;
+                }
+                case FUNCTIONAL_REGION_NAMES: {
+                    map.put(en, getSortedList(entryNameMap, en));
+                    break;
+                }
+            }
+        }
+
+        return map;
+    }
+
+    private static List<EntityName> getSortedList(Multimap<Overview.EntityNameClass, EntityName> entryMap, Overview.EntityNameClass en) {
+
+        return getSortedList(entryMap, en, EntityName.newDefaultComparator());
+    }
+
+    private static List<EntityName> getSortedList(Multimap<Overview.EntityNameClass, EntityName> entryMap, Overview.EntityNameClass en, Comparator<EntityName> comparator) {
+
+        List<EntityName> list = new ArrayList<>(entryMap.get(en));
+        for(EntityName e : list){
+            if(e.getSynonyms() != null){
+                e.getSynonyms().sort(comparator);
+            }
+        }
+        list.sort(comparator);
+
+        return list;
+    }
 }
