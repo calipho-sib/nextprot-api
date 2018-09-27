48f7e18a00fd97912ff0403fc03c9e0d6a9494b6
fnikitin <frederic.nikitin@isb-sib.ch>
2018-09-27 16:45:38 +0200

    Add one more test that check rule 7

--- core/src/main/java/org/nextprot/api/core/service/impl/ProteinExistenceInferenceServiceImpl.java
+++ core/src/main/java/org/nextprot/api/core/service/impl/ProteinExistenceInferenceServiceImpl.java
@@ -22,6 +22,9 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * See specifications on https://calipho.isb-sib.ch/wiki/display/cal/Protein+existence+%28PE%29+upgrade+rules
+ */
 @Service
 class ProteinExistenceInferenceServiceImpl implements ProteinExistenceInferenceService {
 
