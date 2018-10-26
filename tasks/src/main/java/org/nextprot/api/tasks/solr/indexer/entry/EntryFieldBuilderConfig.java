3c85261e05ba5777559164f6c7d8e885949fbe67
fnikitin <frederic.nikitin@isb-sib.ch>
2018-10-26 16:54:51 +0200

    Config spring for automatic component scan

---
+++ tasks/src/main/java/org/nextprot/api/tasks/solr/indexer/entry/EntryFieldBuilderConfig.java
@@ -0,0 +1,9 @@
+package org.nextprot.api.tasks.solr.indexer.entry;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.nextprot.api.tasks.solr.indexer.entry.impl")
+public class EntryFieldBuilderConfig {
+}
