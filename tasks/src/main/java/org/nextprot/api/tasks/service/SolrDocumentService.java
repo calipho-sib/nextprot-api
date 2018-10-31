95c824211659486854c9e28ebbd0893827a1a167
fnikitin <frederic.nikitin@isb-sib.ch>
2018-10-31 11:03:19 +0100

    Remove useless classes

--- tasks/src/main/java/org/nextprot/api/tasks/service/SolrDocumentService.java
+++
@@ -1,9 +0,0 @@
-package org.nextprot.api.tasks.service;
-
-import org.apache.solr.common.SolrInputDocument;
-import org.nextprot.api.core.domain.Entry;
-
-public interface SolrDocumentService {
-
-	SolrInputDocument solrDocument(Entry entry);
-}
--- tasks/src/main/java/org/nextprot/api/tasks/solr/indexer/entry/EntryFieldBuilderConfig.java
+++
@@ -1,9 +0,0 @@
-package org.nextprot.api.tasks.solr.indexer.entry;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("org.nextprot.api.tasks.solr.indexer.entry.impl")
-public class EntryFieldBuilderConfig {
-}
