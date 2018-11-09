cf974aaf991d2b2b0464690f0d5ffdc4c6eed66d
fnikitin <frederic.nikitin@isb-sib.ch>
2018-11-09 15:26:31 +0100

    New tests for chr field collector

---
+++ solr/src/test/java/org/nextprot/api/solr/indexation/impl/solrdoc/entrydoc/ChromosomeSolrFieldCollectorTest.java
@@ -0,0 +1,54 @@
+package org.nextprot.api.solr.indexation.impl.solrdoc.entrydoc;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.nextprot.api.commons.dbunit.AbstractUnitBaseTest;
+import org.nextprot.api.core.domain.Entry;
+import org.nextprot.api.core.service.EntryBuilderService;
+import org.nextprot.api.core.service.fluent.EntryConfig;
+import org.nextprot.api.solr.core.impl.schema.EntrySolrField;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+@ActiveProfiles({"dev"})
+@ContextConfiguration("classpath:spring/solr-context.xml")
+public class ChromosomeSolrFieldCollectorTest extends AbstractUnitBaseTest {
+
+	@Autowired
+	private EntryBuilderService entryBuilderService;
+
+	// Class under test
+	@Autowired
+	private ChromosomeSolrFieldCollector collector;
+
+	@Test
+	public void testSorting() {
+		Integer computedValue = ChromosomeSolrFieldCollector.sortChr("18q21.33");
+		assertTrue(computedValue.equals(18071330));
+	}
+
+	@Test
+	public void testNX_Q9P2G1() {
+
+		Entry entry = entryBuilderService.build(EntryConfig.newConfig("NX_Q9P2G1").withChromosomalLocations());
+
+		Map<EntrySolrField, Object> fields = new HashMap<>();
+
+		collector.collect(fields, entry, true);
+
+		Assert.assertTrue(fields.get(EntrySolrField.GENE_BAND) instanceof List);
+		List bands = (List)fields.get(EntrySolrField.GENE_BAND);
+		Assert.assertEquals(1, bands.size());
+		// TODO: do we really want only one element that is a string with space delimitor?
+		Assert.assertEquals("q21.2 7q21.2", bands.get(0));
+		Assert.assertEquals("7q21.2", fields.get(EntrySolrField.CHR_LOC));
+		Assert.assertEquals(7071200, fields.get(EntrySolrField.CHR_LOC_S));
+	}
+}
