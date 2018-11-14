4518b8e600cb00a7abdc58450e0df9747e6546c8
fnikitin <frederic.nikitin@isb-sib.ch>
2018-11-14 11:49:45 +0100

    Remove the useless access level public keywords

--- web/src/main/java/org/nextprot/api/web/service/QueryBuilderService.java
+++ web/src/main/java/org/nextprot/api/web/service/QueryBuilderService.java
@@ -5,11 +5,8 @@ import org.nextprot.api.solr.query.dto.QueryRequest;
 
 public interface QueryBuilderService {
 
-	public Query buildQueryForSearch(QueryRequest queryRequest, String indexName);
-
-	
-	public Query buildQueryForProteinLists(String indexName, String queryString, String quality, String sort, String order, String start, String rows, String filter);
-	public Query buildQueryForSearchIndexes(String indexName, String configurationName, QueryRequest request);
-	public Query buildQueryForAutocomplete(String indexName, String queryString, String quality, String sort, String order, String start, String rows, String filter);
-
+	Query buildQueryForSearch(QueryRequest queryRequest, String indexName);
+	Query buildQueryForProteinLists(String indexName, String queryString, String quality, String sort, String order, String start, String rows, String filter);
+	Query buildQueryForSearchIndexes(String indexName, String configurationName, QueryRequest request);
+	Query buildQueryForAutocomplete(String indexName, String queryString, String quality, String sort, String order, String start, String rows, String filter);
 }
