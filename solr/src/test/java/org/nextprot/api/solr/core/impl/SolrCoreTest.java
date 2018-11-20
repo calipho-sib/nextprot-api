52c4a19c44bc6ce646f1d349f1c79346767d9733
fnikitin <frederic.nikitin@isb-sib.ch>
2018-11-20 15:48:33 +0100

    Refactor test for protein search diffs

--- solr/src/test/java/org/nextprot/api/solr/core/impl/SolrCoreTest.java
+++ solr/src/test/java/org/nextprot/api/solr/core/impl/SolrCoreTest.java
@@ -42,50 +42,34 @@ public class SolrCoreTest {
 	}
 
 	@Test
-	public void compareResultsFromCrickAndKantForQueryMSH6() throws QueryConfiguration.MissingSortConfigException {
-
-		SolrCore<EntrySolrField> coreBuild = new SolrGoldOnlyEntryCore("http://kant:8983/solr");
-		SolrCore<EntrySolrField> coreAlpha = new SolrGoldOnlyEntryCore("http://uat-web2:8983/solr");
-
-		for (SortConfig.Criteria criteria : SortConfig.Criteria.values()) {
-
-			Query<EntrySolrField> queryBuild = new Query<>(coreBuild).rows(50).addQuery("MSH6").sort(criteria);
-			Query<EntrySolrField> queryAlpha = new Query<>(coreAlpha).rows(50).addQuery("MSH6").sort(criteria);
-
-			SearchResult bResult = executeQuery(queryBuild);
-			SearchResult aResult = executeQuery(queryAlpha);
-
-			SearchResultDiff srd = SearchResultDiff.compare(bResult, aResult);
-			Assert.assertTrue("criteria "+criteria+", diffs: "+srd.toString(), srd.equals);
-		}
-	}
-
-	@Test
-	public void compareResultsFromCrickAndKantForQueryMSH6AllSolrFields() throws QueryConfiguration.MissingSortConfigException {
+	public void compareProteinSearchResultsFromCrickAndKantForQueryMSH6AllSolrFields() throws QueryConfiguration.MissingSortConfigException {
 
 		Set<EntrySolrField> allEntrySolrFieldSet = new HashSet<>(Arrays.asList(EntrySolrField.values()));
 		allEntrySolrFieldSet.remove(EntrySolrField.TEXT);
 		allEntrySolrFieldSet.remove(EntrySolrField.SCORE);
 
-		SolrCore<EntrySolrField> coreBuild = new SolrGoldOnlyEntryCore("http://kant:8983/solr", allEntrySolrFieldSet);
-		SolrCore<EntrySolrField> coreAlpha = new SolrGoldOnlyEntryCore("http://crick:8983/solr", allEntrySolrFieldSet);
+		ProteinSearchResultDiff diff = proteinQueryDiffs("kant", "crick", "MSH6", allEntrySolrFieldSet, SortConfig.Criteria.AC);
 
-		Query<EntrySolrField> queryBuild = new Query<>(coreBuild).rows(50)
-				.addQuery("MSH6")
-				.sort(SortConfig.Criteria.AC);
-
-		Query<EntrySolrField> queryAlpha = new Query<>(coreAlpha).rows(50)
-				.addQuery("MSH6")
-				.sort(SortConfig.Criteria.AC);
-
-		SearchResult bResult = executeQuery(queryBuild);
-		SearchResult aResult = executeQuery(queryAlpha);
-
-		SearchResultDiff srd = SearchResultDiff.compare(bResult, aResult);
-		Assert.assertTrue("criteria "+SortConfig.Criteria.AC+", diffs: "+srd.toString(), srd.equals);
+		Assert.assertTrue("criteria "+SortConfig.Criteria.AC+", diffs: "+diff.toString(), diff.equals);
 	}
 
-	private static class SearchResultDiff {
+	private ProteinSearchResultDiff proteinQueryDiffs(String hostname1, String hostname2, String query, Set<EntrySolrField> fl, SortConfig.Criteria criteria) throws QueryConfiguration.MissingSortConfigException {
+
+		SolrCore<EntrySolrField> core1 = new SolrGoldOnlyEntryCore("http://"+hostname1+":8983/solr", fl);
+		SolrCore<EntrySolrField> core2 = new SolrGoldOnlyEntryCore("http://"+hostname2+":8983/solr", fl);
+
+		Query<EntrySolrField> queryOnHost1 = new Query<>(core1).rows(50)
+				.addQuery(query)
+				.sort(criteria);
+
+		Query<EntrySolrField> queryOnHost2 = new Query<>(core2).rows(50)
+				.addQuery(query)
+				.sort(criteria);
+
+		return ProteinSearchResultDiff.compare(executeQuery(queryOnHost1), executeQuery(queryOnHost2));
+	}
+
+	private static class ProteinSearchResultDiff {
 
 		private FieldDiff<String> entity;
 		private FieldDiff<String> index;
@@ -94,9 +78,9 @@ public class SolrCoreTest {
 		private ListDiff results;
 		private boolean equals;
 
-		static SearchResultDiff compare(SearchResult sr1, SearchResult sr2) {
+		static ProteinSearchResultDiff compare(SearchResult sr1, SearchResult sr2) {
 
-			SearchResultDiff diff = new SearchResultDiff();
+			ProteinSearchResultDiff diff = new ProteinSearchResultDiff();
 
 			diff.entity = new FieldDiff<>(sr1.getEntity(), sr2.getEntity());
 			diff.index = new FieldDiff<>(sr1.getIndex(), sr2.getIndex());
@@ -129,175 +113,175 @@ public class SolrCoreTest {
 
 			return sb.toString();
 		}
-	}
 
-	private static class FieldDiff<T extends Object> {
+		private static class FieldDiff<T extends Object> {
 
-		private T o1;
-		private T o2;
-		private boolean equals;
+			private T o1;
+			private T o2;
+			private boolean equals;
 
-		FieldDiff(T o1, T o2) {
+			FieldDiff(T o1, T o2) {
 
-			this(o1, o2, (a, b) -> Objects.equals(a, b));
-		}
-
-		FieldDiff(T o1, T o2, BiFunction<T, T, Boolean> op) {
-
-			this.o1 = o1;
-			this.o2 = o2;
-			this.equals = op.apply(o1, o2);
-		}
-
-		public String toString() {
-
-			StringBuilder sb = new StringBuilder();
-
-			sb.append("equals:").append(equals);
-
-			if (!equals) {
-
-				sb.append(" (diff: ").append(o1).append(" != "+o2+")");
+				this(o1, o2, (a, b) -> Objects.equals(a, b));
 			}
 
-			return sb.toString();
-		}
-	}
+			FieldDiff(T o1, T o2, BiFunction<T, T, Boolean> op) {
 
-	private static class ListDiff {
-
-		private List<Map<String, Object>> l1;
-		private List<Map<String, Object>> l2;
-		private Map<Integer, MapDiff> diffs;
-		private boolean equals;
-
-		ListDiff(List<Map<String, Object>> l1, List<Map<String, Object>> l2) {
-
-			this.l1 = l1;
-			this.l2 = l2;
-			this.diffs = diffs();
-		}
-
-		private Map<Integer, MapDiff> diffs() {
-
-			Map<Integer, MapDiff> diffs = new TreeMap<>();
-
-			int maxIndex = (l1.size() > l2.size()) ? l1.size() : l2.size();
-
-			for (int i = 0; i < maxIndex; i++) {
-
-				Map<String, Object> map1 = l1.get(i);
-				Map<String, Object> map2 = l2.get(i);
-
-				MapDiff mapDiff = new MapDiff(map1, map2);
-
-				if (!mapDiff.equals) {
-					diffs.put(i, mapDiff);
-				}
+				this.o1 = o1;
+				this.o2 = o2;
+				this.equals = op.apply(o1, o2);
 			}
 
-			equals = diffs.isEmpty();
+			public String toString() {
 
-			return diffs;
+				StringBuilder sb = new StringBuilder();
+
+				sb.append("equals:").append(equals);
+
+				if (!equals) {
+
+					sb.append(" (diff: ").append(o1).append(" != "+o2+")");
+				}
+
+				return sb.toString();
+			}
 		}
 
-		public String toString() {
+		private static class ListDiff {
 
-			StringBuilder sb = new StringBuilder();
+			private List<Map<String, Object>> l1;
+			private List<Map<String, Object>> l2;
+			private Map<Integer, MapDiff> diffs;
+			private boolean equals;
 
-			if (!equals) {
-				sb.append("diffs:\n");
+			ListDiff(List<Map<String, Object>> l1, List<Map<String, Object>> l2) {
 
-				for (Map.Entry<Integer, MapDiff> entry : diffs.entrySet()) {
-
-					sb.append("\t"+entry.getKey()+" -> "+entry.getValue().toString()+"\n");
-				}
+				this.l1 = l1;
+				this.l2 = l2;
+				this.diffs = diffs();
 			}
 
-			return sb.toString();
-		}
-	}
+			private Map<Integer, MapDiff> diffs() {
 
-	private static class MapDiff {
+				Map<Integer, MapDiff> diffs = new TreeMap<>();
 
-		private Map<String, Object> m1;
-		private Map<String, Object> m2;
-		private Map<String, FieldDiff> diffs;
-		private boolean equals;
+				int maxIndex = (l1.size() > l2.size()) ? l1.size() : l2.size();
 
-		MapDiff(Map<String, Object> m1, Map<String, Object> m2) {
+				for (int i = 0; i < maxIndex; i++) {
 
-			this.m1 = m1;
-			this.m2 = m2;
-			this.diffs = diffs();
-		}
+					Map<String, Object> map1 = l1.get(i);
+					Map<String, Object> map2 = l2.get(i);
 
-		private Map<String, FieldDiff> diffs() {
+					MapDiff mapDiff = new MapDiff(map1, map2);
 
-			Map<String, FieldDiff> diffs = new HashMap<>();
-
-			Set<String> keys1 = m1.keySet();
-			Set<String> keys2 = m2.keySet();
-
-			Set<String> keys = (keys1.size() > keys2.size()) ? keys1 : keys2;
-
-			for (String key : keys) {
-
-				FieldDiff<Object> diffMap;
-
-				if (m1.get(key) instanceof List) {
-
-					List<String> l1 = ((List<String>) m1.get(key)).stream()
-							.sorted()
-							.collect(Collectors.toList());
-					List<String> l2 = ((List<String>) m2.get(key)).stream()
-							.sorted()
-							.collect(Collectors.toList());
-
-					diffMap = new FieldDiff<>(l1, l2);
-				}
-				else if (key.equals(EntrySolrField.PUBLI_COMPUTED_COUNT.getName()) ||
-						key.equals(EntrySolrField.PUBLI_CURATED_COUNT.getName()) ||
-						key.equals(EntrySolrField.PUBLI_LARGE_SCALE_COUNT.getName())) {
-
-					diffMap = new FieldDiff<>(m1.get(key), m2.get(key), (a, b) -> (int)a >= (int)b);
-				}
-				else if (key.equals(EntrySolrField.INFORMATIONAL_SCORE.getName())) {
-
-					diffMap = new FieldDiff<>(m1.get(key), m2.get(key), (a, b) -> (float)a >= (float)b);
-				}
-
-				else {
-					diffMap = new FieldDiff<>(m1.get(key), m2.get(key));
-				}
-
-				if (!diffMap.equals) {
-					diffs.put(key, diffMap);
-				}
-			}
-
-			equals = diffs.isEmpty();
-
-			return diffs;
-		}
-
-		public String toString() {
-
-			StringBuilder sb = new StringBuilder();
-
-			sb.append("equals:").append(equals);
-
-			if (!equals) {
-
-				for (Map.Entry<String, FieldDiff> entry : diffs.entrySet()) {
-
-					if (!entry.getValue().equals) {
-						sb.append(" (diff at ").append(entry.getKey()).append(":").append(entry.getValue().o1).append(" != "+entry.getValue().o2+")");
+					if (!mapDiff.equals) {
+						diffs.put(i, mapDiff);
 					}
 				}
+
+				equals = diffs.isEmpty();
+
+				return diffs;
 			}
 
-			return sb.toString();
+			public String toString() {
+
+				StringBuilder sb = new StringBuilder();
+
+				if (!equals) {
+					sb.append("diffs:\n");
+
+					for (Map.Entry<Integer, MapDiff> entry : diffs.entrySet()) {
+
+						sb.append("\t"+entry.getKey()+" -> "+entry.getValue().toString()+"\n");
+					}
+				}
+
+				return sb.toString();
+			}
+
+			private static class MapDiff {
+
+				private Map<String, Object> m1;
+				private Map<String, Object> m2;
+				private Map<String, FieldDiff> diffs;
+				private boolean equals;
+
+				MapDiff(Map<String, Object> m1, Map<String, Object> m2) {
+
+					this.m1 = m1;
+					this.m2 = m2;
+					this.diffs = diffs();
+				}
+
+				private Map<String, FieldDiff> diffs() {
+
+					Map<String, FieldDiff> diffs = new HashMap<>();
+
+					Set<String> keys1 = m1.keySet();
+					Set<String> keys2 = m2.keySet();
+
+					Set<String> keys = (keys1.size() > keys2.size()) ? keys1 : keys2;
+
+					for (String key : keys) {
+
+						FieldDiff<Object> diffMap;
+
+						if (m1.get(key) instanceof List) {
+
+							List<String> l1 = ((List<String>) m1.get(key)).stream()
+									.sorted()
+									.collect(Collectors.toList());
+							List<String> l2 = ((List<String>) m2.get(key)).stream()
+									.sorted()
+									.collect(Collectors.toList());
+
+							diffMap = new FieldDiff<>(l1, l2);
+						}
+						else if (key.equals(EntrySolrField.PUBLI_COMPUTED_COUNT.getName()) ||
+								key.equals(EntrySolrField.PUBLI_CURATED_COUNT.getName()) ||
+								key.equals(EntrySolrField.PUBLI_LARGE_SCALE_COUNT.getName())) {
+
+							diffMap = new FieldDiff<>(m1.get(key), m2.get(key), (a, b) -> (int)a >= (int)b);
+						}
+						else if (key.equals(EntrySolrField.INFORMATIONAL_SCORE.getName())) {
+
+							diffMap = new FieldDiff<>(m1.get(key), m2.get(key), (a, b) -> (float)a >= (float)b);
+						}
+
+						else {
+							diffMap = new FieldDiff<>(m1.get(key), m2.get(key));
+						}
+
+						if (!diffMap.equals) {
+							diffs.put(key, diffMap);
+						}
+					}
+
+					equals = diffs.isEmpty();
+
+					return diffs;
+				}
+
+				public String toString() {
+
+					StringBuilder sb = new StringBuilder();
+
+					sb.append("equals:").append(equals);
+
+					if (!equals) {
+
+						for (Map.Entry<String, FieldDiff> entry : diffs.entrySet()) {
+
+							if (!entry.getValue().equals) {
+								sb.append(" (diff at ").append(entry.getKey()).append(":").append(entry.getValue().o1).append(" != "+entry.getValue().o2+")");
+							}
+						}
+					}
+
+					return sb.toString();
+				}
+			}
 		}
 	}
 
