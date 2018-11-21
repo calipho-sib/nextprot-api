19ed42689dd6c17fa634274cee2487016b0c0cca
fnikitin <frederic.nikitin@gmail.com>
2018-11-21 15:09:50 +0100

    Remove Criteria field from sortconfig class

--- solr/src/main/java/org/nextprot/api/solr/core/impl/settings/SortConfig.java
+++ solr/src/main/java/org/nextprot/api/solr/core/impl/settings/SortConfig.java
@@ -8,51 +8,22 @@ import java.util.List;
 
 public class SortConfig<F extends SolrField> {
 
-	public enum Criteria {
-		SCORE,
-		NAME,
-		GENE,
-		PROTEIN,
-		FAMILY,
-		LENGTH,
-		AC,
-		CHROMOSOME
-		;
-
-		private final String name;
-
-		Criteria() {
-			this.name = name().toLowerCase();
-		}
-
-		public String getName() {
-			return name;
-		}
-	}
-
-	private Criteria criteria;
 	private List<SortBy<F>> sorting;
 	private int boost = -1;
 	
-	public SortConfig(Criteria criteria, SortBy<F> sortBy) {
-		this.criteria = criteria;
+	public SortConfig(SortBy<F> sortBy) {
 		this.sorting = new ArrayList<>();
 		this.sorting.add(sortBy);
 	}
 
-	public SortConfig(Criteria criteria, SortBy<F> sortBy, int boost) {
-		this(criteria, sortBy);
+	public SortConfig(SortBy<F> sortBy, int boost) {
+		this(sortBy);
 		this.boost = boost;
 	}
 
-	public SortConfig(Criteria criteria, List<SortBy<F>> sorting) {
-		this.criteria = criteria;
+	public SortConfig(List<SortBy<F>> sorting) {
 		this.sorting = new ArrayList<>();
 		this.sorting.addAll(sorting);
-	}
-
-	public Criteria getCriteria() {
-		return criteria;
 	}
 
 	public List<SortBy<F>> getSorting() {
@@ -63,7 +34,29 @@ public class SortConfig<F extends SolrField> {
 		return boost;
 	}
 
-	public static class SortBy<F extends SolrField> {
+    public enum Criteria {
+        SCORE,
+        NAME,
+        GENE,
+        PROTEIN,
+        FAMILY,
+        LENGTH,
+        AC,
+        CHROMOSOME
+        ;
+
+        private final String name;
+
+        Criteria() {
+            this.name = name().toLowerCase();
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class SortBy<F extends SolrField> {
 
 		private final F field;
 		private final ORDER order;
