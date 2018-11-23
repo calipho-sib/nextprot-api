0f3a3150864bfe07f3bec2abffbc1b251698589f
fnikitin <frederic.nikitin@isb-sib.ch>
2018-11-23 14:18:04 +0100

    Testing connection first

--- etl-statement/src/main/java/org/nextprot/api/etl/service/StatementExtractorService.java
+++ etl-statement/src/main/java/org/nextprot/api/etl/service/StatementExtractorService.java
@@ -3,13 +3,11 @@ package org.nextprot.api.etl.service;
 import org.nextprot.commons.statements.Statement;
 import org.nextprot.commons.statements.constants.NextProtSource;
 
+import java.io.IOException;
 import java.util.Set;
 
 public interface StatementExtractorService {
 
-	Set<Statement> getStatementsFromJsonFile(NextProtSource source, String release, String jsonFileName);
-
-	Set<Statement> getStatementsForSource(NextProtSource source, String release);
-
-
+	Set<Statement> getStatementsFromJsonFile(NextProtSource source, String release, String jsonFileName) throws IOException;
+	Set<Statement> getStatementsForSource(NextProtSource source, String release) throws IOException;
 }
--- etl-statement/src/main/java/org/nextprot/api/etl/service/impl/StatementRemoteServiceImpl.java
+++ etl-statement/src/main/java/org/nextprot/api/etl/service/impl/StatementRemoteServiceImpl.java
@@ -7,10 +7,12 @@ import org.nextprot.api.commons.exception.NextProtException;
 import org.nextprot.commons.statements.Statement;
 import org.nextprot.commons.statements.constants.NextProtSource;
 import org.springframework.stereotype.Service;
+import sun.net.www.protocol.http.HttpURLConnection;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
@@ -24,48 +26,58 @@ public class StatementRemoteServiceImpl extends StatementExtractorBase {
 
 	// BioEditor Raw Statement service for a Gene. Example for msh2:
 	// http://kant.isb-sib.ch:9000/bioeditor/gene/msh2/statements
-	public Set<Statement> getStatementsFromJsonFile(NextProtSource source, String release, String jsonFileName) {
+	public Set<Statement> getStatementsFromJsonFile(NextProtSource source, String release, String jsonFileName) throws IOException {
 
 		String urlString = source.getStatementsUrl() + "/" + release + "/" + jsonFileName;
-
 		if (!jsonFileName.endsWith(".json")) {
+			urlString += ".json";
+		}
 
-            urlString += ".json";
-        }
+		if (isServiceUp(urlString)) {
+			return deserialize(getInputStreamFromUrl(urlString));
+		}
 
-		return deserialize(getInputStreamFromUrl(urlString));
+		return new HashSet<>();
 	}
 
 	// BioEditor Raw Statement service for all data (CAREFUL WITH THIS ONE)
 	// http://kant.isb-sib.ch:9000/bioeditor/statements
-	public Set<Statement> getStatementsForSource(NextProtSource source, String release) {
+	public Set<Statement> getStatementsForSource(NextProtSource source, String release) throws IOException {
 
 		Set<Statement> statements = new LinkedHashSet<>();
         getJsonFilenamesForRelease(source, release)
-                .forEach(jsonFilename -> statements.addAll(getStatementsFromJsonFile(source, release, jsonFilename)));
+                .forEach(jsonFilename -> {
+	                try {
+		                statements.addAll(getStatementsFromJsonFile(source, release, jsonFilename));
+	                } catch (IOException e) {
+		                throw new NextProtException(e.getMessage());
+	                }
+                });
 		return statements;
 	}
 
-	Set<String> getJsonFilenamesForRelease(NextProtSource source, String release) {
+	Set<String> getJsonFilenamesForRelease(NextProtSource source, String release) throws IOException {
 		Set<String> genes = new TreeSet<>();
 		String urlString = source.getStatementsUrl() + "/" + release;
 		LOGGER.info("Requesting " +  urlString );
 
-        try (InputStream is = getInputStreamFromUrl(urlString)) {
+		if (isServiceUp(urlString)) {
+			try (InputStream is = getInputStreamFromUrl(urlString)) {
 
-            if (is != null) {
+				if (is != null) {
 
-                String content = IOUtils.toString(is, "UTF8");
-                Pattern pattern = Pattern.compile("href\\=\\\"(.*.json)\\\"", Pattern.MULTILINE);
-                Matcher matcher = pattern.matcher(content);
-                while (matcher.find()) {
-                    genes.add(matcher.group(1));
-                }
-            }
-        }
-        catch (IOException e) {
-            throw new NextProtException("Cannot find json filenames for source "+ source.getSourceName() + ":" + e.getLocalizedMessage());
-        }
+					String content = IOUtils.toString(is, "UTF8");
+					Pattern pattern = Pattern.compile("href\\=\\\"(.*.json)\\\"", Pattern.MULTILINE);
+					Matcher matcher = pattern.matcher(content);
+					while (matcher.find()) {
+						genes.add(matcher.group(1));
+					}
+				}
+			}
+			catch (IOException e) {
+				throw new NextProtException("Cannot find json filenames for source "+ source.getSourceName() + ":" + e.getLocalizedMessage());
+			}
+		}
 
 		return genes;
 	}
@@ -83,4 +95,15 @@ public class StatementRemoteServiceImpl extends StatementExtractorBase {
 			
 	}
 
+	private boolean isServiceUp(String url) throws IOException {
+
+		URL nxflatServerURL = new URL(url);
+
+		HttpURLConnection connection = (HttpURLConnection) nxflatServerURL.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setConnectTimeout(3000);
+		connection.connect();
+
+		return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+	}
 }
