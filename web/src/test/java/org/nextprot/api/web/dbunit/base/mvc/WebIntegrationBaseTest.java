083d19fad2ccd6b5d71547feb55f103576a02b14
fnikitin <frederic.nikitin@gmail.com>
2018-11-26 15:24:24 +0100

    Revert cache profile in IT

--- web/src/test/java/org/nextprot/api/web/dbunit/base/mvc/WebIntegrationBaseTest.java
+++ web/src/test/java/org/nextprot/api/web/dbunit/base/mvc/WebIntegrationBaseTest.java
@@ -2,6 +2,6 @@ package org.nextprot.api.web.dbunit.base.mvc;
 
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles({"dev"})
+@ActiveProfiles({"dev","cache"})
 public abstract class WebIntegrationBaseTest extends WebUnitBaseTest {
 }
