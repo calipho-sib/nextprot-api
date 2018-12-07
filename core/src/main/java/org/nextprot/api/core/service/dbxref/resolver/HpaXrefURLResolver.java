7c1b091fae7d480d2ec8d475bb112eb66f51337e
fnikitin <frederic.nikitin@isb-sib.ch>
2018-12-07 16:21:45 +0100

    HPA resolver now depends on a version number

--- core/src/main/java/org/nextprot/api/core/service/dbxref/resolver/HpaXrefURLResolver.java
+++ core/src/main/java/org/nextprot/api/core/service/dbxref/resolver/HpaXrefURLResolver.java
@@ -1,23 +1,46 @@
 package org.nextprot.api.core.service.dbxref.resolver;
 
+import com.google.common.base.Preconditions;
 import org.nextprot.api.core.domain.CvDatabasePreferredLink;
 import org.nextprot.api.core.domain.DbXref;
 
 class HpaXrefURLResolver extends DefaultDbXrefURLResolver {
 
+	private final int version;
+
+	public HpaXrefURLResolver(int version) {
+
+		super();
+
+		Preconditions.checkArgument(version > 0, "HPA version number should be a positive number");
+
+		this.version = version;
+	}
+
     @Override
     public String getTemplateURL(DbXref xref) {
 
         String accession = xref.getAccession();
+		String url;
 
         if (accession.startsWith("ENSG")) {
             if (accession.endsWith("subcellular")) {
-                return CvDatabasePreferredLink.HPA_SUBCELL.getLink();
+	            url = CvDatabasePreferredLink.HPA_SUBCELL.getLink();
             }
             else {
-                return CvDatabasePreferredLink.HPA_GENE.getLink();
+	            url = CvDatabasePreferredLink.HPA_GENE.getLink();
             }
         }
-        return CvDatabasePreferredLink.HPA_ANTIBODY.getLink();
-    }
+        else {
+	        url = CvDatabasePreferredLink.HPA_ANTIBODY.getLink();
+        }
+
+        if (!url.contains("%v")) {
+
+        	throw new AssertionError("cannot replace %v by the proper version "+version+": urls for HPA lack " +
+			        "the %v placeholder (url="+ url +")");
+        }
+
+		return url.replace("%v", String.valueOf(version));
+	}
 }
