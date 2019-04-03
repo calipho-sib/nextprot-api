0c727a4b1ff834fcf99d0b3704c63c598d3e3887
fnikitin <frederic.nikitin@isb-sib.ch>
2019-04-03 15:33:42 +0200

    Fixing statement mapper

--- core/src/main/java/org/nextprot/api/core/dao/impl/StatementMapper.java
+++ core/src/main/java/org/nextprot/api/core/dao/impl/StatementMapper.java
@@ -2,29 +2,52 @@ package org.nextprot.api.core.dao.impl;
 
 import org.nextprot.commons.statements.Statement;
 import org.nextprot.commons.statements.StatementBuilder;
-import org.nextprot.commons.statements.specs.NXFlatTableSchema;
+import org.nextprot.commons.statements.specs.Specifications;
 import org.nextprot.commons.statements.specs.StatementField;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class StatementMapper implements RowMapper<Statement> {
+
 	public Statement mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-		NXFlatTableSchema schema = NXFlatTableSchema.fromResultSet(rs);
+		Specifications.Builder builder = new Specifications.Builder();
+
+		int index = findColumnIndex(rs.getMetaData(), Specifications.EXTRA_FIELDS);
+
+		if (index > 0) {
+			builder.withExtraFieldsValue(rs.getString(index));
+		}
+
+		Specifications specifications = builder.build();
 
 		StatementBuilder sfbuilder = new StatementBuilder()
-				.withSpecifications(schema);
+				.withSpecifications(specifications);
 
-		for(StatementField key : schema.getFields()) {
+		for (StatementField field : specifications.getFields()) {
 
-			String value = rs.getString(key.getName());
+			String value = rs.getString(field.getName());
 			if (value != null) {
-				sfbuilder.addField(key, value);
+				sfbuilder.addField(field, value);
 			}
 		}
+
 		return sfbuilder.build();
 	}
-	
+
+	private int findColumnIndex(ResultSetMetaData metaData, String columnName) throws SQLException {
+
+		for (int i=1 ; i<=metaData.getColumnCount() ; i++) {
+
+			if (metaData.getColumnName(i).equals(columnName)) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
 }
