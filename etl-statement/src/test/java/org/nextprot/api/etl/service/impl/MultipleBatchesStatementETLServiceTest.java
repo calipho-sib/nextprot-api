d85a0c4e57d83a07621f41ad4f31c96c1085ef00
frednikitin <frederic.nikitin@isb-sib.ch>
2019-05-09 17:38:20 +0200

    Create tests for MultipleBatchesStatementETLService

---
+++ etl-statement/src/test/java/org/nextprot/api/etl/service/impl/MultipleBatchesStatementETLServiceTest.java
@@ -0,0 +1,46 @@
+package org.nextprot.api.etl.service.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.nextprot.api.core.service.MasterIdentifierService;
+import org.nextprot.api.etl.StatementSource;
+import org.nextprot.api.etl.service.StatementLoaderService;
+import org.nextprot.api.etl.service.StatementSourceService;
+import org.nextprot.api.etl.service.StatementTransformerService;
+
+import java.io.IOException;
+
+public class MultipleBatchesStatementETLServiceTest {
+
+	@InjectMocks
+	private MultipleBatchesStatementETLService multipleBatchesStatementETLService = new MultipleBatchesStatementETLService();
+
+	@Mock
+	private MasterIdentifierService masterIdentifierService;
+
+	@Mock
+	private StatementSourceService statementSourceService;
+
+	@Mock
+	private StatementTransformerService statementTransformerService;
+
+	@Mock
+	private StatementLoaderService statementLoadService;
+
+	@Before
+	public void init() {
+
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void extractTransformLoadStatements() throws IOException {
+
+		String log = multipleBatchesStatementETLService.extractTransformLoadStatements(StatementSource.BioEditor, "X", true);
+
+		System.out.println(log);
+	}
+}
