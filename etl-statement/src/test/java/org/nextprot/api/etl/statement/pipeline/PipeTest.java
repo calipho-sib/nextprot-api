8bba091c4e3db2c0ed8f653019cc080beb816847
frednikitin <frederic.nikitin@isb-sib.ch>
2019-05-28 16:35:30 +0200

    Update test

--- etl-statement/src/test/java/org/nextprot/api/etl/statement/pipeline/PipeTest.java
+++ etl-statement/src/test/java/org/nextprot/api/etl/statement/pipeline/PipeTest.java
@@ -1,5 +1,7 @@
 package org.nextprot.api.etl.statement.pipeline;
 
+import org.nextprot.commons.statements.Statement;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -25,19 +27,22 @@ public class PipeTest {
 		public static void main(String[] args) throws IOException {
 
 			URL url = new URL("http://kant.sib.swiss:9001/glyconnect/2019-01-22/all-entries.json");
-
 			Reader reader = new InputStreamReader(url.openStream());
 
-			// Now build up the pipe, starting with the sink, and working
-			// backwards, through various filters, until we reach the source
+			Pump<Statement> pump = new StatementPump(reader, 10);
+
+			Source source = new Source(pump);
+
+			ExampleFilter filter = new ExampleFilter(10);
+			source.connect(filter);
+
 			NxFlatTableSink sink = new NxFlatTableSink(NxFlatTableSink.Table.entry_mapped_statements);
-			ExampleFilter filter = new ExampleFilter(sink);
-			StatementReaderPipeSource source = new StatementReaderPipeSource(new StatementPump(reader, 10), filter);
+			filter.connect(sink);
 
 			// Start the pipe -- start each of the threads in the pipe running.
 			// This call returns quickly, since the each component of the pipe
 			// is its own thread
-			System.out.println("Starting pipe...");
+			System.out.println("Starting the source pipe...");
 			source.startPipe();
 
 			// Wait for the pipe to complete
