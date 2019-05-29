7f2f2476f08c20972e9d9aa4b98b964279a90c69
frednikitin <frederic.nikitin@isb-sib.ch>
2019-05-29 14:09:34 +0200

    Add new classes to implement

---
+++ etl-statement/src/main/java/org/nextprot/api/etl/statement/pipeline/Demultiplexer.java
@@ -0,0 +1,11 @@
+package org.nextprot.api.etl.statement.pipeline;
+
+
+/**
+ * De-multiplexer receive statements via one input channel and
+ * load balance them to multiple output channels.
+ */
+public class Demultiplexer {
+
+
+}
---
+++ etl-statement/src/main/java/org/nextprot/api/etl/statement/pipeline/Multiplexer.java
@@ -0,0 +1,11 @@
+package org.nextprot.api.etl.statement.pipeline;
+
+
+/**
+ * Multiplexer receive statements from multiple input channels and send
+ * them to a single channel.
+ */
+public class Multiplexer {
+
+
+}
