ba000e5f27ca3a8f76b10a156107c377d7845b0f
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2019-06-20 18:29:12 +0200

    [Feature] API service to get entries for a given chromosomal location

--- core/src/main/java/org/nextprot/api/core/dao/impl/GeneIdentifierDaoImpl.java
+++ core/src/main/java/org/nextprot/api/core/dao/impl/GeneIdentifierDaoImpl.java
@@ -3,8 +3,12 @@ package org.nextprot.api.core.dao.impl;
 import org.nextprot.api.commons.spring.jdbc.DataSourceServiceLocator;
 import org.nextprot.api.commons.utils.SQLDictionary;
 import org.nextprot.api.core.dao.GeneIdentifierDao;
+import org.nextprot.api.core.domain.ChromosomalLocation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -39,5 +43,21 @@ public class GeneIdentifierDaoImpl implements GeneIdentifierDao {
 		map.values().forEach(geneNames -> geneNames.sort(String::compareTo));
 
 		return map;
+	}
+
+	@Override
+	public String findGeneNameByChromosomalLocation(ChromosomalLocation chromosomalLocation) {
+		// Finds the gene name for the given chromosomal location
+		String chromosome = chromosomalLocation.getChromosome();
+		int firstPosition = chromosomalLocation.getFirstPosition();
+		int lastPosition = chromosomalLocation.getLastPosition();
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("chromosome", chromosome);
+		parameters.put("first_position", firstPosition);
+		parameters.put("last_position", lastPosition);
+		SqlParameterSource parameterSource = new MapSqlParameterSource(parameters);
+		String geneName = new NamedParameterJdbcTemplate(dsLocator.getDataSource())
+				.queryForObject(sqlDictionary.getSQLQuery("gene-entry-by-chromosomal-location"), parameterSource, String.class);
+		return geneName;
 	}
 }
