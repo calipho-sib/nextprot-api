dcd79e6cdf91ed631776051c42efcfa27a696549
Valentine Rech de Laval <Valentine.RechdeLaval@isb-sib.ch>
2019-08-07 11:42:57 +0200

    Add permission to request in POST for peptide uniqueness checker

--- web/src/main/java/org/nextprot/api/web/controller/EntriesSearchController.java
+++ web/src/main/java/org/nextprot/api/web/controller/EntriesSearchController.java
@@ -1,13 +1,10 @@
 package org.nextprot.api.web.controller;
 
-import java.util.List;
-
 import org.jsondoc.core.annotation.Api;
 import org.jsondoc.core.annotation.ApiMethod;
 import org.jsondoc.core.annotation.ApiQueryParam;
 import org.jsondoc.core.pojo.ApiVerb;
 import org.nextprot.api.commons.exception.NPreconditions;
-import org.nextprot.api.commons.exception.NextProtException;
 import org.nextprot.api.core.domain.Entry;
 import org.nextprot.api.web.service.PepXService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @Api(name = "Peptide uniqueness checker", description = "Retrieve accession nos. of isoforms having a sequence matching the query peptide(s)", group = "Tools")
 @RequestMapping(value = "/entries/search/")
@@ -27,7 +26,7 @@ public class EntriesSearchController {
 	private PepXService pepXService;
 
 	@ResponseBody
-	@RequestMapping(value = "peptide", method = { RequestMethod.GET }, produces = { MediaType.APPLICATION_JSON_VALUE })
+	@RequestMapping(value = "peptide", method = { RequestMethod.GET, RequestMethod.POST }, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = { MediaType.APPLICATION_JSON_VALUE })
 	@ApiMethod(path = "peptide", verb = ApiVerb.GET, description = "Retrieve accession nos. of isoforms having a sequence matching the query peptide(s), taking into account variants. Leucine and isoleucine are considered to be equivalent.")
 	public List<Entry> pepx(
 			@ApiQueryParam(name = "peptide(s)", description = "A peptide or a list of peptides separated with a comma", allowedvalues = { "NDVVPTMAQGVLEYK" }) @RequestParam(value = "peptide", required = true) String peptide)
