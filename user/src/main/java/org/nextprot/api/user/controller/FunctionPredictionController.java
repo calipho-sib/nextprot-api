d7be77966995e742434c9073ab42824d6a1525fa
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2021-04-13 12:36:02 +0200

    Function prediction controller

---
+++ user/src/main/java/org/nextprot/api/user/controller/FunctionPredictionController.java
@@ -0,0 +1,31 @@
+package org.nextprot.api.user.controller;
+
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.pojo.ApiVerb;
+import org.nextprot.api.user.domain.UserProteinList;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Controller to handle function predictions
+ */
+@Lazy
+@Controller
+@Api(name = "Function Predictions", description = "Method to access queries without authentication (SPARQL)", group = "Function Predictions")
+public class FunctionPredictionController {
+
+    @ApiMethod(verb = ApiVerb.GET, description = "Gets all function predictions", produces = { MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE })
+    @RequestMapping(value = "/functionpredictions", method = { RequestMethod.GET })
+    @ResponseBody
+    public String getLists() {
+        return "Predictions";
+    }
+}
