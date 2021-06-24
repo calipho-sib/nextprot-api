eeb80e4f9c4c40a19eef7982d7fc8ded042b09c0
Valentine Rech de Laval <Valentine.RechdeLaval@isb-sib.ch>
2021-06-24 16:41:27 +0200

    Update name, allowed values and description of pepx services

--- web/src/main/java/org/nextprot/api/web/controller/EntriesSearchController.java
+++ web/src/main/java/org/nextprot/api/web/controller/EntriesSearchController.java
@@ -6,6 +6,7 @@ import org.jsondoc.core.annotation.ApiMethod;
 import org.jsondoc.core.annotation.ApiQueryParam;
 import org.jsondoc.core.pojo.ApiVerb;
 import org.nextprot.api.commons.exception.NPreconditions;
+import org.nextprot.api.commons.exception.NextProtException;
 import org.nextprot.api.core.domain.Entry;
 import org.nextprot.api.web.service.PepXService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,27 +32,27 @@ public class EntriesSearchController {
 
 	@ResponseBody
 	@RequestMapping(value = "peptide", method = { RequestMethod.GET }, produces = { MediaType.APPLICATION_JSON_VALUE })
-	@ApiMethod(path = "peptide", verb = ApiVerb.GET, description = "Retrieve accession nos. of isoforms having a sequence matching the query peptide(s), taking into account variants. Leucine and isoleucine are considered to be equivalent.")
+	@ApiMethod(path = "peptide", verb = ApiVerb.GET, description = "Retrieve accession nos. of isoforms having a sequence matching the query peptide(s), taking into account variants. By default, leucine and isoleucine are considered to be equivalent (mode = J).")
 	public List<Entry> pepx(
 			@ApiQueryParam(name = "peptide(s)", description = "A peptide or a list of peptides separated with a comma", allowedvalues = { "NDVVPTMAQGVLEYK" }) 
 			@RequestParam(value = "peptide", required = true) String peptide,
 			@ApiQueryParam(name = "no variant match", description = "Tells if the variants should be taken into account for match", allowedvalues = { "false" })
 			@RequestParam(value = "no-variant-match", required = false) boolean noVariantMatch,
-			@ApiQueryParam(name = "mode I/L", description = "Tells if leucine and isoleucine are considered to be equivalent. Default is true.", allowedvalues = { "true" })
-			@RequestParam(value = "mode-il", required = false) Boolean modeIL) {
+			@ApiQueryParam(name = "mode", description = "Tells if leucine and isoleucine are considered equivalent (mode = J) or not (mode = IL). By default, leucine and isoleucine are considered to be equivalent.", allowedvalues = { "J" })
+			@RequestParam(value = "mode", required = false) String modeIL) {
 		checkPeptideLength(Arrays.stream(peptide.split(",")));
 		return getEntriesWithPeptides(peptide, "GET", noVariantMatch, modeIL);
 	}
 
 	@ResponseBody
 	@RequestMapping(value = "peptide-post", method = { RequestMethod.POST }, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = { MediaType.APPLICATION_JSON_VALUE })
-	@ApiMethod(path = "peptide-post", verb = ApiVerb.POST, description = "Retrieve accession nos. of isoforms having a sequence matching the query peptides, taking into account variants. Leucine and isoleucine are considered to be equivalent.")
+	@ApiMethod(path = "peptide-post", verb = ApiVerb.POST, description = "Retrieve accession nos. of isoforms having a sequence matching the query peptides, taking into account variants. By default, leucine and isoleucine are considered to be equivalent (mode = J).")
 	// TODO: add description of body
 	public List<Entry> pepx(@ApiBodyObject @RequestBody List<String> peptideList,
 			@ApiQueryParam(name = "no variant match", description = "Tells if the variants should be taken into account for match", allowedvalues = { "false" })
 			@RequestParam(value = "no-variant-match", required = false) boolean noVariantMatch,
-			@ApiQueryParam(name = "mode I/L", description = "Tells if leucine and isoleucine are considered to be equivalent. Default is true.", allowedvalues = { "true" })
-			@RequestParam(value = "mode-il", required = false) Boolean modeIL) {
+			@ApiQueryParam(name = "mode", description = "Tells if leucine and isoleucine are considered equivalent (mode = J) or not (mode = IL). By default, leucine and isoleucine are considered to be equivalent.", allowedvalues = { "J" })
+			@RequestParam(value = "mode", required = false) String modeIL) {
 		NPreconditions.checkNotNull(peptideList, "The peptide list must be not null");
 		NPreconditions.checkNotEmpty(peptideList, "The peptide list must be not empty");
 		checkPeptideLength(peptideList.stream());
@@ -62,10 +63,20 @@ public class EntriesSearchController {
 		NPreconditions.checkTrue(peptideList.allMatch(p -> p.length() >= 6), "The minimum length of all peptides must be 6");
 	}
 
-	private List<Entry> getEntriesWithPeptides(String peptide, String method, boolean ignoreVariantMatches, Boolean modeIL) {
-		// Default value for modeIL is true due to legacy (when the parameter didn't exit)
-		if (modeIL == null) {
-			modeIL = true;
+	private List<Entry> getEntriesWithPeptides(String peptide, String method, boolean ignoreVariantMatches, String mode) {
+		// By default, leucine and isoleucine are considered to be equivalent (mode = J) due to legacy (when the parameter is not provided)
+		boolean modeIL = true;
+		if (mode != null) {
+			switch (mode) {
+				case "IL":
+					modeIL = false;
+					break;
+				case "J":
+					break;
+				default:
+					throw new NextProtException("Unknown mode. Mode value can be 'J' to consider leucine and isoleucine equivalent or " +
+							"'IL' to consider leucine and isoleucine different.");
+			}
 		}
 		return pepXService.findEntriesWithPeptides(peptide, modeIL, method, ignoreVariantMatches);
 	}
