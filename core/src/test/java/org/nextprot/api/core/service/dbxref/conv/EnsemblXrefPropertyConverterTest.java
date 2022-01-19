fb60be4ad83a8e9ab96d707bac538d61a56531cd
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2022-01-19 12:41:07 +0100

    New "nxmap" properties for ENST xrefs to reflect ENST,P,G,iso relationships validated by our mapping / alignments

--- core/src/test/java/org/nextprot/api/core/service/dbxref/conv/EnsemblXrefPropertyConverterTest.java
+++ core/src/test/java/org/nextprot/api/core/service/dbxref/conv/EnsemblXrefPropertyConverterTest.java
@@ -4,42 +4,34 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.nextprot.api.commons.constants.IdentifierOffset;
 import org.nextprot.api.core.domain.DbXref;
+import org.nextprot.api.core.domain.DbXref.EnsemblInfos;
 
 import java.util.List;
 
 public class EnsemblXrefPropertyConverterTest {
 
+    
     @Test
-    public void testConvert() {
+    public void testConvertFromToProperty() {
+    	
+    	EnsemblInfos info1 = new EnsemblInfos(1234L, 5678L, 10L, "ENST1", "ENSG2", "ENSP3", "NX_A00001-1");
+    	
+    	DbXref.DbXrefProperty prop = info1.toDbXrefProperty();
+    	Assert.assertEquals("nxmap", prop.getName());
+    	Assert.assertEquals(new Long(1234), prop.getDbXrefId());
+    	Assert.assertEquals(new Long(IdentifierOffset.XREF_PROPERTY_OFFSET+ 5678), prop.getPropertyId());
+    	Assert.assertEquals("ENST1|ENSG2|ENSP3|NX_A00001-1|GOLD", prop.getValue());
 
-        EnsemblXrefPropertyConverter converter = new EnsemblXrefPropertyConverter();
+    	EnsemblInfos info2 = new EnsemblInfos(prop);
+    	
+    	Assert.assertEquals(info1.getEnsg(), info2.getEnsg());
+    	Assert.assertEquals(info1.getEnst(), info2.getEnst());
+    	Assert.assertEquals(info1.getEnsp(), info2.getEnsp());
+    	Assert.assertEquals(info1.getIso(), info2.getIso());
+    	Assert.assertEquals(info1.getEnstIsoMapId(), info2.getEnstIsoMapId());
+    	Assert.assertEquals(info1.getEnstIsoMapQual(), info2.getEnstIsoMapQual());
+    	Assert.assertEquals(info1.getEnstXrefId(), info2.getEnstXrefId());
 
-        DbXref.EnsemblInfos infos = new DbXref.EnsemblInfos(620964, "ENSG00000254647", 1L, "ENSP00000408400", 2L);
-
-        List<DbXref.DbXrefProperty> props = converter.convert(infos);
-        Assert.assertEquals(2, props.size());
-
-        assertProducedXrefPropertyListContains(props, 620964, IdentifierOffset.XREF_ENSEMBL_GENE_PROPERTY_OFFSET + 1, EnsemblXrefPropertyConverter.MAPPED_GENE_NAME, "ENSG00000254647");
-        assertProducedXrefPropertyListContains(props, 620964, IdentifierOffset.XREF_ENSEMBL_PROTEIN_PROPERTY_OFFSET + 2, EnsemblXrefPropertyConverter.MAPPED_PROTEIN_NAME, "ENSP00000408400");
     }
 
-    private void assertProducedXrefPropertyListContains(List<DbXref.DbXrefProperty> producedXrefProperties, long expectedRefId, long expectedPropId, String expectedName, String expectedValue) {
-
-        DbXref.DbXrefProperty xrefPropToCheck = null;
-
-        for (DbXref.DbXrefProperty producedProperty : producedXrefProperties) {
-
-            if (producedProperty.getPropertyId() == expectedPropId) {
-
-                xrefPropToCheck = producedProperty;
-                break;
-            }
-        }
-
-        Assert.assertNotNull(xrefPropToCheck);
-
-        Assert.assertEquals(expectedRefId, xrefPropToCheck.getDbXrefId().longValue());
-        Assert.assertEquals(expectedName, xrefPropToCheck.getName());
-        Assert.assertEquals(expectedValue, xrefPropToCheck.getValue());
-    }
 }
