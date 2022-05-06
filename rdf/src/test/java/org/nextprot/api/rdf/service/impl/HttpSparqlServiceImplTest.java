963ab86a210b9ed87fbd5021ee2b6f59cfe21dcc
Valentine Rech de Laval <Valentine.RechdeLaval@isb-sib.ch>
2022-05-06 01:06:43 -0700

    Enable tests

--- rdf/src/test/java/org/nextprot/api/rdf/service/impl/HttpSparqlServiceImplTest.java
+++ rdf/src/test/java/org/nextprot/api/rdf/service/impl/HttpSparqlServiceImplTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.nextprot.api.rdf.service.HttpSparqlService;
 
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,19 +16,17 @@ public class HttpSparqlServiceImplTest {
 
 	@Test
 	public void testSearchWithOneVar() {
-
-		if (runTest()) {
-			HttpSparqlService sparqlService = new HttpSparqlServiceImpl();
-
-			HttpSparqlService.SparqlResponse response = sparqlService.executeSparqlQuery(SPARQL_DEFAULT_URL,
-					"select distinct ?entry where {\n" +
-							"  ?entry :isoform ?iso.\n" +
-							"  ?iso :keyword / :term cv:KW-0597.\n" +
-							"  ?iso :cellularComponent /:term /:childOf cv:SL-0086.\n" +
-							"}");
-
-			checkExpectedValues(response, Collections.singletonList("entry"), 5666);
-		}
+		
+		HttpSparqlService sparqlService = new HttpSparqlServiceImpl();
+		
+		HttpSparqlService.SparqlResponse response = sparqlService.executeSparqlQuery(SPARQL_DEFAULT_URL,
+				"select distinct ?entry where {\n" +
+						"  ?entry :isoform ?iso.\n" +
+						"  ?iso :keyword / :term cv:KW-0597.\n" +
+						"  ?iso :cellularComponent /:term /:childOf cv:SL-0086.\n" +
+						"}");
+		
+		checkExpectedValues(response, Collections.singletonList("entry"), 5666);
 	}
 
 	@Test
@@ -44,14 +41,12 @@ public class HttpSparqlServiceImplTest {
 
 	@Test
 	public void testSearchWithTwoVars() {
-
-		if (runTest()) {
-			HttpSparqlService sparqlService = new HttpSparqlServiceImpl();
-
-			HttpSparqlService.SparqlResponse response = sparqlService.executeSparqlQuery(SPARQL_DEFAULT_URL, "select distinct ?entry ?iso where {\n" + "  ?entry :isoform ?iso.\n" + "  ?iso :keyword / :term cv:KW-0597.\n" + "  ?iso :cellularComponent /:term /:childOf cv:SL-0086.\n" + "}");
-
-			checkExpectedValues(response, Arrays.asList("entry", "iso"), 13915);
-		}
+		
+		HttpSparqlService sparqlService = new HttpSparqlServiceImpl();
+		
+		HttpSparqlService.SparqlResponse response = sparqlService.executeSparqlQuery(SPARQL_DEFAULT_URL, "select distinct ?entry ?iso where {\n" + "  ?entry :isoform ?iso.\n" + "  ?iso :keyword / :term cv:KW-0597.\n" + "  ?iso :cellularComponent /:term /:childOf cv:SL-0086.\n" + "}");
+		
+		checkExpectedValues(response, Arrays.asList("entry", "iso"), 13915);
 	}
 
 	private void checkExpectedValues(HttpSparqlService.SparqlResponse response, List<String> expectedVars, int expectedRows) {
@@ -59,10 +54,5 @@ public class HttpSparqlServiceImplTest {
 		Assert.assertTrue(!response.getVars().isEmpty());
 		Assert.assertEquals(expectedVars, response.getVars());
 		Assert.assertTrue(response.rows() >= expectedRows);
-	}
-
-	private boolean runTest() {
-		// The test should be performed if the current date is after than 2022-04-12
-		return LocalDate.now().isAfter(LocalDate.of(2022, 5, 12));
 	}
 }
